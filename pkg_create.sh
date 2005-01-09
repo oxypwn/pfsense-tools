957eda26de78adf15688ea855a6f6cde3bb2db1c
Scott Ullrich <sullrich@pfsense.org>
2005-01-09 08:12:47 +0000

    Finish switchover of packages to freebsd servers.

--- pkg_create.sh
+++ pkg_create.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-# special ports
-#  /usr/ports/security/stunnel -- do not ask about adding users.
-#  /usr/ports/dns/powerdns -- rather include sqlite3.
+echo Removing old ports structures...
+rm -rf /usr/ports/*
+
+rm /etc/ports-supfile
+cp ports-supfile /etc/
+cvsup /etc/ports-supfile
+mkdir -p /usr/ports/packages/All
+mkdir -p /usr/ports/distfiles/
 
 mkdir -p /usr/ports/packages/All
 rm -rf /usr/ports/packages/All/*
@@ -10,9 +15,16 @@ rm -rf /usr/ports/packages/All/*
 PACKAGES="/usr/ports/ftp/pure-ftpd/ \
 	/usr/ports/net/ntop/ \
 	/usr/ports/security/nmap \
+	/usr/ports/dns/powerdns \
+	/usr/ports/security/stunnel \
+	/usr/ports/security/nmap \
 	/usr/ports/sysutils/pfstat"
 
 for PORT in $PACKAGES; do
+	cd $PORT && make deinstall
 	cd $PORT && make package-recursive
 done
 
+# Handle stunnel special case where we need to eliminate
+# the script from asking if a user should be added or
+# removed.
