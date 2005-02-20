9a5fc26ffc1654965c48e41c9b5d231d72c51522
Scott Ullrich <sullrich@pfsense.org>
2005-02-20 22:40:09 +0000

    Instead of using hard links to /home/sullrich/freesbie and such define shell variables.

--- doall.sh
+++ doall.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+
+FREESBIEBASEDIR=/usr/local/livefs
+LOCALDIR=/home/sullrich/freesbie
+PATHISO=/home/sullrich/freesbie/FreeSBIE.iso
+
 cd /home/sullrich
-rm -rf /home/sullrich/pfSense
+rm -rf $LOCALDIR
 cvs -d:ext:sullrich@216.135.66.16:/cvsroot co pfSense
 
 # if platform == net45xx/wrap ###########################
@@ -10,28 +15,28 @@ cvs -d:ext:sullrich@216.135.66.16:/cvsroot co pfSense
 #sleep 3
 # end if ################################################
 
-cd /home/sullrich/pfSense
-if [ ! -e /home/sullrich/pfSense/libexec ]; then
-	mkdir -p /home/sullrich/pfSense/libexec
+cd $LOCALDIR
+if [ ! -e $LOCALDIR/libexec ]; then
+	mkdir -p $LOCALDIR/libexec
 fi
-cp /usr/lib/libkrb5.so.7 /usr/local/livefs/usr/lib/
-mkdir -p /usr/local/livefs/var/run
-echo "#!/bin/sh" > /home/sullrich/pfSense/script
-echo rm /etc/resolv.conf >> /home/sullrich/pfSense/script
-echo rm /etc/hosts >> /home/sullrich/pfSense/script
-echo ln -s /cf/conf /conf >> /home/sullrich/pfSense/script
-echo ln -s /conf /cf/conf >> /home/sullrich/pfSense/script
-echo ln -s /var/run/htpasswd /usr/local/www/.htpasswd >> /home/sullrich/pfSense/script
-echo ln -s /var/etc/hosts /etc/hosts >> /home/sullrich/pfSense/script
-echo ln -s /var/etc/resolv.conf /etc/resolv.conf >> /home/sullrich/pfSense/script
-echo ln -s /lib/libm.so.3 /lib/libm.so.2 >> /home/sullrich/pfSense/script
-echo ln -s /lib/libc.so.5 /lib/libc.so.4 >> /home/sullrich/pfSense/script
-cat /home/sullrich/pfSense/script
-chmod a+x /home/sullrich/pfSense/script
-chroot /home/sullrich/pfSense/ /script
-find /home/sullrich/pfSense && find /home/sullrich/pfSense -name CVS -exec rm -rf {} \;
-rm /home/sullrich/pfSense.tgz
-cd /home/sullrich/pfSense & tar czvPf /home/sullrich/pfSense.tgz .
+cp /usr/lib/libkrb5.so.7 $FREESBIEBASEDIR/usr/lib/
+mkdir -p $FREESBIEBASEDIR/var/run
+echo "#!/bin/sh" > $LOCALDIR/script
+echo rm /etc/resolv.conf >> $LOCALDIR/script
+echo rm /etc/hosts >> $LOCALDIR/script
+echo ln -s /cf/conf /conf >> $LOCALDIR/script
+echo ln -s /conf /cf/conf >> $LOCALDIR/script
+echo ln -s /var/run/htpasswd /usr/local/www/.htpasswd >> $LOCALDIR/script
+echo ln -s /var/etc/hosts /etc/hosts >> $LOCALDIR/script
+echo ln -s /var/etc/resolv.conf /etc/resolv.conf >> $LOCALDIR/script
+echo ln -s /lib/libm.so.3 /lib/libm.so.2 >> $LOCALDIR/script
+echo ln -s /lib/libc.so.5 /lib/libc.so.4 >> $LOCALDIR/script
+cat $LOCALDIR/script
+chmod a+x $LOCALDIR/script
+chroot $LOCALDIR/ /script
+find $LOCALDIR && find $LOCALDIR -name CVS -exec rm -rf {} \;
+rm $LOCALDIR.tgz
+cd $LOCALDIR & tar czvPf $LOCALDIR.tgz .
 cd /home/sullrich/freesbie
 ./0.rmdir.sh
 ./1.mkdir.sh
@@ -41,25 +46,25 @@ cd /home/sullrich/freesbie
 ./5.patchfiles.sh
 ./6.packages.sh
 ./7.customuser.sh
-cp /sbin/pf* /usr/local/livefs/sbin
-chmod a+x /usr/local/livefs/sbin/pf*
-cp /home/sullrich/freesbie/files/gettytab /usr/local/livefs/etc/
-cp /sbin/ip* /usr/local/livefs/sbin/
-cp /usr/sbin/ip* /usr/local/livefs/usr/sbin/
-rm -rf /usr/local/livefs/dist/pfSense.tgz
-cp /home/sullrich/pfSense.tgz /usr/local/livefs/dist/
-cp /home/sullrich/freesbie/files/ip* /usr/local/livefs/boot/kernel/
-cp /home/sullrich/freesbie/files/dummynet* /usr/local/livefs/boot/kernel/
-cp /usr/lib/libstdc* /usr/local/livefs/usr/lib/
-cp /home/sullrich/freesbie/files/foobar/ttys /usr/local/livefs/etc/ttys
-mkdir -p /usr/local/livefs/usr/local/share/dfuibe_installer
-cp /home/sullrich/freesbie/files/sources.conf /usr/local/livefs/usr/local/share/dfuibe_installer/sources.conf
-cp /home/sullrich/freesbie/files/loader.rc /usr/local/livefs/boot/loader.rc
-rm -rf /usr/local/livefs/etc/shells
-cp /home/sullrich/freesbie/files/shells /usr/local/livefs/etc/shells
-echo exit > /usr/local/livefs/root/.xcustom.sh
-echo hint.acpi.0.disabled=\"1\" >> /usr/local/livefs/boot/device.hints
-echo "-m -P" >> /usr/local/livefs/boot.config
+cp /sbin/pf* $FREESBIEBASEDIR/sbin
+chmod a+x $FREESBIEBASEDIR/sbin/pf*
+cp /home/sullrich/freesbie/files/gettytab $FREESBIEBASEDIR/etc/
+cp /sbin/ip* $FREESBIEBASEDIR/sbin/
+cp /usr/sbin/ip* $FREESBIEBASEDIR/usr/sbin/
+rm -rf $FREESBIEBASEDIR/dist/pfSense.tgz
+cp $LOCALDIR.tgz $FREESBIEBASEDIR/dist/
+cp /home/sullrich/freesbie/files/ip* $FREESBIEBASEDIR/boot/kernel/
+cp /home/sullrich/freesbie/files/dummynet* $FREESBIEBASEDIR/boot/kernel/
+cp /usr/lib/libstdc* $FREESBIEBASEDIR/usr/lib/
+cp /home/sullrich/freesbie/files/foobar/ttys $FREESBIEBASEDIR/etc/ttys
+mkdir -p $FREESBIEBASEDIR/usr/local/share/dfuibe_installer
+cp /home/sullrich/freesbie/files/sources.conf $FREESBIEBASEDIR/usr/local/share/dfuibe_installer/sources.conf
+cp /home/sullrich/freesbie/files/loader.rc $FREESBIEBASEDIR/boot/loader.rc
+rm -rf $FREESBIEBASEDIR/etc/shells
+cp /home/sullrich/freesbie/files/shells $FREESBIEBASEDIR/etc/shells
+echo exit > $FREESBIEBASEDIR/root/.xcustom.sh
+echo hint.acpi.0.disabled=\"1\" >> $FREESBIEBASEDIR/boot/device.hints
+echo "-m -P" >> $FREESBIEBASEDIR/boot.config
 # trim off some extra fat.
 ./8.preparefs.sh
 ./81.mkiso.sh
