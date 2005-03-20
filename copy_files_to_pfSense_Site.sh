258f2c67286190057c8cf3c566840e0fe63e4095
Scott Ullrich <sullrich@pfsense.org>
2005-03-20 22:17:02 +0000

    Remove master.passwd, passwd and /etc/ttys on upgrade file

--- copy_files_to_pfSense_Site.sh
+++ copy_files_to_pfSense_Site.sh
@@ -4,13 +4,16 @@ echo Copying ISO to 10.0.250.50:~sullrich ... CTRL-C to abort.
 scp /home/sullrich/freesbie/FreeSBIE.iso sullrich@10.0.250.50:~
 echo Copying ISO to www.livebsd.com ... CTRL-C to abort.
 scp -C /home/sullrich/freesbie/FreeSBIE.iso \
-        sullrich@www.pfsense.com:/usr/local/www/pfsense/Etomite0.6/downloads/pfSense-0.35.iso
-
+        sullrich@www.pfsense.com:/usr/local/www/pfsense/Etomite0.6/downloads/pfSense-0.53.iso
 
 cd /home/sullrich/pfSense
 rm -rf conf*
 rm -rf usr/local/www/trigger_initial_wizard
 PVERSUFFIX=`date "+%Y.%m%d"`-`cat /home/sullrich/pfSense/etc/version`
+rm /home/sullrich/pfSense/master.passwd /home/sullrich/pfSense/passwd
+rm /home/sullrich/pfSense/ttys
+cd /home/sullrich/pfSense && cp -R /usr/local/livefs/boot .
+cd /home/sullrich/pfSense/boot && rm device.hints loader.conf loader.rc
 cd /home/sullrich/pfSense && tar czvPf /pfSenseUpdate-$PVERSUFFIX.tgz .
 echo Copying pfSenseUpdate-$PVERSUFFIX.tgz to updates folder/
 scp -C /pfSenseUpdate-$PVERSUFFIX.tgz \
