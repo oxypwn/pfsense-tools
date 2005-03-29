c60e288a1d30e9db95a06f5c73216bfcbdc5b5c5
Scott Ullrich <sullrich@pfsense.org>
2005-03-29 02:26:26 +0000

    Update progress bar to latest.

--- upload_progress_meter.c
+++ upload_progress_meter.c
@@ -43,7 +43,7 @@ static void file_php_get_info(char *, zval * );
 static void mmcache_php_get_info(char *, zval * );
 
 PHP_INI_BEGIN()
-PHP_INI_ENTRY("upload_progress_meter.store_method",  "", PHP_INI_ALL, NULL)
+PHP_INI_ENTRY("upload_progress_meter.store_method",  "file", PHP_INI_ALL, NULL)
 PHP_INI_ENTRY("upload_progress_meter.mmcache",  "0", PHP_INI_ALL, NULL)
 PHP_INI_ENTRY("upload_progress_meter.file",  "1", PHP_INI_ALL, NULL)
 PHP_INI_ENTRY("upload_progress_meter.file.filename_template",  "/tmp/upt_%s.txt", PHP_INI_ALL, NULL)
@@ -70,8 +70,8 @@ zend_module_entry upload_progress_meter_module_entry =
     upload_progress_meter_functions,
     PHP_MINIT(upload_progress_meter),
     PHP_MSHUTDOWN(upload_progress_meter),
-    NULL, 
-    NULL, 
+    NULL,
+    NULL,
     PHP_MINFO(upload_progress_meter),
     NO_VERSION_YET,
     STANDARD_MODULE_PROPERTIES
@@ -93,7 +93,7 @@ PHP_MINFO_FUNCTION(upload_progress_meter)
    php_info_print_table_row(2, "available backend modules", "file");
    php_info_print_table_end();
 }
-	
+
 extern int upload_progress_register_callback( void* );
 static void * upload_progress_callback( void *, int, int, int );
 PHP_MINIT_FUNCTION(upload_progress_meter)
@@ -106,7 +106,7 @@ PHP_MINIT_FUNCTION(upload_progress_meter)
 PHP_MSHUTDOWN_FUNCTION(upload_progress_meter)
 {
    upload_progress_register_callback( NULL );
-   
+
    UNREGISTER_INI_ENTRIES();
    return SUCCESS;
 }
@@ -124,20 +124,20 @@ ZEND_FUNCTION(upload_progress_meter_get_info)
    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &id, &id_lg) == FAILURE) {
       return;
    }
-  
+
    if (!check_identifier(id))
       return;	/* invalid identifier, may be a security risk */
 
    method = select_method();
 
-   if (method == 'm') 
+   if (method == 'm')
       return mmcache_php_get_info( id, return_value );
    if (method == 'f')
       return file_php_get_info( id, return_value );
 
    RETURN_FALSE;
 }
-static void mmcache_php_get_info(char * id, zval * return_value) {} 
+static void mmcache_php_get_info(char * id, zval * return_value) {}
 static void file_php_get_info(char * id, zval * return_value)
 {
    char s[1024];
@@ -157,22 +157,22 @@ static void file_php_get_info(char * id, zval * return_value)
 	 char *k, *v, *e;
 	 e = strchr(s,'=');
 	 if (!e) continue;
-	 
+
 	 *e = 0; /* break the line into 2 parts */
 	 v = e+1;
 	 k = s;
-	 
+
 	 /* trim spaces in front and after the name/value */
 	 while (*k && *k <= 32) v++;
 	 while (*v && *v <= 32) v++;
 	 for (e=k; *e; e++) if (*e <= 32) { *e = 0; break; }
 	 for (e=v; *e; e++) if (*e <= 32) { *e = 0; break; }
-	 
+
 	 add_assoc_string( return_value, k, v, 1 );
       }
       fclose(F);
    }
-   
+
    if (filename) efree(filename);
    return;
 }
@@ -183,27 +183,27 @@ static void file_php_get_info(char * id, zval * return_value)
 static void * upload_progress_callback( void *pointer, int read_bytes, int total_bytes, int what_happened )
 {
 //   sapi_module.sapi_error(E_NOTICE, "meter: callback called: read %d of %d reason=%d", read_bytes, total_bytes, what_happened );
-   
+
    if (pointer == NULL) /* invalid call. ignoring... */
       return NULL;
-   
-   
+
+
    if (read_bytes == 0) { 	/* INIT time */
       char  method;
 
-      if (!check_identifier(pointer)) 
+      if (!check_identifier(pointer))
 	 return NULL;		/* invalid identifier, possibly a security risk */
 
       method = select_method();
-      if (method == 'm') 
+      if (method == 'm')
 	 return callback_mmcache( pointer, read_bytes, total_bytes, what_happened );
-      
+
       if (method == 'f')
 	 return callback_file( pointer, read_bytes, total_bytes, what_happened );
-      
+
       return NULL;
    }
-   
+
    if ( ((UPCdata*)pointer)->__magic  != UPC_MAGIC) return NULL;
    if ( ((UPCdata*)pointer)->__struct_lg != sizeof(UPCdata)) return NULL;
    if ( ((UPCdata*)pointer)->callback == NULL) return NULL;
@@ -214,13 +214,13 @@ static void * upload_progress_callback( void *pointer, int read_bytes, int total
 static void * callback_file( void *pointer, int read_bytes, int total_bytes, int what_happened )
 {
 //   sapi_module.sapi_error(E_NOTICE, "meter: callback file: read %d of %d reason=%d", read_bytes, total_bytes, what_happened );
-   
+
    if (pointer == NULL) /* invalid call. ignoring... */
       return NULL;
 
    if (read_bytes == 0) { 	/* INIT time */
       UPCdata * progress = emalloc( sizeof(UPCdata) ); /* alocate internal structure */
-      
+
       progress->__magic = UPC_MAGIC;
       progress->__struct_lg = sizeof(UPCdata);
 
@@ -229,7 +229,7 @@ static void * callback_file( void *pointer, int read_bytes, int total_bytes, int
 
 sapi_module.sapi_error(E_NOTICE, "meter: callback file: identifier=%s", progress->identifier );
 
-      progress->time_start = 
+      progress->time_start =
       progress->time_last  = time(NULL);
       progress->speed_average  = 0;
       progress->speed_last     = 0;
@@ -237,39 +237,39 @@ sapi_module.sapi_error(E_NOTICE, "meter: callback file: identifier=%s", progress
       progress->bytes_total    = total_bytes;
       progress->files_uploaded = 0;
       progress->est_sec        = 0;
-      
+
       update_file(progress);
-	 
+
       return progress;
    }
 
 
-   
+
    {  /* compute upload speed and update file if necessary */
       UPCdata * progress = pointer;
       time_t crtime = time(NULL);
       int d,dt,ds;
-      
-   
-      if (what_happened > 0) 
+
+
+      if (what_happened > 0)
 	 progress->files_uploaded++;
 
       if (progress->time_last > crtime)  /* just in case we encounter a fracture in time */
 	 progress->time_start = progress->time_last = crtime;
 
-       	 
+
       dt = crtime - progress->time_last;
       ds = crtime - progress->time_start;
       d = read_bytes - progress->bytes_uploaded;
-      
+
       if (dt) {
 	 progress->speed_last = d/dt;
-      
+
 	 progress->time_last = crtime;
 	 progress->bytes_uploaded = read_bytes;
 
 	 progress->speed_average = ds ? read_bytes / ds : 0;
-         progress->est_sec = progress->speed_average ? 
+         progress->est_sec = progress->speed_average ?
 		 		(progress->bytes_total - read_bytes) / progress->speed_average
 				:
 				-1;
@@ -285,7 +285,7 @@ sapi_module.sapi_error(E_NOTICE, "meter: callback file: identifier=%s", progress
       efree( pointer );
       return NULL;
    }
-      
+
    return pointer;
 }
 
@@ -298,22 +298,22 @@ static int select_method(void)
 
    while (index < Nlg) {
       int x = index + 1;
-      
+
       if (methods[index] <= 32 || methods[index] == ',') {
 	 index++;
 	 continue;
       }
-      
+
       while (x < Nlg) {
-	 if (methods[x] <= 32 || methods[x] == ',') 
-	    break; 
+	 if (methods[x] <= 32 || methods[x] == ',')
+	    break;
 	 else x++;
       }
 
       methods[x] = 0;
-      
+
       //sapi_module.sapi_error(E_NOTICE, "meter: index=%d x=%d Nlg=%d crM=%s", index,x,Nlg,methods+index);
-      
+
       if (!strcasecmp( methods+index, "mmcache" ) && INI_INT("upload_progress_meter.mmcache") && mmcache_loaded() ) {
 	 selected = 'm';
 	 break;
@@ -322,10 +322,10 @@ static int select_method(void)
 	 selected = 'f';
 	 break;
       }
-      
+
       index = x;
    }
-   
+
    efree(methods);
    return selected;
 }
@@ -347,7 +347,7 @@ static int check_identifier(char * identifier)
 	 return 0; /* reject strange looking identifiers */
       }
    }
-   
+
    return 1;
 }
 static char * mk_filename(char * identifier)
@@ -355,9 +355,9 @@ static char * mk_filename(char * identifier)
    char * template = INI_STR("upload_progress_meter.file.filename_template");
    char * x;
    char * filename;
-   
+
    filename = emalloc( strlen(template) + strlen(identifier) + 3 );
-   
+
    x = strstr( template, "%s" );
    if (x==NULL) {
       sprintf( filename, "%s/%s", template, identifier );
@@ -366,7 +366,7 @@ static char * mk_filename(char * identifier)
       strcpy( filename + (x - template), identifier );
       strcat( filename, x+2 );
    }
-   
+
    return filename;
 }
 static void update_file(UPCdata *X)
@@ -377,10 +377,10 @@ static void update_file(UPCdata *X)
    F = VCWD_FOPEN(X->identifier, "wb");
    if (F) {
       fprintf(F, "time_start=%d\ntime_last=%d\nspeed_average=%d\nspeed_last=%d\nbytes_uploaded=%d\nbytes_total=%d\nfiles_uploaded=%d\nest_sec=%d\n",
-		      X->time_start, X->time_last, 
-		      X->speed_average, X->speed_last, 
-		      X->bytes_uploaded, X->bytes_total, 
-		      X->files_uploaded, 
+		      X->time_start, X->time_last,
+		      X->speed_average, X->speed_last,
+		      X->bytes_uploaded, X->bytes_total,
+		      X->files_uploaded,
 		      X->est_sec );
       fclose(F);
    }else{
