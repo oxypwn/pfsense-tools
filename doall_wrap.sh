3f85e7cd5b688397290ca8e72fc8b3493e85cd12
Scott Ullrich <sullrich@pfsense.org>
2005-04-10 01:50:22 +0000

    Split wrap process into its own file

--- doall_wrap.sh
+++ doall_wrap.sh
@@ -1,26 +1,19 @@
-#!/bin/sh
-
-# pfSense master builder script
-# (C)2005 Scott Ullrich and the pfSense project
-# All rights reserved.
-
-#set -e -u
 
 . ../freesbie/config.sh
 . ../freesbie/.common.sh
 
 PFSENSECVS=/home/sullrich/pfSense
-UPDATES=/home/sullrich/updates
 
-cd /home/sullrich
-rm -rf $PFSENSECVS
-cvs -d:ext:sullrich@216.135.66.16:/cvsroot co pfSense
+rm pfSense/usr/local/www/trigger_initial_wizard
+ls -la pfSense/usr/local/www/trigger_initial_wizard
+cho net45xx >> pfSense/usr/local/www/platform
+sleep 3
 
 cd $PFSENSECVS
 if [ ! -e $PFSENSECVS/libexec ]; then
         mkdir -p $PFSENSECVS/libexec
 fi
-#cp /usr/lib/libkrb5.so.7 $LOCALDIR/usr/lib/
+cp /usr/lib/libkrb5.so.7 $LOCALDIR/usr/lib/
 mkdir -p $LOCALDIR/var/run
 echo "#!/bin/sh" > $PFSENSECVS/script
 echo rm /etc/resolv.conf >> $PFSENSECVS/script
@@ -35,31 +28,21 @@ echo ln -s /lib/libc.so.5 /lib/libc.so.4 >> $PFSENSECVS/script
 cat $PFSENSECVS/script
 chmod a+x $PFSENSECVS/script
 chroot $PFSENSECVS/ /script
-#find $PFSENSECVS -name CVS -exec rm -rf {} \;
-rm -rf $PFSENSECVS.tgz
+find $PFSENSECVS && find $PFSENSECVS -name CVS -exec rm -rf {} \;
+rm $PFSENSECVS.tgz
 cd $PFSENSECVS & tar czvPf $PFSENSECVS.tgz .
 cd $LOCALDIR
 ./0.rmdir.sh
 ./1.mkdir.sh
 #./2.buildworld.sh
-if [ "$?" != "0" ]; then
-    echo "Something went wrong."
-    exit 1;
-fi
 ./3.installworld.sh
-./4.kernel.sh FREESBIE.5
-if [ "$?" != "0" ]; then
-    echo "Something went wrong."
-    exit 1;
-fi
+./4.kernel.sh FREESBIE.5.WRAP-SOEKRIS
 ./5.patchfiles.sh
 ./6.packages.sh
 ./7.customuser.sh
-
 # restore values if overwritten accidently.
 . ../freesbie/config.sh
 . ../freesbie/.common.sh
-
 cp /sbin/pf* $FREESBIEBASEDIR/sbin
 chmod a+x $FREESBIEBASEDIR/sbin/pf*
 cp $LOCALDIR/files/gettytab $FREESBIEBASEDIR/etc/
@@ -73,40 +56,26 @@ cp /usr/lib/libstdc* $FREESBIEBASEDIR/usr/lib/
 cp $LOCALDIR/files/foobar/ttys $FREESBIEBASEDIR/etc/ttys
 mkdir -p $FREESBIEBASEDIR/usr/local/share/dfuibe_installer
 cp $LOCALDIR/files/sources.conf \
-$FREESBIEBASEDIR/usr/local/share/dfuibe_installer/sources.conf
+        $FREESBIEBASEDIR/usr/local/share/dfuibe_installer/sources.conf
 cp $LOCALDIR/files/loader.rc $FREESBIEBASEDIR/boot/loader.rc
 rm -rf $FREESBIEBASEDIR/etc/shells
 cp $LOCALDIR/files/shells $FREESBIEBASEDIR/etc/shells
 echo exit > $FREESBIEBASEDIR/root/.xcustom.sh
 touch $FREESBIEBASEDIR/root/.hushlogin
 echo hint.acpi.0.disabled=\"1\" >> $FREESBIEBASEDIR/boot/device.hints
+# trim off some extra fat.
+#./8.preparefs.sh
+#./81.mkiso.sh
 
+# XXX: tar up kernel and world for embedded updates
 version_kernel=`cat /usr/local/livefs/etc/version_kernel`
 version_base=`cat /usr/local/livefs/etc/version_base`
 version=`cat /usr/local/livefs/etc/version`
-
-# trim off some extra fat.
-./8.preparefs.sh
-if [ "$?" != "0" ]; then
-    echo "Something went wrong."
-    exit 1;
-fi
-./81.mkiso.sh
-
-mkdir -p $UPDATES
-
-# XXX: tar up base and kernel for pfsense versions
-echo Creating tarballs...
 cd /usr/local/livefs
-tar czpf /$UPDATES$UPDATES/kernel-${version_kernel}.tgz /usr/local/livefs/boot/
+tar czvpf /kernel-${version_kernel}-wrap-soekris.tgz /usr/local/livefs/boot/
 rm -rf /usr/local/livefs/boot/
-tar czpf /$UPDATES/base-${version_base}.tgz .
-cd /home/sullrich/pfSense/
-tar zcpf /$UPDATES/pfSense-${version}.tgz .
+tar czvpf /base-${version_base}-wrap-soekris.tgz .
 
-exit
-
-
-
-
+# Finish up wrap configuration
+/home/sullrich/tools/finish_wrap.sh
 
