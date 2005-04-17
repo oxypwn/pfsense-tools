dbe52e2b3be6c0dccd6e9d20ca1854ba6c89334c
Scott Ullrich <sullrich@pfsense.org>
2005-04-17 17:41:06 +0000

    Remove fstab from the update file.

--- copy_files_to_pfSense_Site.sh
+++ copy_files_to_pfSense_Site.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 VERSION=0.60
+PRODUCTNAME=pfSense
 SCPUSERNAME=sullrich
 DSTISO=pfSense-$VERSION.iso
 LIVEFS=/usr/local/livefs/
 PVERSUFFIX=`date "+%Y.%m%d"`-`cat /home/sullrich/pfSense/etc/version`
-FILENAME=pfSense-Full-Update-${VERSION}-${PVERSUFFIX}.tgz
+FILENAME=${PRODUCTNAME}-Full-Update-${VERSION}-${PVERSUFFIX}.tgz
 SRCISO=FreeSBIE.iso
 DSTWEBSITE=www.pfsense.com:/usr/local/www/pfsense/Etomite0.6/downloads/
-UPDATESDIR=/home/sullrich/updates
-FREESBIEDIR=/home/sullrich/freesbie
-WEBSITEWWWDIR=/usr/local/www/pfsense/Etomite0.6
+UPDATESDIR=/home/sullrich/updates/
+FREESBIEDIR=/home/sullrich/freesbie/
+WEBSITEWWWDIR=/usr/local/www/pfsense/Etomite0.6/
 
 # Copy image to root of developers box
 echo Copying ISO to 10.0.250.50:~${SCPUSERNAME} ... CTRL-C to abort.
@@ -28,6 +29,7 @@ rm ${LIVEFS}/usr/local/www/trigger_initial_wizard
 rm ${LIVEFS}/etc/master.passwd
 rm ${LIVEFS}/etc/passwd
 rm ${LIVEFS}/etc/ttys
+rm ${LIVEFS}/etc/fstab
 rm ${LIVEFS}/boot/device.hints
 rm ${LIVEFS}/boot/loader.conf
 rm ${LIVEFS}/boot/loader.rc
