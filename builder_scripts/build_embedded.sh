2daa19b67e594769f16ff35d1bf8f295bcf42476
Scott Ullrich <sullrich@pfsense.org>
2005-06-16 23:21:46 +0000

    Clone build_iso.sh to build_updates.sh and build_embedded.sh which will become the new scripts for the build process

---
+++ builder_scripts/build_embedded.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# pfSense master builder script
+# (C)2005 Scott Ullrich and the pfSense project
+# All rights reserved.
+
+set -e -u		# uncomment me if you want to exit on shell errors
+
+# Read in FreeSBIE configuration variables and set:
+#   FREESBIEBASEDIR=/usr/local/livefs
+#   LOCALDIR=/home/pfSense/freesbie
+#   PATHISO=/home/pfSense/freesbie/FreeSBIE.iso
+. ../../freesbie/config.sh
+. ../../freesbie/.common.sh
+
+# Suck in script helper functions
+. ./builder_common.sh
+
+# This is the base working directory for all builder
+# operations
+BASE_DIR=/home/pfSense
+
+# This is the directory where the latest pfSense cvs co 
+# is checked out to.
+CVS_CO_DIR=$BASE_DIR/pfSense
+
+# This is the user that has access to the pfSense repo
+CVS_USER=sullrich
+
+# Checkout pfSense information and set our version variables.
+cd $BASE_DIR && cvs -d:ext:$CVS_USER@216.135.66.16:/cvsroot co pfSense >/dev/null
+
+# Calculate versions
+version_kernel=`cat $CVS_CO_DIR/etc/version_kernel`
+version_base=`cat $CVS_CO_DIR/etc/version_base`
+version=`cat $CVS_CO_DIR/etc/version`
+
+cd $LOCALDIR 
+
+$LOCALDIR/0.rmdir.sh
+
+$LOCALDIR/1.mkdir.sh
+
+$LOCALDIR/2.buildworld.sh		# This can be remarked out if completed
+					# prior to this build
+$LOCALDIR/3.installworld.sh
+
+$LOCALDIR/4.kernel.sh pfSense.6
+
+$LOCALDIR/5.patchfiles.sh
+
+$LOCALDIR/6.packages.sh
+
+$LOCALDIR/7.customuser.sh
+
+# Add extra files such as buildtime of version, bsnmpd, etc.
+populate_extra
+set_image_as_cdrom
+create_pfSense_tarball
+copy_pfSesne_tarball_to_custom_directory
+
+$LOCALDIR/8.preparefs.sh
+
+$LOCALDIR/81.mkiso.sh
+
+
+
+
