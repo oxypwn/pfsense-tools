ca04d8034805dd9b069d9c4f86a984a5d9b2b25b
Scott Ullrich <sullrich@pfsense.org>
2005-07-01 19:14:13 +0000

    * Kill syslogd before updating * Recreate the clog files * Restart syslogd

--- cvs_sync.sh
+++ cvs_sync.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-# cvs_sync
-# Part of the pfSense project
-# For users of cvs accounts to keep your test machine
-# up to date
+#  cvs_sync
+#  Written by Scott Ullrich
+#  (C)2005 Scott Ullrich
+#  Part of the pfSense project
+#  For users of cvs accounts to keep your test machine
+#  up to date
 
+# **** SET THIS! ****
 #CVSUSER=sullrich
+
+# Temporary directory where we will check out to.
 TMPDIR=/tmp/pfSense/
+
+/sbin/killall syslogd
 
 if [ ! -r $CVSUSER ]; then
     CVSUSER=`whoami`
@@ -45,3 +52,12 @@ do
         DIR=`echo $FILE | cut -d/ -f2`
         cd $TMPDIR && cp -P -R $TMPDIR$DIR/* /$DIR/
 done
+
+clog -i -s 10000 /var/log/system.log
+clog -i -s 10000 /var/log/filter.log
+clog -i -s 10000 /var/log/dhcpd.log
+clog -i -s 10000 /var/log/vpn.log
+clog -i -s 10000 /var/log/portalauth.log
+
+/usr/sbin/syslogd
+/bin/chmod 0600 /var/log/system.log /var/log/filter.log /var/log/dhcpd.log /var/log/vpn.log /var/log/portalauth.log
