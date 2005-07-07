380ee19432f71c65fc27c6dbddd4e8b5eab55cc2
Colin Smith <colin@pfsense.org>
2005-07-07 06:07:59 +0000

    * Clean directory of manifest_* before each run. * Take command argument specifying version. * Compress manifests.

--- builder_scripts/genmanifest.sh
+++ builder_scripts/genmanifest.sh
@@ -9,10 +9,20 @@ make_manifest() {
 	/usr/sbin/mtree -X exclude.list -c -k cksum,md5digest -p $1 > manifest_$SUFFIX
 }
 
+echo -n "Deleting old manifests... "
+rm manifest_*
+echo "done."
+
 echo -n "Creating manifest for /etc/*... "
 make_manifest pfSense/etc
 echo "done."
 
 echo -n "Creating manifest for /usr/local/www/*... "
 make_manifest pfSense/usr/local/www
+echo "done."
+
+echo -n "Adding manifests to tarfile... "
+tar rf pfSense-Firmware-Manifest-$1.tar manifest_*
+gzip pfSense-Firmware-Manifest-$1.tar
+mv pfSense-Firmware-Manifest-$1.tar.gz pfSense-Firmware-Manifest-$1.tgz
 echo "done."
