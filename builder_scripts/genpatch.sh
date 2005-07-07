76f472fffc8e16097102b34eef93b61e0e7ae100
Colin Smith <colin@pfsense.org>
2005-07-07 18:49:30 +0000

    * Clean up. * diff/mtree /usr/local/etc * Create full firmware patches.

--- builder_scripts/genpatch.sh
+++ builder_scripts/genpatch.sh
@@ -9,32 +9,27 @@ DIFFTIME=`cvs -d /cvsroot/ log pfSense/etc/version | grep 'date:' | cut -d ';' -
 NEWVER=`cat pfSense/etc/version`
 
 make_diff() {
+	echo -n "Diffing /$3*... "
         cvs -d /cvsroot/ diff -u -D "$1 $2" $CVS_CO_DIR/$3 2> /dev/null >> pfsense_update.patch
+	echo "done."
 }
 
 make_manifest() {
+	echo -n "Creating manifest for /$1*... "
 	/usr/sbin/mtree -X exclude.list -c -k cksum,md5digest -p $CVS_CO_DIR/$1 > $2
+	echo "done."
 }
 
 echo -n "Deleting old files... "
 rm pfsense_update.patch manifest_* 2> /dev/null
 echo "done."
 
-echo -n "Diffing /etc/*... "
 make_diff $DIFFTIME etc/
-echo "done."
-
-echo -n "Creating manifest for /etc/*... "
 make_manifest etc/ manifest_etc
-echo "done."
-
-echo -n "Diffing /usr/local/www/*... "
+make_diff $DIFFTIME usr/local/etc/
+make_manifest usr/local/etc/ manifest_usr.local.etc
 make_diff $DIFFTIME usr/local/www/
-echo "done."
-
-echo -n "Creating manifest for /usr/local/www/*... "
 make_manifest usr/local/www manifest_usr.local.www
-echo "done."
 
 echo -n "Creating patch tgz... "
 tar czf pfSense-Diff-$CATEGORY-Update-$NEWVER.tgz pfsense_update.patch manifest_*
@@ -42,4 +37,8 @@ echo "done."
 
 echo -n "Cleaning up... "
 rm pfsense_update.patch manifest_* 2> /dev/null
+echo "done."
+
+echo -n "Creating full patch... "
+tar czf pfSense-Full-$CATEGORY-Update-$NEWVER.tgz $CVS_CO_DIR/etc/* $CVS_CO_DIR/usr/local/www/* $CVS_CO_DIR/usr/local/etc/* 2> /dev/null
 echo "done."
