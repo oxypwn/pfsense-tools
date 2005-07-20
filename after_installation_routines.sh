5df89eae334784d10362656fe5f6380c8a447c86
Scott Ullrich <sullrich@pfsense.org>
2005-07-20 23:14:37 +0000

    Cleanup after installation tasks

--- after_installation_routines.sh
+++ after_installation_routines.sh
@@ -7,7 +7,6 @@ rm -rf /FreeSBIE/mnt/cloop
 rm -rf /FreeSBIE/mnt/dist
 rm -f /FreeSBIE/mnt/etc/rc.local
 rm /FreeSBIE/mnt/root/.tcshrc
-rm /FreeSBIE/mnt/root/.message*
 rm /FreeSBIE/mnt/etc/rc.conf
 touch /FreeSBIE/mnt/etc/rc.conf
 
@@ -20,14 +19,6 @@ rm /FreeSBIE/mnt/etc/rc.sendmail
 # target installation area.
 mkdir -p /FreeSBIE/mnt/cf/conf
 cp /cf/conf/* /FreeSBIE/mnt/cf/conf/
-cp /cf/conf.default/* /FreeSBIE/mnt/conf.default/
-
-# Chroot in and create link
-echo "#!/bin/sh" >> /FreeSBIE/mnt/script
-echo "/bin/ln -s /cf/conf /conf" >> /FreeSBIE/mnt/script
-chmod a+rx /FreeSBIE/mnt/script
-chroot /FreeSBIE/mnt/ /bin/sh /FreeSBIE/mnt/script
-rm /FreeSBIE/mnt/script
 
 # Prevent the system from asking for these twice
 touch /FreeSBIE/mnt/root/.part_mount
@@ -42,7 +33,7 @@ cd /FreeSBIE/mnt && bzcat /FreeSBIE/dist/FreeSBIE.root.dist.bz2 | mtree -PU -p /
 cd /FreeSBIE/mnt/usr && bzcat /FreeSBIE/dist/FreeSBIE.usr.dist.bz2 | mtree -PUr -p /FreeSBIE/mnt/usr
 cd /FreeSBIE/mnt/var && bzcat /FreeSBIE/dist/FreeSBIE.var.dist.bz2 | mtree -PUr -p /FreeSBIE/mnt/var
 
-cd /FreeSBIE/mnt && rm FreeSBIE/ cloop/ dist/ boot/mfsroot.gz
+cd /FreeSBIE/mnt && rm -rf FreeSBIE/ cloop/ dist/ boot/mfsroot.gz
 
 rm /FreeSBIE/mnt/etc/motd
 
@@ -50,7 +41,6 @@ rm /FreeSBIE/mnt/etc/motd
 # from running
 echo "pfSense" > /FreeSBIE/mnt/etc/platform
 
-chroot /FreeSBIE/mnt/ ln -s /config.xml /cf/conf/config.xml
 # Self destruct myself.
 rm -f /FreeSBIE/mnt/usr/local/bin/after_installation_routines.sh
 
