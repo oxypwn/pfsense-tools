7c4a0f03b8bed1de8519d4c3679c7aff665e6f21
Scott Ullrich <sullrich@pfsense.org>
2005-07-20 21:43:55 +0000

    Don't forget the ignore directive

--- installer/conf/uinavctl.lua
+++ installer/conf/uinavctl.lua
@@ -41,8 +41,8 @@ return {
 
 	["*/configure/*"] = "ignore", 		  -- do not configure, we've already did it.
 
-	["*/pit/configure_console"],
+	["*/pit/configure_console"] = "ignore",,
 
-	["*/install/finished"],
+	["*/install/finished"] = "ignore",
 
 }
