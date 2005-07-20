28ef9fe04dc9e88e3c3d1d8146e186bd27272566
Scott Ullrich <sullrich@pfsense.org>
2005-07-20 22:36:11 +0000

    Do not warn the user about ommiting partitions

--- installer/conf/prefs.lua
+++ installer/conf/prefs.lua
@@ -1,18 +1,23 @@
--- $Id$
 
 -- Default configuration file for general information and parameterization
 -- of the BSD Installer.  This configuration file should return a table,
 -- with each key set to its own type of value.
 
 return {
-	--
-	-- Name of the install media in use.
-	--
-	media_name		= "pfSense",
+        --
+        -- Name of the install media in use.
+        --
+        media_name              = "LiveCD",
 
-	--
-	-- Whether crashdumps (to a suitable swap partition) will be
-	-- enabled upon installation, or not.
-	--
-	enable_crashdumps	= true,
+        --
+        -- Whether crashdumps (to a suitable swap partition) will be
+        -- enabled upon installation, or not.
+        --
+        enable_crashdumps       = true,
+
+        --
+        -- Whether the user should be warned about the ramifications
+        -- of omitting certain mountpoints such as /tmp, /usr, &c.
+        --
+        warn_omit_subpartitions = true,
 }
