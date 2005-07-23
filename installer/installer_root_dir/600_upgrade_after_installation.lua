7ac7981dc8850714e3d69c6fd336c292667b7b67
Scott Ullrich <sullrich@pfsense.org>
2005-07-23 02:34:57 +0000

    Break twice after first sentence

--- installer/installer_root_dir/600_upgrade_after_installation.lua
+++ installer/installer_root_dir/600_upgrade_after_installation.lua
@@ -99,7 +99,7 @@ return {
         end        
 	-- success!
 	App.ui:inform(
-	    _("pfSense has been installed successfully!" ..
+	    _("pfSense has been installed successfully!\n\n" ..
 	      "After the reboot surf into 192.168.1.1 " ..
 	      "with the username admin and the password " ..
 	      "pfsense."))
