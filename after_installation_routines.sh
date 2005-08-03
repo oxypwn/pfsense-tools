139c4d752656de9e92e611fd0d26df7ba173efc6
Scott Ullrich <sullrich@pfsense.org>
2005-08-03 02:05:01 +0000

    Create /conf symlink after install

--- after_installation_routines.sh
+++ after_installation_routines.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
 # Lets cleanup from fake root environment
-rm -f /FreeSBIE/mnt/etc/rc.d/freesbie_1st
-rm -f /FreeSBIE/mnt/usr/local/share/freesbie/files/000.freesbie_2nd.sh
 rm -rf /FreeSBIE/mnt/cloop
 rm -rf /FreeSBIE/mnt/dist
+rm -f /FreeSBIE/mnt/etc/rc.d/freesbie_1st
+rm -f /FreeSBIE/mnt/usr/local/share/freesbie/files/000.freesbie_2nd.sh
 rm -f /FreeSBIE/mnt/etc/rc.local
-rm /FreeSBIE/mnt/root/.tcshrc
-rm /FreeSBIE/mnt/etc/rc.conf
+rm -f /FreeSBIE/mnt/root/.tcshrc
+rm -f /FreeSBIE/mnt/etc/rc.conf
+rm -f /FreeSBIE/mnt/etc/rc.conf
+rm -f /FreeSBIE/mnt/etc/rc.firewall
+rm -f /FreeSBIE/mnt/etc/rc.sendmail
 touch /FreeSBIE/mnt/etc/rc.conf
 
-# Zap some unneeded rc stuff
-rm /FreeSBIE/mnt/etc/rc.conf
-rm /FreeSBIE/mnt/etc/rc.firewall*
-rm /FreeSBIE/mnt/etc/rc.sendmail
-
-# Copy the current running systems config.xml to the
-# target installation area.
+# Copy the current running systems config.xml to the target installation area.
 mkdir -p /FreeSBIE/mnt/cf/conf
 cp /cf/conf/* /FreeSBIE/mnt/cf/conf/
 
@@ -25,20 +22,17 @@ touch /FreeSBIE/mnt/root/.part_mount
 touch /FreeSBIE/mnt/root/.first_time
 
 # Updating boot loader
-cp -R /boot/* /FreeSBIE/mnt/boot/
+#cp -R /boot/* /FreeSBIE/mnt/boot/
 cat /boot/loader.conf | grep -v ^mfsroot > /FreeSBIE/mnt/boot/loader.conf
-
-# Enable permission for playback.
-#   cd /FreeSBIE/mnt && bzcat /FreeSBIE/dist/FreeSBIE.root.dist.bz2 | mtree -PU -p /FreeSBIE/mnt
-#   cd /FreeSBIE/mnt/usr && bzcat /FreeSBIE/dist/FreeSBIE.usr.dist.bz2 | mtree -PUr -p /FreeSBIE/mnt/usr
-#   cd /FreeSBIE/mnt/var && bzcat /FreeSBIE/dist/FreeSBIE.var.dist.bz2 | mtree -PUr -p /FreeSBIE/mnt/var
 
 cd /FreeSBIE/mnt && rm -rf FreeSBIE/ cloop/ dist/ boot/mfsroot.gz
 
-rm /FreeSBIE/mnt/etc/motd
+rm -f /FreeSBIE/mnt/etc/motd
 
-# Set platform back to pfSense to prevent freesbie_1st
-# from running
+chroot /FreeSBIE/mnt ln -s /cf/conf /conf
+chroot /FreeSBIE/mnt ln -s /conf /cf/conf
+
+# Set platform back to pfSense to prevent freesbie_1st from running
 echo "pfSense" > /FreeSBIE/mnt/etc/platform
 
 # Self destruct myself.
