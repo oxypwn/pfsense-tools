c55f9cd5a857ffc5e61229082f099b9cc20a428c
Scott Ullrich <sullrich@pfsense.org>
2005-08-12 05:19:50 +0000

    Full path to kbdcontrol

--- pfPorts/kbdcheck/files/kbdcheck.c
+++ pfPorts/kbdcheck/files/kbdcheck.c
@@ -11,7 +11,7 @@ int main(void) {
 	if ((fd = open("/dev/ukbd0", O_RDONLY)) != -1) {
 		close(fd);
 		printf("Enabling USB keyboard override...");
-		system("kbdcontrol -k /dev/ukbd0 < /dev/console");
+		system("/usr/sbin/kbdcontrol -k /dev/ukbd0 < /dev/console");
 		printf("done.\n");
 	}
 }
