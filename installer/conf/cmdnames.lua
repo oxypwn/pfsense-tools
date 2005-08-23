686fac85674508c75c9285ca6eacd3e13da06ba6
Scott Ullrich <sullrich@pfsense.org>
2005-08-23 23:56:19 +0000

    Use /var/log

--- installer/conf/cmdnames.lua
+++ installer/conf/cmdnames.lua
@@ -91,7 +91,7 @@ local cmd_names = {
 
 	-- These aren't commands, but they're configurable here nonetheless.
 
-	DMESG_BOOT	= "var/run/dmesg.boot",
+	DMESG_BOOT	= "var/log/dmesg.boot",
 	MODULES_DIR	= "modules"
 }
 
