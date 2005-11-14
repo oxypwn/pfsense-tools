5d965a67b2502c3ec6ff56f0ffb769015e65bc4a
Scott Ullrich <sullrich@pfsense.org>
2005-11-14 17:49:08 +0000

    Export all variables

--- builder_scripts/pfsense_local.sh
+++ builder_scripts/pfsense_local.sh
@@ -1,13 +1,13 @@
 # This is the base working directory for all builder
 # operations
-BASE_DIR=/home/pfsense
+export BASE_DIR=/home/pfsense
 
 # This is the directory where the latest pfSense cvs co
 # is checked out to.
-CVS_CO_DIR=$BASE_DIR/pfSense
+export CVS_CO_DIR=$BASE_DIR/pfSense
 
 # This is the user that has access to the pfSense repo
-CVS_USER=sullrich
+export CVS_USER=sullrich
 
 export UPDATESDIR=$BASE_DIR/updates
 
