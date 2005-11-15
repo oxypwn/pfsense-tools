043f6386fdaae5a7a93e1bb416e9db5816baf016
Dario Freni <satu@pfsense.org>
2005-11-15 17:54:26 +0000

    Make all variables overridable by env

--- builder_scripts/pfsense_local.sh
+++ builder_scripts/pfsense_local.sh
@@ -1,30 +1,30 @@
 # This is the base working directory for all builder
 # operations
-export BASE_DIR=/home/pfsense
+export BASE_DIR=${BASE_DIR:-/home/pfsense}
 
-export CUSTOMROOT=${BASE_DIR}/tools/builder_scripts/customroot
+export CUSTOMROOT=${CUSTOMROOT:-${BASE_DIR}/tools/builder_scripts/customroot}
 
 # This is the directory where the latest pfSense cvs co
 # is checked out to.
-export CVS_CO_DIR=$BASE_DIR/pfSense
+export CVS_CO_DIR=${CVS_CO_DIR:-${BASE_DIR}/pfSense}
 
 # This is the user that has access to the pfSense repo
-export CVS_USER=sullrich
+export CVS_USER=${CVS_USER:-sullrich}
 
 # pfSense repo IP address. Tipically cvs.pfsense.org,
 # but somebody could use a ssh tunnel and specify
 # a different one
-export CVS_IP=cvs.pfsense.org
+export CVS_IP=${CVS_IP:-cvs.pfsense.org}
 
-export UPDATESDIR=$BASE_DIR/updates
+export UPDATESDIR=${UPDATESDIR:-$BASE_DIR/updates}
 
-export PFSENSEBASEDIR=/usr/local/pfsense-fs
+export PFSENSEBASEDIR=${PFSENSEBASEDIR:-/usr/local/pfsense-fs}
 
-export PFSENSEISODIR=/usr/local/pfsense-clone
+export PFSENSEISODIR=${PFSENSEISODIR:-/usr/local/pfsense-clone}
 
-export SRCDIR=/usr/src
-export FREESBIE_CONF=$PWD/conf/pfsense-freesbie.conf
-export FREESBIE_PATH=/home/satu/cvs/freesbie2
-export KERNELCONF=$PWD/conf/pfSense.6
-export MAKE_CONF=$PWD/conf/make.conf
-export MAKEOBJDIRPREFIX=/usr/obj.pfSense
+export SRCDIR=${SRCDIR:-/usr/src}
+export FREESBIE_CONF=${FREESBIE_CONF:-${PWD}/conf/pfsense-freesbie.conf}
+export FREESBIE_PATH=${FREESBIE_PATH:-/home/pfsense/freesbie2}
+export KERNELCONF=${KERNELCONF:-${PWD}/conf/pfSense.6}
+export MAKE_CONF=${MAKE_CONF:-$PWD/conf/make.conf}
+export MAKEOBJDIRPREFIX=${MAKEOBJDIRPREFIX:-/usr/obj.pfSense}
