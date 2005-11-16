14124563c04d98b242628c6a039b78b445f47192
Dario Freni <satu@pfsense.org>
2005-11-16 01:23:40 +0000

    Add a qemu wrapper script and an init script for network interfaces

---
+++ builder_scripts/qemu/launch-bridge.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -x
+IFNAME="bridge0"
+EMUNAME="$1"
+
+if ! kldstat -v | grep -q if_tap; 
+then
+        if kldload if_tap; then
+                echo "if_tap module loaded"
+        else
+                echo "if_tap module failed to load"
+        fi
+fi
+
+ifconfig -l | grep $IFNAME
+
+if [ $? = 0 ]; then
+	if [ "$EMUNAME" = "tap0" ]; then
+		ifconfig bridge0 destroy
+		ifconfig bridge0 create
+	fi
+else 
+	ifconfig bridge0 create
+fi
+
+ifconfig bridge0 addm $EMUNAME stp $EMUNAME 
+ifconfig bridge0 up
---
+++ builder_scripts/qemu/qemu.sh
@@ -0,0 +1,1 @@
+qemu -cdrom /usr/obj.pfSense/pfSense.iso -boot c -nics 2 -n launch-bridge.sh -localtime -parallel pty -nographic 
