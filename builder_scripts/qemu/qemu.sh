a0f68ea953d2f8a4ba60cbbdfbbae83e60a910be
Dario Freni <satu@pfsense.org>
2005-11-19 14:18:59 +0000

    Use qemu-img instead of dd.
    Noticed by: Maledictus on IRC

--- builder_scripts/qemu/qemu.sh
+++ builder_scripts/qemu/qemu.sh
@@ -6,7 +6,7 @@ set -u
 ISO_IMAGE="/usr/obj.pfSense/pfSense.iso"
 IMG_PREFIX="harddisk-"
 
-DD_ARG_SIZE="bs=1k count=10k"
+IMG_SIZE="10M"
 
 INSTANCE_ID="$1"
 
@@ -14,7 +14,7 @@ INSTANCE_ID="$1"
 macaddr=`printf ${INSTANCE_ID} | md5 | awk '{for(i=0;i<6;i++){ a=a "" substr($1,2*i,2); if(i!=5) a=a ":" }}END{print a;}'`
 
 [ ! -f "${IMG_PREFIX}${INSTANCE_ID}" ] && \
-  dd if=/dev/zero of=${IMG_PREFIX}${INSTANCE_ID} ${DD_ARG_SIZE}
+  qemu-img create ${IMG_PREFIX}${INSTANCE_ID} ${IMG_SIZE}
 
 qemu -hda ${IMG_PREFIX}${INSTANCE_ID} -cdrom ${ISO_IMAGE} \
   -boot d -nics 2 -n launch-bridge.sh -macaddr ${macaddr} \
