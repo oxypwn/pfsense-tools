fe616945830bd2346b1a1d92df101fea14c38593
Scott Ullrich <sullrich@pfsense.org>
2006-02-04 07:56:16 +0000

    Rework developers iso

--- builder_scripts/build_deviso.sh
+++ builder_scripts/build_deviso.sh
@@ -35,7 +35,62 @@ if [ -f "${objdir}/${build_id_w}.world.done" ]; then
     rm -rf /usr/obj*
 fi
 
-sh -x ./build_iso.sh
+#!/bin/sh
+
+# pfSense master builder script
+# (C)2005 Scott Ullrich and the pfSense project
+# All rights reserved.
+#
+# $Id$
+
+set -e -u
+
+# Suck in local vars
+. ./pfsense_local.sh
+
+# Suck in script helper functions
+. ./builder_common.sh
+
+# Use pfSense.6 as kernel configuration file
+export KERNELCONF=${KERNELCONF:-"${PWD}/conf/pfSense.6"}
+
+# Add etcmfs and rootmfs to the EXTRA plugins used by freesbie2
+export EXTRA="${EXTRA:-} rootmfs varmfs etcmfs" 
+
+# Clean out directories
+freesbie_make cleandir
+
+# Checkout a fresh copy from pfsense cvs depot
+update_cvs_depot
+
+# Calculate versions
+export version_kernel=`cat $CVS_CO_DIR/etc/version_kernel`
+export version_base=`cat $CVS_CO_DIR/etc/version_base`
+export version=`cat $CVS_CO_DIR/etc/version`
+
+# Build if needed and install world and kernel
+make_world_kernel
+
+# Add extra files such as buildtime of version, bsnmpd, etc.
+echo ">>> Phase populate_extra"
+( populate_extra )
+echo ">>> Phase set_image_as_cdrom"
+( set_image_as_cdrom )
+
+# Nuke the boot directory
+[ -d "${CVS_CO_DIR}/boot" ] && rm -rf ${CVS_CO_DIR}/boot
+
+rm -f conf/packages
+
+set +e # grep could fail
+(cd /var/db/pkg && ls | grep bsdinstaller) > conf/packages
+(cd /var/db/pkg && ls | grep lighttpd) >> conf/packages
+(cd /var/db/pkg && ls | grep lua) >> conf/packages
+(cd /var/db/pkg && ls | grep cpdup) >> conf/packages
+set -e
+
+# Invoke FreeSBIE2 toolchain
+freesbie_make iso
 
 # Re-remove lockfiles after build
 rm -f ${objdir}/${build_id_w}.world.done ${objdir}/${build_id_k}.kernel.done
