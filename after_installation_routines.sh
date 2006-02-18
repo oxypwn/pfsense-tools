184c524b8335c82aa9c6ced47e5f68d46700c782
Scott Ullrich <sullrich@pfsense.org>
2006-02-18 20:30:18 +0000

    Detect when we are installing to VMWARE and fixup files

--- after_installation_routines.sh
+++ after_installation_routines.sh
@@ -56,10 +56,13 @@ cp /tmp/new.fdisk /mnt/var/installer_logs
 # Make sure malloc.conf is not set for debugging
 # and increase the pages to 32
 rm /etc/malloc.conf
-ln -s "aj>" /etc/malloc.conf
+ln -s "aj" /etc/malloc.conf
 
 mkdir -p /mnt/var/db/pkg
 cd /var/db/pkg ; tar -cpf - . | (cd /mnt/var/db/pkg ; tar -xpf -)
+
+# If the platform is vmware, lets do some fixups.
+if [ -e /var/IS_VMWARE ]; then; echo "kern.timecounter.hardware=i8254" >> /mnt/etc/sysctl.conf; echo hint.apic.0.disabled="1" >> /mnt/boot/loader.conf; echo kern.hz="100" >> /mnt/boot/loader.conf; fi;
 
 #Sync disks
 /bin/sync
