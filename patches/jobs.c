c6ddc8eaccbda71d12b4b019e1e11fc861bd6773
Scott Ullrich <sullrich@pfsense.org>
2006-03-13 07:01:57 +0000

    Patch /bin/sh to detect famous job control bug and bail.

---
+++ patches/jobs.c
@@ -0,0 +1,13 @@
+--- jobs.c	Thu Jan 12 05:24:46 2006
++++ jobs.c.sku	Mon Mar 13 06:58:09 2006
+@@ -148,7 +148,9 @@
+ 			if (initialpgrp < 0) {
+ out:				out2str("sh: can't access tty; job control turned off\n");
+ 				mflag = 0;
+-				return;
++				out2str("We are sorry.  We need to logout now, please type 0 at the prompt");
++				system("touch /tmp/ttybug");
++				exit;
+ 			}
+ 			if (initialpgrp == -1)
+ 				initialpgrp = getpgrp();
