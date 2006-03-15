d3d28087bc34c50797c21c50d1f4e9b5c6a346b0
Scott Ullrich <sullrich@pfsense.org>
2006-03-15 21:51:35 +0000

    Add /tmp/check_reload_status status indicator

--- pfPorts/check_reload_status/files/check_reload_status.c
+++ pfPorts/check_reload_status/files/check_reload_status.c
@@ -49,6 +49,7 @@ int main(void) {
 	FILE *f;
 	cycle_time = CYCLE;
 	/* daemonize */
+	system("echo starting > /tmp/check_reload_status");
 	if( fork() == 0 ) {
 	  /* close stdin and stderr */
 	  fclose( stdin );
@@ -56,10 +57,12 @@ int main(void) {
 	  /* loop forever until the cows come home */
 	  while(1) {
 	      if(fexist("/tmp/restart_webgui") == 1) {
+			  system("echo /tmp/restartwebgui > /tmp/check_reload_status");
 		      system("/bin/rm /tmp/restart_webgui");
 		      system("/usr/bin/nice -n20 /etc/rc.restart_webgui");
 	      }
 	      if(fexist("/tmp/rc.linkup") == 1) {
+			  system("echo /tmp/rc.linkup > /tmp/check_reload_status");
 		      char buf[FILENAME_MAX + 2];
 		      if (!(f = fopen("/tmp/rc.linkup", "r"))) {
 			      fprintf(stderr, "Could not open /tmp/rc.linkup for input.\n");
@@ -73,6 +76,7 @@ int main(void) {
 		      system(temp);
 	      }		  
 	      if(fexist("/tmp/rc.newwanip") == 1) {
+			  system("echo /tmp/rc.newwanip > /tmp/check_reload_status");
 		      char buf[FILENAME_MAX + 2];
 		      if (!(f = fopen("/tmp/rc.newwanip", "r"))) {
 			      fprintf(stderr, "Could not open /tmp/rc.newwanip for input.\n");
@@ -90,35 +94,39 @@ int main(void) {
 		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.filter_configure >/dev/null");
 	      }
 	      if(fexist("/tmp/reload_all") == 1) {
+			  system("echo /tmp/reload_all > /tmp/check_reload_status");
 		      system("/bin/rm /tmp/reload_all");
 		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.reload_all >/dev/null");
 	      }
 	      if(fexist("/tmp/reload_interfaces") == 1) {
+			  system("echo /tmp/reload_interfaces > /tmp/check_reload_status");
 		      system("/bin/rm /tmp/reload_interfaces");
 		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.reload_interfaces >/dev/null");
 	      }
 	      if(fexist("/tmp/update_dyndns") == 1) {
+			  system("echo /tmp/update_dyndns > /tmp/check_reload_status");
 		      system("/bin/rm /tmp/update_dyndns");
 		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.dyndns.update");
 	      }
 	      if(fexist("/tmp/interfaces_wan_configure") == 1) {
+			  system("echo /tmp/interfaces_wan_configure > /tmp/check_reload_status");
 		      system("/bin/rm  /tmp/interfaces_wan_configure");
 		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/interfaces_wan_configure");
 	      }
-	      if(fexist("/tmp/interfaces_wan_configure") == 1) {
-		      system("/bin/rm /tmp/interfaces_wan_configure");
-		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/interfaces_wan_configure");
-	      }
 	      if(fexist("/tmp/interfaces_opt_configure") == 1) {
+			  system("echo /tmp/interfaces_opt_configure > /tmp/check_reload_status");
 		      system("/bin/rm /tmp/interfaces_opt_configure");
 		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/interfaces_opt_configure");
 	      }
 	      if(fexist("/tmp/start_sshd") == 1) {
+			  system("echo /tmp/start_sshd > /tmp/check_reload_status");
 		      system("/bin/rm /tmp/start_sshd");
 		      system("/usr/bin/nice -n20 /etc/sshd");
-	      }		  
+	      }
+		  system("echo sleeping > /tmp/check_reload_status");
 	      sleep( cycle_time );
 	  }
+	  system("echo exiting > /tmp/check_reload_status");
 	} else {
 	      /* Exit parent process */
 	      if( signal( SIGINT, SIG_DFL ) != SIG_DFL )
