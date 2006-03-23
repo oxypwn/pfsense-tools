3e140fca8b112fa3eaa4e5f5836ae647c8dfcd20
Scott Ullrich <sullrich@pfsense.org>
2006-03-23 02:05:50 +0000

    * Add header and cleanup * Add right column textarea

--- CoreGUIBuilder/index.php
+++ CoreGUIBuilder/index.php
@@ -27,6 +27,11 @@
 	POSSIBILITY OF SUCH DAMAGE.
 */
 
+if($_POST['myparam']) {
+	echo $_POST['myparam'];
+	exit;
+}
+
 require("guiconfig.inc");
 
 $pgtitle = "CoreGUIBuilder";
@@ -43,12 +48,11 @@ include("head.inc");
 		  background-color:#990000;
 		  padding:8px;
 		  width:98%;
-		  z-order:100;
 		}			
 		div.toolboxborder {
 			position: absolute;
-			top: 89px;
-			left: 450px;
+			top: 69px;
+			left: 460px;
 		  border:1px solid #eeeeee;
 		  background-color:white;
 		  padding:8px;
@@ -58,8 +62,8 @@ include("head.inc");
 		  border:1px solid #990000;
 		  background-color:white;
 		  padding:8px;
-		  width:400px;
-		  height:500px;
+		  width:420px;
+		  height:900px;
 		}		
 		span.title {
 		  margin:0;
@@ -173,7 +177,7 @@ include("head.inc");
 
 
 <div id="formcanvas" class="formcanvas" style="clear:left; height:500px;margin-top:10px;">
-	Drag items to create form:
+	<b>Drag items to create form:</b><p>
 	<table width="100%" border="0" name="formcanvas_table" id="formcanvas_table">
 		<tbody name="formcanvas_tbody" id="formcanvas_tbody">
 		</tbody>
@@ -184,29 +188,31 @@ include("head.inc");
 
 <div id="toolbox" name="toolbox" class="toolboxborder">
 	<font color="black">
-	Toolbox<p>
+	<b>Toolbox</b><p>
 	<div onDblClick="OnDropForm('textarea',  0)" class="toolbox" name="textarea" id="textarea">Textarea<br><textarea name="textarea_control"></textarea></div>
 	<br>
 	<div onDblClick="OnDropForm('input',     0)" class="toolbox" name="input" id="input">Input<br><input name="input_control"></div>
 	<br>
-
-	<div onDblClick="OnDropForm('password',  0)" class="toolbox" name="input" id="input">Password<br><input name="password"></div>
+	<div onDblClick="OnDropForm('password',  0)" class="toolbox" name="password" id="password">Password<br><input name="password"></div>
 	<br>
-
 	<div onDblClick="OnDropForm('checkbox',  0)" class="toolbox" name="checkbox" id="checkbox">Checkbox<br><input type="checkbox" name="checkbox"></div>
 	<br>
 	<div onDblClick="OnDropForm('select',    0)" class="toolbox" name="select" id="select">Select box<br><select><option>OPTION1</option></select></div>
 	<br>
 	<div onDblClick="OnDropForm('interfaces_select', 0)" class="toolbox" name="interfaces_select" id="interfaces_select">Interfaces selection<br><select><option>WAN</option><option>LAN</option></select></div>
+	<br>
+	<div onDblClick="OnDropForm('headerbar', 0)" class="toolbox" name="headerbar" id="headerbar" style="background-color:#990000"><font color="white">Header</div>
+	<br>
 </div>
 
 <script type="text/javascript">
 	/* init the draggables */
-	
 	new Draggable('textarea',			{revert:true})
 	new Draggable('input',				{revert:true})
+	new Draggable('password',			{revert:true})
 	new Draggable('checkbox',			{revert:true})
 	new Draggable('select',   			{revert:true})
+	new Draggable('headerbar', 			{revert:true})
 	new Draggable('interfaces_select',	{revert:true})
 	/* init the droppable */
 	Droppables.add('formcanvas', {
@@ -226,10 +232,12 @@ include("head.inc");
      *  variables, such as field name, caption,
      *  etc.
      */
-	function FORM_ELEMENTS(field_name, left_caption, right_caption) {
+	function FORM_ELEMENTS(field_name, left_caption, right_caption, element_id) {
 		this.field_name = field_name;
+		this.element_id = element_id;
 		this.left_caption = left_caption;
 		this.right_caption = right_caption;
+		
 	}
 
 	/* setup an array that holds the item info such as text description, etc */
@@ -238,42 +246,61 @@ include("head.inc");
 	function OnDropForm(element_id, positionY, field) {
 		var table = document.getElementById("formcanvas_table");
 		var tbody = document.getElementById('formcanvas_table').getElementsByTagName('tbody')[0];
-		var row = document.createElement('tr');
-		row.setAttribute("id", "formcanvas_row_" + form_elements);
-		var cell1 = document.createElement('td');
-		var cell2 = document.createElement('td');
-		cell1.innerHTML = 'Click me to edit...';
-		cell1.setAttribute("class", "vncellreq");
-		cell2.innerHTML = newCanvasItem('Test', field, element_id);
-		cell2.setAttribute("class", "vtable");
-		row.appendChild(cell1);
-		row.appendChild(cell2);
-		tbody.appendChild(row);
+		if(element_id == 'headerbar') {
+			var row = document.createElement('tr');
+			row.setAttribute("height", "40");
+			row.setAttribute("id", "formcanvas_row_" + form_elements);
+			var cell1 = document.createElement('td');
+			cell1.setAttribute("colspan", "2");
+			row.setAttribute("bgcolor", "#990000");
+			cell1.innerHTML = '<div style="color:white" name="' + form_elements + '_headerbar" id="' + form_elements + '_headerbar">Click me to edit...</div>';
+			row.appendChild(cell1);
+			tbody.appendChild(row);
+			new Ajax.InPlaceEditor(form_elements + '_headerbar', 'index.php', { callback: function(form, value) { updateHeaderCaption(form.id, value); return '&myparam=' + escape(value) }});
+		} else {
+			var row = document.createElement('tr');
+			row.setAttribute("id", "formcanvas_row_" + form_elements);
+			var cell1 = document.createElement('td');
+			var cell2 = document.createElement('td');
+			cell1.innerHTML = '<p name="' + form_elements + '_left_caption" id="' + form_elements + '_left_caption">Click me to edit...</p>';
+			cell1.setAttribute("class", "vncellreq");
+			cell2.innerHTML = newCanvasItem('Test', field, element_id) + '<p><p name="' + form_elements + '_right_caption" id="' + form_elements + '_right_caption">Click me to edit...</p>';
+			cell2.setAttribute("class", "vtable");
+			row.appendChild(cell1);
+			row.appendChild(cell2);
+			tbody.appendChild(row);
+			new Ajax.InPlaceEditor(form_elements + '_left_caption', 'index.php', { callback: function(form, value) { updateLeftCaption(form.id, value); return '&myparam=' + escape(value) }});
+			new Ajax.InPlaceEditor(form_elements + '_right_caption', 'index.php', { callback: function(form, value) { updateRightCaption(form.id, value); return '&myparam=' + escape(value) }});	
+		}
 		/* create a new javascript object on our form element tracking array */
-		form_elements_properties[form_elements] = new FORM_ELEMENTS( 'field_name', 'Click me to edit...', element_id);
+		form_elements_properties[form_elements] = new FORM_ELEMENTS( 'field_name', 'Click me to edit...', 'Click me to edit...' , element_id);
 		/* make the new element draggable in its container */
 		new Draggable('formcanvas_row_' + form_elements, {revert:false})
-		form_elements++;
+		/* we now have another control.  ++ our controller count */
+		form_elements++;		
 		/* allow the form canvas area to be resortable */
 		Sortable.create('formcanvas_tbody',{dropOnEmpty:true,tag:'tr'});
 	}
-
+	
 	/* convert a field type to beginning html */
 	function newCanvasItem(text, field, element_id) {
 		switch(element_id) {
 			case "textarea":
 				return "<textarea></textarea>";
-			break;
 			case "input":
 				return "<input></input>";
-			break;
+			case "password":
+				return "<input type=\"password\"></input>";
 			case "checkbox":
 				return "<input type='checkbox'></input>";
 			case "select":
 				return "<select><option>OPTION1</option></select>";
 			case "interfaces_select":
 				return "<select><option>WAN</option><option>LAN</option></select>";
-			break;
+			case "headerbar":
+				return "";
+			/* default fallback case */
+			return "";
 		}
 	}
 
@@ -293,13 +320,38 @@ include("head.inc");
 		new Effect.SlideDown('about_screen', {duration:.5});
 	}
 	
-	function closeAboutScreen() {
-		new Effect.SlideUp('about_screen', {duration:.5});
-		$('about_screen').style.visibility="hidden";
+	function show_main_form() {
 		$('formcanvas').style.visibility = 'visible';
-		$('toolbox').style.visibility = 'visible';
+		$('toolbox').style.visibility = 'visible';	
+	}
+	
+	function closeAboutScreen() {
+		new Effect.SlideUp('about_screen', {duration:.5});		
+		//$('about_screen').style.visibility="hidden";
+        window.setTimeout('show_main_form()', 700);
 		return false;
 	}	
+
+	/* callback used after in place editing for the left caption area */
+	function updateHeaderCaption(formid, value) {
+		var formid_split = formid.split("_");
+		var id = formid_split[0];
+		form_elements_properties[id].left_caption = value;
+	}
+	
+	/* callback used after in place editing for the left caption area */
+	function updateLeftCaption(formid, value) {
+		var formid_split = formid.split("_");
+		var id = formid_split[0];
+		form_elements_properties[id].left_caption = value;
+	}
+
+	/* callback used after in place editing for the right caption area */
+	function updateRightCaption(formid, value) {
+		var formid_split = formid.split("_");
+		var id = formid_split[0];
+		form_elements_properties[id].right_caption = value;
+	}
 	
 	/* expand about screen on bootup */
 	expandAboutScreen();
@@ -308,7 +360,12 @@ include("head.inc");
 
 <br>
 
+<b>NOTE:</b> "Click me to edit..." will be stripped out when the form is exported to XML.
+
 <?php include("fend.inc"); ?>
 
 </body>
 </html>
+
+
+
