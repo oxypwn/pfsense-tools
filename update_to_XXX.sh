70f82dead6ba9b0af8ac5671409c82ab0c82dd5d
Scott Ullrich <sullrich@pfsense.org>
2006-08-19 07:04:43 +0000

    * Modularize script and make it easier to read * Add (C) * General cleanups

--- update_to_XXX.sh
+++ update_to_XXX.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+#set -e -x
+
+# pfSense upgrade script
+# (C)2006 Scott Ullrich
+# All rights reserved.
+
+# This script will aide in upgrading
+# to a newer pfSense version
+
 # -- SETABLE VARIABLES
 # Previous version
 PREVIOUS_VERSION="RC2"
 # New version
 TARGET_VERSION="RC2f"
 # Set to 1 to force a kernel update and reboot
-KERNEL_UPDATE_NEEDED=0
+KERNEL_UPDATE_NEEDED=1
 # Set to 1 if the ruleset will change after update
-RULESET_CHANGES=0
+RULESET_CHANGES=1
 # Where will the updates be stored
 PATH_TO_UPDATE="http://www.pfsense.com/~sullrich"
 
@@ -24,7 +33,8 @@ VERSION=`cat /etc/version`
 #    ${PATH_TO_UPDATE}/${TARGET_VERSION}_${PLATFORM}_kernel.tgz
 
 restore_backups() {
-	echo "Something bad happened.  Aborting."
+	echo
+	echo "*** Something bad happened.  Aborting!"
 	echo
 	echo "Restoring backup..."
 	/etc/rc.conf_mount_rw
@@ -43,108 +53,157 @@ restore_backups() {
 	exit
 }
 
-if [ $VERSION != $PREVIOUS_VERSION ]; then
-    echo "This upgrades version $PREVIOUS_VERSION only."
-    exit
-else
-    echo "$PREVIOUS_VERSION detected.  Beginning update."
-fi
-
-if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
-    echo
-    echo "NOTE!  This upgrade will reboot pfSense after completion!"
-    echo
-    echo -n "CTRL-C now if this is a problem.  Upgrade will start in 10 seconds."
-    sleep 5
-    echo -n "."
-    sleep 5
-    echo "."
-    echo
-fi
-
-echo "One moment, we are going to upgrade to ${TARGET_VERSION}..."
-echo -n "Making sure we are rw... "
-/etc/rc.conf_mount_rw
-PREVIOUS_CHECKSUM=`md5 /tmp/rules.debug`
-echo "done"
-
-echo "Backing up the files before we upgrade..."
-(cd / && fetch -q -o - ${PATH_TO_UPDATE}/$TARGET_VERSION.tgz \
-	| tar tvzpf - | awk '{ print $9 }' | tar czvfp /tmp/backup.tgz -T -)
-if [ $? -ne 0 ]; then
-	echo "ERROR!  Could not create backup.  Exiting."
-	exit
-fi
-if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
-    echo "Kernel update needed for ${PLATFORM} platform, backing up..."
-    (cd / && fetch -q -o - ${PATH_TO_UPDATE}/${TARGET_VERSION}_${PLATFORM}_kernel.tgz \
-        | tar tvzpf - | awk '{ print $9 }' | tar czvfp /tmp/backup_kernel.tgz -T -)
+backup() {
+	echo "Backing up the files before we upgrade..."
+	(cd / && fetch -q -o - ${PATH_TO_UPDATE}/$TARGET_VERSION.tgz \
+		| tar tvzpf - | awk '{ print $9 }' | tar czvfp /tmp/backup.tgz -T -)
 	if [ $? -ne 0 ]; then
 		echo "ERROR!  Could not create backup.  Exiting."
 		exit
 	fi
-fi
+}
 
-echo "Now pulling down the update file, please wait..."
-fetch -q -o - ${PATH_TO_UPDATE}/$TARGET_VERSION.tgz | tar xzvpf - -C /
-if [ $? -ne 0 ]; then
-	restore_backups
-fi
-if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
-    echo "Now pulling down the kernel update file, please wait..."
-    fetch -q -o - ${PATH_TO_UPDATE}/${TARGET_VERSION}_${PLATFORM}_kernel.tgz \
-		| tar xzvpf - -C /
-fi
-if [ $? -ne 0 ]; then
-	restore_backups
-fi
-
-echo -n "Reloading filter... "
-cp /tmp/rules.debug /tmp/rules.debug.before_update
-/etc/rc.filter_configure
-TARGET_CHECKSUM=`md5 /tmp/rules.debug`
-if [ $RULESET_CHANGES -gt 0 ]; then
-		if [ "$PREVIOUS_CHECKSUM" != "$TARGET_CHECKSUM" ]; then
-		    echo " *** rule difference detected. ***"
-		    echo "*** Notice ***  Filter rules appear to be different now!"
-		    echo "                This may be normal if pfSense fixed a filter rule bug."
-		    echo "                If you experience problems, run this command to restore"
-		    echo "                the previous version: tar xzvpf /tmp/backup.tgz -C /"
-		else
-		    echo "done"
+backup_kernel() {
+	if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
+	    echo "Kernel update needed for ${PLATFORM} platform, backing up..."
+	    (cd / && fetch -q -o - ${PATH_TO_UPDATE}/${TARGET_VERSION}_${PLATFORM}_kernel.tgz \
+	        | tar tvzpf - | awk '{ print $9 }' | tar czvfp /tmp/backup_kernel.tgz -T -)
+		if [ $? -ne 0 ]; then
+			echo "ERROR!  Could not create backup.  Exiting."
+			exit
 		fi
+	fi
+}
+
+update() {
+	echo "Now pulling down the update file, please wait..."
+	fetch -q -o - ${PATH_TO_UPDATE}/$TARGET_VERSION.tgz | tar xzvpf - -C /
+	if [ $? -ne 0 ]; then
+		restore_backups
+	fi
+}
+
+update_kernel() {
+	if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
+	    echo "Now pulling down the kernel update file, please wait..."
+	    fetch -q -o - ${PATH_TO_UPDATE}/${TARGET_VERSION}_${PLATFORM}_kernel.tgz \
+			| tar xzvpf - -C /
+	fi
+	if [ $? -ne 0 ]; then
+		restore_backups
+	fi
+}
+
+reload_filter() {
+	echo -n "Reloading filter... "
+	cp /tmp/rules.debug /tmp/rules.debug.before_update
+	/etc/rc.filter_configure
+	TARGET_CHECKSUM=`md5 /tmp/rules.debug`
+	if [ $RULESET_CHANGES -gt 0 ]; then
+			if [ "$PREVIOUS_CHECKSUM" != "$TARGET_CHECKSUM" ]; then
+			    echo "*** rule difference detected. ***"
+			    echo "*** Notice ***  Filter rules appear to be different now!"
+			    echo "                This may be normal if pfSense fixed a filter rule bug."
+			    echo "                If you experience problems, run this command to restore"
+			    echo "                the previous version: tar xzvpf /tmp/backup.tgz -C /"
+			else
+			    echo "done"
+			fi
+		else
+			echo "done"
+	fi
+}
+
+test_filter_status() {
+	pfctl -f /tmp/rules.debug
+	if [ $? -ne 0 ]; then
+		restore_backups
 	else
-		echo "done"
-fi
+	    echo "You are now updated to $TARGET_VERSION"
+	    echo $TARGET_VERSION > /etc/version
+	fi
+}
 
-pfctl -f /tmp/rules.debug
-if [ $? -ne 0 ]; then
-	restore_backups
-else
-    echo "You are now updated to $TARGET_VERSION"
-    echo $TARGET_VERSION > /etc/version
-fi
+rw() {
+	echo -n "Making sure we are rw... "
+	/etc/rc.conf_mount_rw
+	PREVIOUS_CHECKSUM=`md5 /tmp/rules.debug`
+	echo "done"
+}
 
-echo -n "Making sure we are ro... "
-/etc/rc.conf_mount_ro
-echo "done"
-echo -n "Old version: "
-echo $VERSION
-echo -n "New version: "
-cat /etc/version
+ro() {
+	echo -n "Making sure we are ro... "
+	/etc/rc.conf_mount_ro
+	echo "done"
+}
 
-if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
-    echo
-    echo -n "Rebooting in 4.."
-    sleep 1
-    echo -n "3.."
-    sleep 1
-    echo -n "2."
-    sleep 1
-    echo -n "1."
-    sync
-    echo "."
-    sync
-    shutdown -r now
-fi
+check_upgrade_status() {
+	GREPPED=`echo $VERSION | grep $PREVIOUS_VERSION | wc -l`
+	if [ $GREPPED -lt 1 ]; then
+	    echo "This upgrades version $PREVIOUS_VERSION only."
+	    exit
+	else
+	    echo "$PREVIOUS_VERSION detected.  Beginning update."
+	fi
+}
+
+alert_reboot_needed() {
+	if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
+	    echo
+	    echo "NOTE!  This upgrade will reboot pfSense after completion!"
+	    echo
+	    echo -n "CTRL-C now if this is a problem.  Upgrade will start in 10 seconds."
+	    sleep 5
+	    echo -n "."
+	    sleep 5
+	    echo "."
+	    echo
+	    if [ -f /usr/bin/clear ]; then
+	    	/usr/bin/clear
+	    fi
+	fi
+	echo "Beginning upgrade and setting reboot needed flag."
+}
+
+show_version_status() {
+	echo -n "Old version: "
+	echo $VERSION
+	echo -n "New version: "
+	cat /etc/version
+}
+
+reboot_if_needed() {
+	if [ $KERNEL_UPDATE_NEEDED -gt 0 ]; then
+	    echo
+	    echo -n "Rebooting in 4.."
+	    sleep 1
+	    echo -n "3.."
+	    sleep 1
+	    echo -n "2."
+	    sleep 1
+	    echo -n "1."
+	    sync
+	    echo "."
+	    sync
+	    shutdown -r now
+	fi
+}
+
+welcome() {
+	echo "One moment, we are going to upgrade to ${TARGET_VERSION}..."
+}
+
+check_upgrade_status
+welcome
+alert_reboot_needed
+rw
+backup
+backup_kernel
+update
+update_kernel
+reload_filter
+test_filter_status
+ro
+show_version_status
+reboot_if_needed
 
