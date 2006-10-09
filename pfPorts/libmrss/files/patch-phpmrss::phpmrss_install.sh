dcaa22be597e7af630a5deecfc93cea671497630
Daniel Stefan Haischt <dsh@pfsense.org>
2006-10-09 15:57:27 +0000

    Added RSS PECL extension that will be utilized by a diag_logs-rss package.

---
+++ pfPorts/libmrss/files/patch-phpmrss::phpmrss_install.sh
@@ -0,0 +1,8 @@
+--- phpmrss/phpmrss_install.sh.orig	Mon Oct  9 15:34:36 2006
++++ phpmrss/phpmrss_install.sh	Mon Oct  9 15:34:47 2006
+@@ -1,4 +1,4 @@
+-#!/bin/bash
++#!/usr/local/bin/bash
+ 
+ function check {
+   if ! test $1 = "0"; then
