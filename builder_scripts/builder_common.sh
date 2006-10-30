0658021351c4921c1c2f8760a83282fb8df994ce
Scott Ullrich <sullrich@pfsense.org>
2006-10-30 17:38:25 +0000

    Nuke /root directory when creating mini updates

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -138,6 +138,9 @@ create_pfSense_BaseSystem_Small_update_tarball() {
 	rm -f ${CVS_CO_DIR}/etc/pw*
 	rm -f ${CVS_CO_DIR}/etc/ttys
 
+	# Nuke /root/ directory contents
+	rm -rf ${CVS_CO_DIR}/root
+
 	cd ${CVS_CO_DIR} && tar czPf ${UPDATESDIR}/${FILENAME} .
 
 	ls -lah ${UPDATESDIR}/${FILENAME}
