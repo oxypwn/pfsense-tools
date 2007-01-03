f5b133b246f182d128a8548c907924c272ce589e
Scott Ullrich <sullrich@pfsense.org>
2007-01-03 04:59:58 +0000

    Further cleanups.  Add default variable files.

--- snapshot_server/build_snapshots.sh
+++ snapshot_server/build_snapshots.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Ensure directories exist
 mkdir -p /usr/local/www/data/updates
 mkdir -p /usr/local/www/data/iso
 mkdir -p /usr/local/www/data/embedded
@@ -10,11 +11,11 @@ mkdir -p /usr/local/www/data/head/embedded
 rm -rf /usr/obj*
 
 set_source() {
-	echo $1 > /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt
+	echo $1 > /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt
 }
 
 update_sources() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Updating sources and building $CURRENTLY_BUILDING ISO..."
 		cd /home/pfsense/tools/builder_scripts/
 		./cvsup_current
@@ -22,7 +23,7 @@ update_sources() {
 }
 
 build_embedded() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Building embedded $CURRENTLY_BUILDING ..."
 		cd /home/pfsense/tools/builder_scripts/
 		./build_embedded.sh
@@ -31,52 +32,57 @@ build_embedded() {
 }
 
 build_updates() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Building updates..."
 		cd /home/pfsense/tools/builder_scripts/
 		./build_updates.sh
 }
 
 build_iso() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Building ISO..."
 		cd /home/pfsense/tools/builder_scripts/
 		./build_iso.sh
 }
 
 setstatus() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
 		STATUS=$1
 		echo $1
-		uptime >   /usr/local/www/data/status.txt
-		date >>    /usr/local/www/data/status.txt
-		echo $1 >> /usr/local/www/data/status.txt
+		echo "Currently building $CURRENTLY_BUILDING" > /usr/local/www/status.txt
+		uptime  >> /usr/local/www/status.txt
+		date    >> /usr/local/www/status.txt
+		echo $1 >> /usr/local/www/status.txt
 }
 
 while [ /bin/true ]; do
 
 		# -- RELENG_1
+		setstatus "Setting build to -RELENG_1..."
 		set_source "-RELENG_1"
-		CURRENTLY_BUILDING=`cat /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
 		cp /root/pfsense_local_releng_1.sh /home/pfsense/tools/pfsense_local.sh
 
 		update_sources
 		build_updates
-		cp /usr/obj.pfSense/pfSense.iso.gz /usr/local/www/data/iso/
+		cp /usr/obj.pfSense/pfSense.iso.gz /usr/local/www/iso/
 
 		build_embedded
-		cp /usr/obj.pfSense/pfSense.img.gz /usr/local/www/data/embedded/
+		cp /usr/obj.pfSense/pfSense.img.gz /usr/local/www/embedded/
 
 		setstatus "Copying files for -RELENG_1 build..."
-		cp /home/pfsense/updates/* /usr/local/www/data/updates/
+		cp /home/pfsense/updates/* /usr/local/www/updates/
 
 		setstatus "Cleaning up..."
 		rm -rf /usr/obj.pfSense
 
+		setstatus "Cooling down..."
+		sleep 500
+
 		# -- HEAD
-		setstatus "Setting build to -HEAD"
+		setstatus "Setting build to -HEAD..."
 		set_source "-HEAD"
-		CURRENTLY_BUILDING=`cat /usr/local/www/data/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
 		cp /root/pfsense_local_releng_1_head.sh \
 			/home/pfsense/tools/pfsense_local.sh
 
@@ -84,10 +90,11 @@ while [ /bin/true ]; do
 
 		update_sources
 		build_updates
-		cp /usr/obj.pfSense/pfSense.iso.gz /usr/local/www/data/head/iso/
-		cp /home/pfsense/updates/* /usr/local/www/data/head/updates/
+		cp /usr/obj.pfSense/pfSense.iso.gz /usr/local/www/head/iso/
+		cp /home/pfsense/updates/* /usr/local/www/head/updates/
+
 		build_embedded
-		cp /usr/obj.pfSense/pfSense.img.gz /usr/local/www/data/head/embedded/
+		cp /usr/obj.pfSense/pfSense.img.gz /usr/local/www/head/embedded/
 
 		setstatus "Cleaning up..."
 		rm -rf /usr/obj.pfSense
