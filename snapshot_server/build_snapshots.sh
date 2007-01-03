714d5da990ec2c0963ce0bd22f678080b87f562c
Scott Ullrich <sullrich@pfsense.org>
2007-01-03 19:13:03 +0000

    If a directory is used more than once then turn it into a variable for easier maintaining.

--- snapshot_server/build_snapshots.sh
+++ snapshot_server/build_snapshots.sh
@@ -8,49 +8,58 @@ mkdir -p /usr/local/www/data/head/updates
 mkdir -p /usr/local/www/data/head/iso
 mkdir -p /usr/local/www/data/head/embedded
 
+# Local variables that are used by builder scripts
+WEBDATAROOT=/usr/local/www/data
+WEBROOT=/usr/local/www
+TOOLDIR=/home/pfsense/tools
+BUILDERSCRIPTS=/home/pfsense/tools/builder_scripts
+SNAPSHOTSCRIPTSDIR=/root/
+PFSENSEUPDATESDIR=/home/pfsense/updates/
+PFSENSEOBJDIR=/usr/obj.pfSense/
+
 rm -rf /usr/obj*
 
 set_source() {
-	echo $1 > /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt
+	echo $1 > $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt
 }
 
 update_sources() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Updating sources and building $CURRENTLY_BUILDING ISO..."
-		cd /home/pfsense/tools/builder_scripts/
+		cd $BUILDERSCRIPTS
 		./cvsup_current
-		gzip /usr/obj.pfSense/pfSense.iso
+		gzip $PFSENSEOBJDIR/pfSense.iso
 }
 
 build_embedded() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Building embedded $CURRENTLY_BUILDING ..."
-		cd /home/pfsense/tools/builder_scripts/
+		cd $BUILDERSCRIPTS
 		./build_embedded.sh
 		setstatus "Gzipping embedded $CURRENTLY_BUILDING ..."
-		rm /usr/obj.pfSense/pfSense.img.gz
-		gzip /usr/obj.pfSense/pfSense.img		
+		rm $PFSENSEOBJDIR/pfSense.img.gz
+		gzip $PFSENSEOBJDIR/pfSense.img		
 }
 
 build_updates() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Building updates..."
-		cd /home/pfsense/tools/builder_scripts/
+		cd $BUILDERSCRIPTS
 		./build_updates.sh
 }
 
 build_iso() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt`
 		setstatus "Building ISO..."
-		cd /home/pfsense/tools/builder_scripts/
+		cd $BUILDERSCRIPTS
 		./build_iso.sh
 }
 
 setstatus() {
-		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
+		CURRENTLY_BUILDING=`cat $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt`
 		STATUS=$1
 		echo $1
-		echo "Currently building $CURRENTLY_BUILDING" > /usr/local/www/status.txt
+		echo "Currently building $CURRENTLY_BUILDING" > /usr/local/www/data/status.txt
 		uptime  >> /usr/local/www/status.txt
 		date    >> /usr/local/www/status.txt
 		echo $1 >> /usr/local/www/status.txt
@@ -61,19 +70,21 @@ while [ /bin/true ]; do
 		# -- RELENG_1
 		setstatus "Setting build to -RELENG_1..."
 		set_source "-RELENG_1"
-		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
-		cp /root/pfsense_local_releng_1.sh \
-			/home/pfsense/tools//builder_scripts/pfsense_local.sh
+		CURRENTLY_BUILDING=`cat $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt`
+		cp $SNAPSHOTSCRIPTSDIR/pfsense_local_releng_1.sh \
+			$BUILDERSCRIPTS/pfsense_local.sh
 
+		cd $BUILDERSCRIPTS
+		
 		update_sources
 		build_updates
-		cp /usr/obj.pfSense/pfSense.iso.gz /usr/local/www/data/iso/
+		cp $PFSENSEOBJDIR/pfSense.iso.gz $WEBDATAROOT/iso/
 
 		build_embedded
-		cp /usr/obj.pfSense/pfSense.img.gz /usr/local/www/data/embedded/
+		cp $PFSENSEOBJDIR/pfSense.img.gz $WEBDATAROOT/embedded/
 
 		setstatus "Copying files for -RELENG_1 build..."
-		cp /home/pfsense/updates/* /usr/local/www/data/updates/
+		cp $PFSENSEUPDATESDIR/* $WEBDATAROOT/updates/
 
 		setstatus "Cleaning up..."
 		rm -rf /usr/obj*
@@ -84,19 +95,19 @@ while [ /bin/true ]; do
 		# -- HEAD
 		setstatus "Setting build to -HEAD..."
 		set_source "-HEAD"
-		CURRENTLY_BUILDING=`cat /usr/local/www/CURRENTLY_BUILDING_PLATFORM.txt`
-		cp /root/pfsense_local_releng_1_head.sh \
-			/home/pfsense/tools/builder_scripts/pfsense_local.sh
+		CURRENTLY_BUILDING=`cat $WEBROOT/CURRENTLY_BUILDING_PLATFORM.txt`
+		cp $SNAPSHOTSCRIPTSDIR/pfsense_local_releng_1_head.sh \
+			$BUILDERSCRIPTS/pfsense_local.sh
 
-		cd /home/pfsense/tools/builder_scripts/
+		cd $BUILDERSCRIPTS
 
 		update_sources
 		build_updates
-		cp /usr/obj.pfSense/pfSense.iso.gz /usr/local/www/data/head/iso/
-		cp /home/pfsense/updates/* /usr/local/www/data/head/updates/
+		cp $PFSENSEOBJDIR/pfSense.iso.gz $WEBDATAROOT/head/iso/
+		cp $PFSENSEUPDATESDIR/* $WEBDATAROOT/head/updates/
 
 		build_embedded
-		cp /usr/obj.pfSense/pfSense.img.gz /usr/local/www/data/head/embedded/
+		cp $PFSENSEOBJDIR/pfSense.img.gz $WEBDATAROOT/head/embedded/
 
 		setstatus "Cleaning up..."
 		rm -rf /usr/obj*
