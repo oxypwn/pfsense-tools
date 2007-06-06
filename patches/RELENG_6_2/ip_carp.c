e18b974f34deb9532f23b8f569f917f920b897ff
Scott Ullrich <sullrich@pfsense.org>
2007-06-06 22:01:46 +0000

    Add a patch for: glebius     2007-06-06 16:20:51 UTC
     FreeBSD src repository
    
     Modified files:        (Branch: RELENG_6)
       sys/netinet          ip_carp.c
     Log:
     Merge 1.47:
       Make it possible that carpdetach() unlocks on return. Then, in
       carp_clone_destroy() we are on a safe side, we don't need to
       unlock the cif, that can me already non-existent at this point.
    
       Reported by:    Anton Yuzhaninov <citrin rambler-co.ru>

--- patches/RELENG_6_2/ip_carp.c
+++ patches/RELENG_6_2/ip_carp.c
@@ -4,7 +4,13 @@ RCS file: /home/ncvs/src/sys/netinet/ip_carp.c,v
 retrieving revision 1.27.2.9
 diff -u -r1.27.2.9 ip_carp.c
 --- ip_carp.c	10 Oct 2006 18:39:38 -0000	1.27.2.9
-+++ ip_carp.c	19 May 2007 16:35:19 -0000
++++ ip_carp.c	6 Jun 2007 21:56:46 -0000
+@@ -1,4 +1,4 @@
+-/* 	$FreeBSD: src/sys/netinet/ip_carp.c,v 1.27.2.9 2006/10/10 18:39:38 bz Exp $ */
++/* 	$FreeBSD: src/sys/netinet/ip_carp.c,v 1.27.2.11 2007/06/06 16:20:50 glebius Exp $ */
+ 
+ /*
+  * Copyright (c) 2002 Michael Shalayeff. All rights reserved.
 @@ -191,7 +191,7 @@
  static void	carp_input_c(struct mbuf *, struct carp_header *, sa_family_t);
  static int 	carp_clone_create(struct if_clone *, int);
@@ -14,19 +20,18 @@ diff -u -r1.27.2.9 ip_carp.c
  static int	carp_prepare_ad(struct mbuf *, struct carp_softc *,
  		    struct carp_header *);
  static void	carp_send_ad_all(void);
-@@ -406,10 +406,7 @@
+@@ -406,9 +406,7 @@
  
  	if (sc->sc_carpdev)
  		CARP_SCLOCK(sc);
 -	carpdetach(sc);	
 -	if (sc->sc_carpdev)
 -		CARP_SCUNLOCK(sc);
--
-+	carpdetach(sc, 1);
++	carpdetach(sc, 1);	/* Returns unlocked. */
+ 
  	mtx_lock(&carp_mtx);
  	LIST_REMOVE(sc, sc_next);
- 	mtx_unlock(&carp_mtx);
-@@ -420,7 +417,7 @@
+@@ -420,7 +418,7 @@
  }
  
  static void
@@ -35,12 +40,12 @@ diff -u -r1.27.2.9 ip_carp.c
  {
  	struct carp_if *cif;
  
-@@ -450,9 +447,10 @@
+@@ -450,9 +448,10 @@
  			sc->sc_carpdev->if_carp = NULL;
  			CARP_LOCK_DESTROY(cif);
  			FREE(cif, M_IFADDR);
 -		}
-+		} else if (unlock) 
++		} else if (unlock)
 +			CARP_UNLOCK(cif);
 +		sc->sc_carpdev = NULL;
  	}
@@ -48,7 +53,7 @@ diff -u -r1.27.2.9 ip_carp.c
  }
  
  /* Detach an interface from the carp. */
-@@ -471,7 +469,7 @@
+@@ -471,7 +470,7 @@
  	CARP_LOCK(cif);
  	for (sc = TAILQ_FIRST(&cif->vhif_vrs); sc; sc = nextsc) {
  		nextsc = TAILQ_NEXT(sc, sc_list);
@@ -56,4 +61,13 @@ diff -u -r1.27.2.9 ip_carp.c
 +		carpdetach(sc, 0);
  	}
  }
+ 
+@@ -658,7 +657,7 @@
+ 	SC2IFP(sc)->if_ipackets++;
+ 	SC2IFP(sc)->if_ibytes += m->m_pkthdr.len;
+ 
+-	if (SC2IFP(sc)->if_bpf) {
++	if (bpf_peers_present(SC2IFP(sc)->if_bpf)) {
+ 		struct ip *ip = mtod(m, struct ip *);
+ 		uint32_t af1 = af;
  
