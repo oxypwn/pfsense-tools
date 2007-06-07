ec677d66461c13a6b38158cc6455d0486703213b
Scott Ullrich <sullrich@pfsense.org>
2007-06-07 17:08:36 +0000

    Add a mass kernel patch building script that will build a kernel with each patch option and output all kernels to a central location.
    This is handy for building a kernel per patch option to identify slowdowns, regressions, etc.

---
+++ builder_scripts/build_mass_patch_kernels.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e -x
+
+CURRENTDIR=`pwd`
+[ -r "${CURRENTDIR}/pfsense_local.sh" ] && . ${CURRENTDIR}/pfsense_local.sh
+
+PATCHDIR=${PATCHDIR:-${BASE_DIR}/tools/patches/${freebsd_branch}}
+SRCDIR=${SRCDIR:-/usr/src}
+
+# Set these two options
+FBSDBRANCH="RELENG_6_2"
+KERNCONF="pfSense_wrap.6"
+KERNELOUTPUTDIR="/usr/local/www/data/kernels/"
+PATHTOTESTKERNEL="/root/pfSense_wrap.6"
+CVSUPHOST="cvsup.livebsd.com"
+
+for LINE in `cat /root/patches.$FBSDBRANCH` 
+do
+	cd $CURRENTDIR
+	# Loop through and patch files
+	PATCH_DEPTH=`echo $LINE | cut -d~ -f1`
+	PATCH_DIRECTORY=`echo $LINE | cut -d~ -f2`
+	PATCH_FILE=`echo $LINE | cut -d~ -f3`
+	PATCH_FILE_LEN=`echo $PATCH_FILE | wc -c`
+	MOVE_FILE=`echo $LINE | cut -d~ -f4`
+	MOVE_FILE_LEN=`echo $MOVE_FILE | wc -c`
+	
+	cvsup -h cvsup.livebsd.com /home/pfsense/tools/builder_scripts/$FBSDBRANCH-supfile
+	echo "Building kernel with patch $PATCH_FILE"
+
+	if [ $PATCH_FILE_LEN -gt "2" ]; then
+		echo "Patching ${PATCH_FILE}"
+		(cd ${SRCDIR}/${PATCH_DIRECTORY} && patch -f ${PATCH_DEPTH} < ${PATCHDIR}/${PATCH_FILE})
+	fi
+
+	cp $PATHTOTESTKERNEL /usr/src/sys/i386/conf/
+
+	(cd /usr/src/ && make buildkernel KERNCONF=$KERNCONF && make installkernel KERNCONF=$KERNCONF)
+	gzip /boot/kernel/kernel
+	mv /boot/kernel/kernel.gz $KERNELOUTPUTDIR/kernel.gz-$PATCH_FILE
+done
+
+# Build a kernel without any patches at all
+cvsup -h $CVSUPHOST /home/pfsense/tools/builder_scripts/$FBSDBRANCH-supfile
+(cd /usr/src/ && make buildkernel KERNCONF=$KERNCONF && make installkernel KERNCONF=$KERNCONF)
+gzip /boot/kernel/kernel
+mv /boot/kernel/kernel.gz $KERNELOUTPUTDIR/kernel.gz-NOPATCHES
+
