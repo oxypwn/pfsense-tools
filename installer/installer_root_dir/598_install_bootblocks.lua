c4642d9bbd6ec633c548d2d33104183cd5d09a5d
Scott Ullrich <sullrich@pfsense.org>
2007-06-13 22:32:15 +0000

    Simplify and just use grub.  Do not alert when the bootblocks are installed correctly, just alert when they fail to install.

--- installer/installer_root_dir/598_install_bootblocks.lua
+++ installer/installer_root_dir/598_install_bootblocks.lua
@@ -70,18 +70,6 @@ return {
 		    name = _("Install Bootblock?"),
 		    short_desc = _("Install a bootblock on this disk"),
 		    control = "checkbox"
-		},
-		{
-		    id = "packet",
-		    name = _("Packet mode?"),
-		    short_desc = _("Select this to use 'packet mode' to boot the disk"),
-		    control = "checkbox"
-	    },
-		{
-		    id = "usegrub",
-		    name = _("Use Grub"),
-		    short_desc = _("Select this to use Grub to boot the disk"),
-		    control = "checkbox"
 		}
 	    },
 	    	
@@ -113,37 +101,24 @@ return {
 
 		for i, dataset in ipairs(response.datasets) do
 			if dataset.boot0cfg == "Y" then
-				if dataset.usegrub == "Y" then
-					dd = disk_ref[dataset.disk]
-					disk = dd:get_name()
-					-- execute Grub boot block installer
-					cmds:set_replacements{
-					    disk = disk
-					}
-					cmds:add("sysctl kern.geom.debugflags=16")
-					cmds:add("/usr/local/sbin/grub-install --root-directory=/mnt/ /dev/${disk}")
-					cmds:add("echo \"default=0\" > /mnt/boot/grub/menu.lst")
-					cmds:add("echo \"timeout=5\" >> /mnt/boot/grub/menu.lst")
-					cmds:add("echo \"title pfSense\" >> /mnt/boot/grub/menu.lst")
-					cmds:add("echo \"	root (hd0,0,a)\" >> /mnt/boot/grub/menu.lst")
-					cmds:add("echo \"	kernel /boot/loader\" >> /mnt/boot/grub/menu.lst")
-					cmds:add("/usr/local/sbin/grub-install --root-directory=/mnt/ /dev/${disk}")
-				else
-					dd = disk_ref[dataset.disk]
-					dd:cmds_install_bootblock(cmds,
-					    (dataset.packet == "Y"))
-					cmds:set_replacements{
-					    disk = disk
-					}
-					cmds:add("boot0cfg -B -b /boot/boot /dev/${disk}")
-				end
+				dd = disk_ref[dataset.disk]
+				disk = dd:get_name()
+				-- execute Grub boot block installer
+				cmds:set_replacements{
+				    disk = disk
+				}
+				cmds:add("sysctl kern.geom.debugflags=16")
+				cmds:add("/usr/local/sbin/grub-install --root-directory=/mnt/ /dev/${disk}")
+				cmds:add("echo \"default=0\" > /mnt/boot/grub/menu.lst")
+				cmds:add("echo \"timeout=5\" >> /mnt/boot/grub/menu.lst")
+				cmds:add("echo \"title pfSense\" >> /mnt/boot/grub/menu.lst")
+				cmds:add("echo \"	root (hd0,0,a)\" >> /mnt/boot/grub/menu.lst")
+				cmds:add("echo \"	kernel /boot/loader\" >> /mnt/boot/grub/menu.lst")
+				cmds:add("/usr/local/sbin/grub-install --root-directory=/mnt/ /dev/${disk}")
 			end
 		end
 
 		if cmds:execute() then
-			App.ui:inform(_(
-			    "Bootblocks were successfully installed!"
-			))
 			return step:next()
 		else
 			App.ui:inform(_(
