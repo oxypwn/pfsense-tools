dd28c314a60655fd67df7e19e9ae27f5feb9031c
Daniel Stefan Haischt <dsh@pfsense.org>
2007-10-07 01:57:37 +0000

    * chroot issues solved. installedpackages section is now getting propagated   to any config.xml file.

--- builder_scripts/pfs_custom_pkginstall.sh
+++ builder_scripts/pfs_custom_pkginstall.sh
@@ -33,7 +33,10 @@
 #                                                                            
 #========================================================================== 
 
-PLATFORM=`cat $CVS_CO_DIR/etc/platform`
+#
+# Uncomment to debug this script standalone
+#
+#. ./pfsense_local.sh
 
 #
 # Note: This function prepares the environment needed by the script pfspkg_installer
@@ -47,63 +50,23 @@ install_custom_packages_setup() {
 	if [ ! -z "${custom_package_list:-}" ]; then
 		# Notes:
 		# ======
-		# /etc/platform is required cause some scripts are demanding for its existence
 		# devfs mount is required cause PHP requires /dev/stdin
-		# tried to fake symlink /conf
 		# php.ini needed to make PHP argv capable
 		#
-		echo "Installing custom packages to: ${TODIR} using platform type ${PLATFORM} ..."
+		/bin/echo "Installing custom packages to: ${TODIR} ..."
 
-#		if [ ! -f ${TODIR}/etc/platform ]; then
-#			echo "installing temporary platform file to ${TODIR}/etc ..."	
-#			touch ${TODIR}/etc/platform
-#			touch ${TODIR}/tmp/remove_platform
-#		else
-#			echo "Using existing platform file from ${TODIR}/etc ..."
-#		fi
+		/bin/echo "Mounting temporary devfs filesystem to ${TODIR} ..."
+		/sbin/mount -t devfs devfs ${TODIR}/dev
 
-		echo "Mounting temporary devfs filesystem to ${TODIR} ..."
-		mount -t devfs devfs ${TODIR}/dev
+		/bin/echo "Copying resolv.conf to ${TODIR} to enable pkg manager to resolve DNS names ..."
+		/bin/cp /etc/resolv.conf ${TODIR}/etc
 
-		echo "Copying resolv.conf to ${TODIR} to enable pkg manager to resolve DNS names ..."
-		cp /etc/resolv.conf ${TODIR}/etc
+		/bin/echo "Installing temporary php.ini to ${TODIR}/tmp ..."
+		/bin/echo "register_argc_argv=1" > ${TODIR}/tmp/php.ini
 
-		if [ ${PLATFORM} != "embedded" ]; then
-			# test whether conf dir is already a symlink
-			if [ ! -f ${TODIR}/conf ]; then
-				# backup original conf dir
-				if [ -d ${TODIR}/conf ]; then
-					echo "Backing up conf dir to ${TODIR}/conf.org ..."
-					chroot ${TODIR} /bin/mv /conf /conf.org
-					touch ${TODIR}/tmp/restore_conf_dir
-				fi
-
-				# install the symlink as it would exist on a live system
-				echo "Symlinking ${TODIR}/cf/conf to ${TODIR}/conf ..."
-				chroot ${TODIR} /bin/ln -s /cf/conf /conf
-				touch ${TODIR}/tmp/remove_conf_symlink
-			else
-				# seems like we are already working with a conf dir that is a symlink
-				echo "Using existing conf dir from ${TODIR} ..."
-			fi
-
-			# now that we do have the symlink in place create
-			# a backup dir if necessary.
-			if [ ! -d ${TODIR}/cf/conf/backup ]; then
-				echo "Creating backup dir in ${TODIR}/cf/conf ..."
-				chroot ${TODIR} /bin/mkdir -p /cf/conf/backup
-				touch ${TODIR}/tmp/remove_backup
-			else
-				echo "Using existing backup dir from ${TODIR}/cf/conf ..."
-			fi
-		fi
-
-		echo "Installing temporary php.ini to ${TODIR}/tmp ..."
-		echo "register_argc_argv=1" > ${TODIR}/tmp/php.ini
-
-		echo "Installing custom pfSense package installer to ${TODIR}/tmp ..."
-		cp ./pfspkg_installer ${TODIR}/tmp
-		chmod a+x ${TODIR}/tmp/pfspkg_installer
+		/bin/echo "Installing custom pfSense package installer to ${TODIR}/tmp ..."
+		/bin/cp ./pfspkg_installer ${TODIR}/tmp
+		/bin/chmod a+x ${TODIR}/tmp/pfspkg_installer
 	fi
 }
 
@@ -111,8 +74,41 @@ install_custom_packages_exec() {
 	DESTNAME="pkginstall.sh"
 
 	# setup script that will be run within the chroot env
-	cat > ${FREESBIE_PATH}/extra/customscripts/${DESTNAME} <<EOF
+	/bin/cat > ${FREESBIE_PATH}/extra/customscripts/${DESTNAME} <<EOF
 #!/bin/sh
+# set to no if you do not want to delete any files that have
+# been created while having executed this script.
+CLEANUP="yes"
+#
+# Setup
+#
+# backup original conf dir
+if [ -d /conf ]; then
+	/bin/echo "Backing up conf dir to /conf.org ..."
+	/bin/mv /conf /conf.org
+	/usr/bin/touch /tmp/restore_conf_dir
+fi
+
+# test whether conf dir is already a symlink
+if [ ! -f /conf ]; then
+	# install the symlink as it would exist on a live system
+	/bin/echo "Symlinking /conf.default to /conf ..."
+	/bin/ln -s /conf.default /conf
+	/usr/bin/touch /tmp/remove_conf_symlink
+else
+	# seems like we are already working with a conf dir that is a symlink
+	/bin/echo "Using existing conf dir from / ..."
+fi
+
+# now that we do have the symlink in place create
+# a backup dir if necessary.
+if [ ! -d /conf/backup ]; then
+	/bin/echo "Creating backup dir in /conf ..."
+	/bin/mkdir -p /conf/backup
+	/usr/bin/touch /tmp/remove_backup
+else
+	/bin/echo "Using existing backup dir from ${TODIR}/conf ..."
+fi
 #
 # Assemble package list if necessary
 #
@@ -125,35 +121,47 @@ install_custom_packages_exec() {
 #
 # Cleanup
 #
-echo "Deleting temporary php.ini from /tmp"
-rm /tmp/php.ini
+/bin/echo "Deleting temporary php.ini from /tmp"
+/bin/rm /tmp/php.ini
 
 if [ -f /tmp/remove_platform ]; then
-	echo "Removing temporary platform file from /etc ..."
-	rm /etc/platform
-	rm /tmp/remove_platform
+	/bin/echo "Removing temporary platform file from /etc ..."
+	/bin/rm /etc/platform
+	/bin/rm /tmp/remove_platform
 fi
 
 if [ -f /tmp/remove_backup ]; then
-	echo "Removing temporary backup dir from /conf ..."
-	rm -rf /cf/conf/backup
-	rm /tmp/remove_backup
+	/bin/echo "Removing temporary backup dir from /conf ..."
+	/bin/rm -rf /conf/backup
+	/bin/rm /tmp/remove_backup
 fi
 
 if [ -f /tmp/remove_conf_symlink ]; then
-	echo "Removing temporary conf dir ..."
-	rm /conf
-	rm /tmp/remove_conf_symlink
+	/bin/echo "Removing temporary conf dir ..."
+	/bin/rm /conf
+	/bin/rm /tmp/remove_conf_symlink
 	
 	if [ -f /tmp/restore_conf_dir ]; then
-		echo "Restoring original conf dir ..."
-		mv /conf.org /conf
-		rm /tmp/restore_conf_dir
+		/bin/echo "Restoring original conf dir ..."
+		/bin/mv /conf.org /conf
+		/bin/rm /tmp/restore_conf_dir
 	fi
 fi
 
-echo "Removing temporary resolv.conf from /etc ..."
-rm /etc/resolv.conf
+/bin/echo "Removing temporary resolv.conf from /etc ..."
+/bin/rm /etc/resolv.conf
+
+/bin/echo "Removing pfspkg_installer script from /tmp ..."
+/bin/rm /tmp/pfspkg_installer
+
+/bin/echo "Removing custom packages list file from /tmp ..."
+/bin/rm /tmp/pkgfile.lst
+
+/bin/echo "Removing possible package install leftover (*.tbz, *.log) ..."
+/bin/rm /tmp/*.log /tmp/*.tbz
+
+/bin/echo "Removing config.cache which was generating during package install ..."
+/bin/rm /tmp/*.cache
 
 EOF
 
