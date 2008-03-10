db20e131ce9251a9a5cb78a66bdf22483f0064bf
Scott Ullrich <sullrich@pfsense.org>
2008-03-10 04:36:32 +0000

    Copy needed libraries manually.

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -161,6 +161,10 @@ populate_extra() {
 		install -s /lib/pam_unix.so ${CVS_CO_DIR}/lib/
 	fi
 	
+	cp /lib/libm.so.* ${CVS_CO_DIR}/lib/
+	cp /lib//libz.so.* ${CVS_CO_DIR}/lib/
+	cp /lib/libc.so.* ${CVS_CO_DIR}/lib/
+	
 	if [ -f /usr/local/lib/libcurl.so.3 ]; then
 		echo "Installing /usr/local/lib/libcurl.so.3"
 		install -s /usr/local/lib/libcurl.so.3 ${CVS_CO_DIR}/usr/local/lib/
