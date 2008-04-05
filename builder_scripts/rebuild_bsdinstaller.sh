1ef6a965d34161c4aaa273fa15baad8b2daaf273
Scott Ullrich <sullrich@pfsense.org>
2008-04-05 22:10:52 +0000

    Work around the fact that packages.tgz is being created until I can track down the origin.

--- builder_scripts/rebuild_bsdinstaller.sh
+++ builder_scripts/rebuild_bsdinstaller.sh
@@ -2,6 +2,8 @@
 
 echo "Rebuilding BSDInstaller..."
 
+mkdir -p conf
+
 PREVIOUSDIR=`pwd`
 
 cd /home/pfsense/installer/installer/scripts/build 
@@ -9,5 +11,11 @@ cd /home/pfsense/installer/installer/scripts/build
 ./create_installer_tarballs.sh 
 ./copy_ports_to_portsdir.sh 
 ./build_installer_packages.sh 
+
+if [ -f /usr/home/pfsense/tools/builder_scripts/conf/packages.tbz ]; then
+	echo "Moving BSDInstaller package into place..."
+	mv /usr/home/pfsense/tools/builder_scripts/conf/packages.tbz \
+		/usr/ports/packages/All/bsdinstaller-2.0.2008.0405.tbz
+fi
 
 cd $PREVIOUSDIR
