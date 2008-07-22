73889288c92996e01a9820d559923b81435db9b4
Scott Ullrich <sullrich@pfsense.org>
2008-07-22 18:30:44 +0000

    Add a script that invokes build_all_kernels for testing / diagnostics.

---
+++ builder_scripts/build_kernels.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# pfSense master builder script
+# (C)2005-2006 Scott Ullrich and the pfSense project
+# All rights reserved.
+#
+# $Id$
+
+# Crank up error reporting, debugging.
+#set -e 
+#set -x
+
+# Suck in local vars
+. ./pfsense_local.sh
+
+# Suck in script helper functions
+. ./builder_common.sh
+
+# Output build flags
+print_flags
+
+# Allow old CVS_CO_DIR to be deleted later
+if [ -d $CVS_CO_DIR ]; then 
+	chflags -R noschg $CVS_CO_DIR
+fi 
+
+# Use pfSense.6 as kernel configuration file
+if [ $FreeBSD_version = "6" ]; then
+	export KERNELCONF=${KERNELCONF:-"${PWD}/conf/pfSense.6"}
+fi
+if [ $FreeBSD_version = "7" ]; then
+	export KERNELCONF=${KERNELCONF:-"${PWD}/conf/pfSense.7"}
+fi
+
+# If a embedded build has been performed we need to nuke
+# /usr/obj.pfSense/ since full uses a different
+# make.conf
+if [ -f /usr/obj.pfSense/pfSense_wrap.6.world.done ]; then
+	echo -n "Removing /usr/obj* since embedded build performed prior..."
+	rm -rf /usr/obj.pfSense/*
+	echo "done."
+fi
+
+# Use normal make.conf
+if [ $FreeBSD_version = "6" ]; then
+	export MAKE_CONF="${PWD}/conf/make.conf"
+	export MAKE_CONF_INSTALL="${PWD}/conf/make.conf"	
+	export MAKE_CONF_INSTALL="${PWD}/conf/make.conf"
+fi
+if [ $FreeBSD_version = "7" ]; then
+	export MAKE_CONF="${PWD}/conf/make.conf.7"
+	export SRC_CONF="${PWD}/conf/make.conf.7"
+	export SRC_CONF_INSTALL="${PWD}/conf/make.conf.7.install"
+fi
+
+# Add etcmfs and rootmfs to the EXTRA plugins used by freesbie2
+export EXTRA="${EXTRA:-} rootmfs varmfs etcmfs"
+
+if [ $FreeBSD_version = "6" ]; then
+	export PRUNE_LIST="${PWD}/remove.list.iso"
+fi
+if [ $FreeBSD_version = "7" ]; then
+	export PRUNE_LIST="${PWD}/remove.list.iso.7"
+fi
+
+# Build SMP, Embedded (wrap) and Developers edition kernels
+echo ">>> Building all extra kernels... $FreeBSD_version  $freebsd_branch ..."
+build_all_kernels
+
+# Check for freesbie builder issues
+if [ -f ${MAKEOBJDIRPREFIX}/usr/home/pfsense/freesbie2/.tmp_kernelbuild ]; then
+	echo "Something has gone wrong!  Press ENTER to view log file."
+	read ans
+	more ${MAKEOBJDIRPREFIX}/usr/home/pfsense/freesbie2/.tmp_kernelbuild 
+	exit
+fi
+
+
+
+
+
+
