fb3709b2d5c0927f1cd34ec11658b571eebddec4
Scott Ullrich <sullrich@pfsense.org>
2008-07-23 01:59:33 +0000

    Build BSDInstaller with -O instead of -O2

--- builder_scripts/rebuild_bsdinstaller.sh
+++ builder_scripts/rebuild_bsdinstaller.sh
@@ -8,6 +8,14 @@ PREVIOUSDIR=`pwd`
 
 cd /home/pfsense/installer/installer/scripts/build 
 
+# Backup old make.conf
+if [ -f /etc/make.conf ]; then
+	mv /etc/make.conf /tmp/
+	echo "WITHOUT_X11=yo" >> /etc/make.conf
+	echo "CFLAGS=-O"
+	MKCNF="pfPorts"
+fi
+
 ./create_installer_tarballs.sh 
 ./copy_ports_to_portsdir.sh 
 ./build_installer_packages.sh 
@@ -16,6 +24,11 @@ if [ -f /usr/home/pfsense/tools/builder_scripts/conf/packages.tbz ]; then
 	echo "Moving BSDInstaller package into place..."
 	mv /usr/home/pfsense/tools/builder_scripts/conf/packages.tbz \
 		/usr/ports/packages/All/bsdinstaller-2.0.2008.0405.tbz
+fi
+
+# Restore previous make.conf
+if [ -f /tmp/make.conf ]; then
+	mv /tmp/make.conf /etc/
 fi
 
 cd $PREVIOUSDIR
