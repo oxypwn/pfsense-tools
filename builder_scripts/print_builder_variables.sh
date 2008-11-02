cb38b644213f35751ba1486e34a0e0d46c0566fa
Scott Ullrich <sullrich@pfsense.org>
2008-11-02 21:13:17 +0000

    Source builder_common.sh

--- builder_scripts/print_builder_variables.sh
+++ builder_scripts/print_builder_variables.sh
@@ -2,5 +2,7 @@
 
 CURRENTDIR=`pwd`
 [ -r "${CURRENTDIR}/pfsense_local.sh" ] && . ${CURRENTDIR}/pfsense_local.sh
+[ -r "${CURRENTDIR}/builder_common.sh" ] && . ${CURRENTDIR}/builder_common.sh
 
 print_flags
+
