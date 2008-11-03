2f3461e2a9bfbf97d99b950c55f80fa81db8bc97
Scott Ullrich <sullrich@pfsense.org>
2008-11-03 17:24:03 +0000

    Remove MBR changes, the original submitter did not return my email in a reasonable amount of time.

--- installer/installer_root_dir7/598_install_bootblocks.lua
+++ installer/installer_root_dir7/598_install_bootblocks.lua
@@ -21,7 +21,6 @@ return {
 		local dataset = {
 			disk = raw_name,
 			boot0cfg = "Y", 
-			usembr = "Y", 
 			usegrub = "N", 
 			packet = "N"
 		}
@@ -84,13 +83,7 @@ return {
 		    name = _("Use Grub"),
 		    short_desc = _("Select this to use Grub to boot the disk"),
 		    control = "checkbox"
-		},
-		{
-		    id = "usembr",
-		    name = _("Overwrite MBR"),
-		    short_desc = _("Select this to overwrite the Master Boot Record"),
-		    control = "checkbox"
-		}		
+		}
 	    },
 	    	
 	    actions = {
@@ -128,9 +121,6 @@ return {
 					cmds:set_replacements{
 					    disk = disk
 					}
-					if dataset.usembr == "Y" then
-						cmds:add("boot0cfg -B -b mbr /dev/${disk}")
-					end
 					cmds:add("sysctl kern.geom.debugflags=16")
 					cmds:add("/bin/mkdir /mnt/boot/grub")
 					cmds:add("echo \"serial --unit=0 --speed=9600\" > /mnt/boot/grub/menu.lst")
@@ -149,9 +139,6 @@ return {
 					cmds:set_replacements{
 					    disk = disk
 					}
-					if dataset.usembr == "Y" then
-						cmds:add("boot0cfg -B -b mbr /dev/${disk}")
-					end
 					cmds:add("boot0cfg -B -b /boot/boot0 /dev/${disk}")
 				end
 			end
