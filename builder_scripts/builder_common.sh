6dd55808bdf5679fca308766daf81587f73e8e14
Scott Ullrich <sullrich@pfsense.org>
2008-11-03 17:59:19 +0000

    cd /usr/ports && run command
    Suggested-by: databeestje@

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -260,7 +260,7 @@ recompile_pfPorts() {
 
 		chmod a+rx ${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG}
 		echo ">>> Executing ${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG}"
-		( su - root -c "${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG}" )
+		( su - root -c "cd /usr/ports/ && ${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG}" )
 		
 		if [ "${MKCNF}x" = "pfPortsx" ]; then
 			mv /tmp/make.conf /etc/
