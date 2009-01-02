fe4a8a76ef6368eac85b6300715f7f843cfd692f
Seth Mos <seth.mos@xs4all.nl>
2009-01-02 12:33:01 +0000

    Also set TARGET_ARCH

--- builder_scripts/pfsense_local.sh
+++ builder_scripts/pfsense_local.sh
@@ -147,6 +147,7 @@ export UNION_DIRS="etc usr root"
 #  Tier 4: None at the moment
 #  Source: http://www.freebsd.org/doc/en/articles/committers-guide/archs.html
 export ARCH="i386"
+export TARGET_ARCH="i386"
 
 # Custom Copy and Remove lists that override base remove.list.* and copy.list.*
 #export CUSTOM_REMOVE_LIST=""
