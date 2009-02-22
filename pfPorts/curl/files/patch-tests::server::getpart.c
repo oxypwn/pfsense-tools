32df7b5e0202302d79d232ce274da235f21125c9
Scott Ullrich <sullrich@sullrich-MacBookPro.local>
2009-02-21 21:59:29 -0500

    Update curl version

--- pfPorts/curl/files/patch-tests::server::getpart.c
+++ pfPorts/curl/files/patch-tests::server::getpart.c
@@ -1,11 +1,12 @@
---- tests/server/getpart.c.orig	Wed Dec 10 15:15:52 2003
-+++ tests/server/getpart.c	Wed Dec 10 15:17:19 2003
-@@ -21,7 +21,7 @@
-   int len = strlen(buffer);
-   int needed_len = len + *stringlen;
+diff -urN -x .svn ../../vendor/curl/tests/server/getpart.c ./tests/server/getpart.c
+--- ../../vendor/curl/tests/server/getpart.c	2008-10-23 17:42:39.000000000 +0300
++++ ./tests/server/getpart.c	2009-01-21 16:12:27.000000000 +0200
+@@ -87,7 +87,7 @@
+     needed_len = len + *stringlen + 1; /* recalculate */
+   }
  
 -  if(needed_len >= *stralloc) {
 +  while(needed_len >= *stralloc) {
      char *newptr;
-     long newsize = needed_len*2; /* get twice the needed size */
+     size_t newsize = needed_len*2; /* get twice the needed size */
  
