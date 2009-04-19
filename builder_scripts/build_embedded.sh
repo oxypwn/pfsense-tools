e8a1f30f11c767e4da5284fb16b23428b01fe180
Scott Ullrich <sullrich@pfsense.org>
2009-04-18 20:58:07 -0400

    Correct source dir for tar operation

--- builder_scripts/build_embedded.sh
+++ builder_scripts/build_embedded.sh
@@ -173,7 +173,7 @@ cd ${CLONEDIR}
 FBSD_VERSION=`/usr/bin/uname -r | /usr/bin/cut -d"." -f1`
 if [ "$FBSD_VERSION" = "8" ]; then
 	echo ">>> Using TAR to clone build_embedded.sh..."
-	( cd ${TMPDIR} && tar cf - * | ( cd /$TMPDIR; tar xfp -) )
+	( tar cf - * | ( cd /$TMPDIR; tar xfp -) )
 else
 	echo ">>> Using CPIO to clone {$TMPDIR}..."
 	find . -print -depth | cpio -dump ${TMPDIR}
