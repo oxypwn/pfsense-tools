0be93cedcf93bad106d7a52db8af33c43d6158f6
Scott Ullrich <sullrich@pfsense.org>
2009-04-18 21:10:05 -0400

    Ensure TMPDIR exists

--- builder_scripts/build_embedded.sh
+++ builder_scripts/build_embedded.sh
@@ -173,6 +173,7 @@ cd ${CLONEDIR}
 FBSD_VERSION=`/usr/bin/uname -r | /usr/bin/cut -d"." -f1`
 if [ "$FBSD_VERSION" = "8" ]; then
 	echo ">>> Using TAR to clone build_embedded.sh..."
+	mkdir -p ${TMPDIR}
 	( tar cf - * | ( cd /$TMPDIR; tar xfp -) )
 else
 	echo ">>> Using CPIO to clone {$TMPDIR}..."
