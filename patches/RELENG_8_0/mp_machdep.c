f317efc1513cd681f3e7d04fe39045c1530a081c
Scott Ullrich <sullrich@pfsense.org>
2009-04-30 17:14:39 -0400

    Previous patch did not work, try this new one.

--- patches/RELENG_8_0/mp_machdep.c
+++ patches/RELENG_8_0/mp_machdep.c
@@ -1,40 +1,23 @@
---- sys/i386/i386/mp_machdep.c	(revision 191699)
-+++ sys/i386/i386/mp_machdep.c	(working copy)
-@@ -267,6 +267,8 @@
- 		else if (type == CPUID_TYPE_CORE)
- 			cpu_cores = cnt;
- 	}
-+	if (cpu_cores == 0)
-+		cpu_cores = 1;
- 	if (cpu_logical == 0)
- 		cpu_logical = 1;
- 	cpu_cores /= cpu_logical;
-@@ -352,7 +354,7 @@
- 	else if (cpu_high)
- 		topo_probe_0x4();
- 	if (cpu_cores == 0)
--		cpu_cores = mp_ncpus;
-+		cpu_cores = mp_ncpus > 0 ? mp_ncpus : 1;
- 	if (cpu_logical == 0)
- 		cpu_logical = 1;
- }
---- sys/amd64/amd64/mp_machdep.c	(revision 191699)
-+++ sys/amd64/amd64/mp_machdep.c	(working copy)
-@@ -214,6 +214,8 @@
- 		else if (type == CPUID_TYPE_CORE)
- 			cpu_cores = cnt;
- 	}
-+	if (cpu_cores == 0)
-+		cpu_cores = 1;
- 	if (cpu_logical == 0)
- 		cpu_logical = 1;
- 	cpu_cores /= cpu_logical;
-@@ -299,7 +301,7 @@
- 	else if (cpu_high)
- 		topo_probe_0x4();
- 	if (cpu_cores == 0)
--		cpu_cores = mp_ncpus;
-+		cpu_cores = mp_ncpus > 0 ? mp_ncpus : 1;
- 	if (cpu_logical == 0)
- 		cpu_logical = 1;
- }
+--- sys/amd64/amd64/mp_machdep.c.orig   2009-05-01 00:59:55.000000000 +0400
++++ sys/amd64/amd64/mp_machdep.c        2009-05-01 01:00:20.000000000 +0400
+@@ -309,6 +309,8 @@ cpu_topo(void)
+ {
+        int cg_flags;
+
++       topo_probe();
++
+        /*
+         * Determine whether any threading flags are
+         * necessry.
+$ diff -urp sys/i386/i386/mp_machdep.c.orig sys/i386/i386/mp_machdep.c
+--- sys/i386/i386/mp_machdep.c.orig     2009-05-01 01:01:53.000000000 +0400
++++ sys/i386/i386/mp_machdep.c  2009-05-01 01:01:41.000000000 +0400
+@@ -362,6 +362,8 @@ cpu_topo(void)
+ {
+        int cg_flags;
+
++       topo_probe();
++
+        /*
+         * Determine whether any threading flags are
+         * necessry.
