4ec1cc300dea4b1f6dbf9686e4693ff9e0c2728a
Scott Ullrich <sullrich@pfsense.org>
2009-05-23 14:55:01 -0400

    Adding script that will update all git repos in /home/pfsense to latest

---
+++ builder_scripts/update_git_repos.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Suck in local vars
+. ./pfsense_local.sh
+
+# Suck in script helper functions
+. ./builder_common.sh
+
+PWD=`pwd`
+
+cd $BASE_DIR
+for FILE in `ls`; do
+	if [ -d $FILE/.git ]; then
+		# Make absolute sure we are on current repo.
+		echo ">>> Updating GIT REPO $FILE"
+		cd $FILE
+		git fetch
+		git rebase origin
+		git reset --hard
+		git rebase origin
+		git reset --hard
+		cd $BASE_DIR
+	fi
+done
+
+cd $PWD
