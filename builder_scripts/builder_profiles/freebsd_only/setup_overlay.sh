7a53ac07e0b5b87a1c0ce4b627f9e5bf75f42880
Scott Ullrich <sullrich@pfsense.org>
2009-05-24 20:00:55 -0400

    Build FreeBSD iso

--- builder_scripts/builder_profiles/freebsd_only/setup_overlay.sh
+++ builder_scripts/builder_profiles/freebsd_only/setup_overlay.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
 TOOLSDIR=/home/pfsense/tools/builder_scripts/
+
 cp pfsense_local.sh    $TOOLSDIR
 cp pfsense-build.conf  $TOOLSDIR
-
-cd $TOOLSDIR/builder_profiles
 
 cd $TOOLSDIR
 ./clean_build.sh
 ./apply_kernel_patches.sh
-./build_iso.sh
-./build_embedded.sh
-
-
+./build_freebsdisoonly.sh
