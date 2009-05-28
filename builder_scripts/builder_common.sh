45c5070d450b80e5d1211948526a0eaed1870248
Seth Mos <seth.mos@xs4all.nl>
2009-05-28 15:32:16 +0200

    Add further fine tuning to the pfPorts recompile output

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -317,7 +317,8 @@ recompile_pfPorts() {
 
 		chmod a+rx ${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG}
 		echo ">>>> Executing ${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG}"
-		( su - root -c "cd /usr/ports/ && ${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG} ${MAKEJ_PORTS}" ) 2>&1 | egrep -wi "(^>>>|error|finding)"
+		( su - root -c "cd /usr/ports/ && ${pfSPORTS_COPY_BASE_DIR}/Makefile.${PFSENSETAG} ${MAKEJ_PORTS}" ) 2>&1 \
+		| egrep -v '(\-Werror|error\.[a-z])' | egrep -wi "(^>>>|error|finding)"
 		
 		if [ "${MKCNF}x" = "pfPortsx" ]; then
 			mv /tmp/make.conf /etc/
