c6c27b471af887a68106b1180dc2d16dc9a365da
Scott Ullrich <sullrich@pfsense.org>
2009-06-05 23:51:12 -0400

    Be less spammy

--- builder_scripts/rebuild_bsdinstaller.sh
+++ builder_scripts/rebuild_bsdinstaller.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Rebuilding BSDInstaller..."
+echo ">>> Rebuilding BSDInstaller..."
 
 . ./pfsense_local.sh
 
@@ -14,14 +14,18 @@ cd $BASE_DIR/installer/installer/scripts/build
 if [ -f /etc/make.conf ]; then
 	mv /etc/make.conf /tmp/
 	echo "WITHOUT_X11=yo" >> /etc/make.conf
-	echo "CFLAGS=-O" >> /etc/make.conf
+	echo "CFLAGS=-O2" >> /etc/make.conf
 	MKCNF="pfPorts"
 fi
 
 export CVSDIR=${CVSDIR:-"$BUILDER_SCRIPTS"}
-./create_installer_tarballs.sh | egrep -B3  -wi "(warning|error)"
-./copy_ports_to_portsdir.sh | egrep -B3  -wi "(warning|error)"
-./build_installer_packages.sh | egrep -B3  -wi "(warning|error)"
+
+echo ">>> Creating installer tarballs..."
+(cd $BASE_DIR/installer/installer/scripts/build  && ./create_installer_tarballs.sh) 2>&1 | egrep -wi '(warning|error)'
+echo ">>> Copying ports to the ports directory..."
+(cd $BASE_DIR/installer/installer/scripts/build  && ./copy_ports_to_portsdir.sh) 2>&1 | egrep -wi '(warning|error)'
+echo ">>> Rebuilding..."
+(cd $BASE_DIR/installer/installer/scripts/build  && ./build_installer_packages.sh) 2>&1 | egrep -wi '(error)'
 
 if [ -f $BUILDER_TOOLS/builder_scripts/conf/packages.tbz ]; then
 	echo "Moving BSDInstaller package into place..."
