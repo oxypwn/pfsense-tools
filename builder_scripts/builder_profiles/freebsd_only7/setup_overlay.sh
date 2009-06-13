75beea9b327f35ce9bfe1664e09cb0509dd583f6
Scott Ullrich <sullrich@pfsense.org>
2009-06-13 19:22:21 -0400

    use /tmp/ not /tmp/tmp

--- builder_scripts/builder_profiles/freebsd_only7/setup_overlay.sh
+++ builder_scripts/builder_profiles/freebsd_only7/setup_overlay.sh
@@ -2,6 +2,10 @@
 
 TOOLSDIR=/home/pfsense/tools/builder_scripts/
 
+cat $TOOLSDIR/RELENG_8-supfile | grep -v "date" > /tmp/RELENG_8-supfile
+cp /tmp/RELENG_8-supfile $TOOLSDIR
+rm /tmp/RELENG_8-supfile
+
 cp pfsense_local.sh    $TOOLSDIR
 cp pfsense-build.conf  $TOOLSDIR
 
--- builder_scripts/builder_profiles/freebsd_only8/setup_overlay.sh
+++ builder_scripts/builder_profiles/freebsd_only8/setup_overlay.sh
@@ -2,9 +2,9 @@
 
 TOOLSDIR=/home/pfsense/tools/builder_scripts/
 
-cat $TOOLSDIR/RELENG_8-supfile | grep -v "date" > /tmp/tmp/RELENG_8-supfile
-cp /tmp/tmp/RELENG_8-supfile $TOOLSDIR
-rm /tmp/tmp/RELENG_8-supfile
+cat $TOOLSDIR/RELENG_8-supfile | grep -v "date" > /tmp/RELENG_8-supfile
+cp /tmp/RELENG_8-supfile $TOOLSDIR
+rm /tmp/RELENG_8-supfile
 
 cp pfsense_local.sh    $TOOLSDIR
 cp pfsense-build.conf  $TOOLSDIR
