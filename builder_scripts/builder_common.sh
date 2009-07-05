509e87d33f012d599a0620a2fd85f614dd413e13
Scott Ullrich <sullrich@pfsense.org>
2009-07-04 21:24:46 -0400

    Sync awk disk code with 8.0

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -1393,74 +1393,69 @@ create_i386_diskimage ( ) {
 		$NANO_CODESIZE \
 		$NANO_CONFSIZE \
 		$NANO_DATASIZE |
-	awk '
-	{
-		printf "# %s\n", $0
+awk '
+{
+	printf "# %s\n", $0
 
-		# NANO_SECTS($3) * NANO_HEADS($4)
-		# size of cylinder in sectors
-		cs = $3 * $4
+	# size of cylinder in sectors
+	cs = $3 * $4
 
-		# NANO_MEDIASIZE($1)
-		# number of full cylinders on media
-		cyl = int ($1 / cs)
+	# number of full cylinders on media
+	cyl = int ($1 / cs)
 
-		# NANO_HEADS($4) NANO_SECTS($3)
-		# output fdisk geometry spec, truncate cyls to 1023
-		if (cyl <= 1023)
-			print "g c" cyl " h" $4 " s" $3
-		else
-			print "g c" 1023 " h" $4 " s" $3
+	# output fdisk geometry spec, truncate cyls to 1023
+	if (cyl <= 1023)
+		print "g c" cyl " h" $4 " s" $3
+	else
+		print "g c" 1023 " h" $4 " s" $3
 
-		# NANO_DATASIZE($7)
-		if ($7 > 0) { 
-			# size of data partition in full cylinders
-			dsl = int (($7 + cs - 1) / cs)
-		} else {
-			dsl = 0;
-		}
-
-		# NANO_CONFSIZE($6)
-		# size of config partition in full cylinders
-		csl = int (($6 + cs - 1) / cs)
-
-		# NANO_CODESIZE($5) $NANO_IMAGES($2) 
-		if ($5 == 0) {
-			# size of image partition(s) in full cylinders
-			isl = int ((cyl - dsl - csl) / $2)
-		} else {
-			isl = int (($5 + cs - 1) / cs)
-		}
-
-		# NANO_SECTS($3) 
-		# First image partition start at second track
-		print "p 1 165 " $3, isl * cs - $3
-		c = isl * cs;
-
-		# NANO_IMAGES($2) NANO_SECTS($3)
-		# Second image partition (if any) also starts offset one 
-		# track to keep them identical.
-		if ($2 > 1) {
-			print "p 2 165 " $3 + c, isl * cs - $3
-			c += isl * cs;
-		}
-
-		# Config partition starts at cylinder boundary.
-		#print "p 3 165 " c, csl * cs
-		#c += csl * cs
-
-		# NANO_DATASIZE($7) NANO_MEDIASIZE($1)
-		# Data partition (if any) starts at cylinder boundary.
-		if ($7 > 0) {
-			print "p 3 165 " c, dsl * cs
-		} else if ($7 < 0 && $1 > $c) {
-			print "p 3 165 " c, $1 - $c
-		} else if ($1 < c) {
-			print "Disk space overcommitted by", \
-			    c - $1, "sectors" > "/dev/stderr"
-			exit 2
-		}
+	if ($7 > 0) { 
+		# size of data partition in full cylinders
+		dsl = int (($7 + cs - 1) / cs)
+	} else {
+		dsl = 0;
 	}
+
+	# size of config partition in full cylinders
+	csl = int (($6 + cs - 1) / cs)
+
+	if ($5 == 0) {
+		# size of image partition(s) in full cylinders
+		isl = int ((cyl - dsl - csl) / $2)
+	} else {
+		isl = int (($5 + cs - 1) / cs)
+	}
+
+	# First image partition start at second track
+	print "p 1 165 " $3, isl * cs - $3
+	c = isl * cs;
+
+	# Second image partition (if any) also starts offset one 
+	# track to keep them identical.
+	if ($2 > 1) {
+		print "p 2 165 " $3 + c, isl * cs - $3
+		c += isl * cs;
+	}
+
+	# Config partition starts at cylinder boundary.
+	print "p 3 165 " c, csl * cs
+	c += csl * cs
+
+	# Data partition (if any) starts at cylinder boundary.
+	if ($7 > 0) {
+		print "p 4 165 " c, dsl * cs
+	} else if ($7 < 0 && $1 > c) {
+		print "p 4 165 " c, $1 - c
+	} else if ($1 < c) {
+		print "Disk space overcommitted by", \
+		    c - $1, "sectors" > "/dev/stderr"
+		exit 2
+	}
+
+	# Force slice 1 to be marked active. This is necessary
+	# for booting the image from a USB device to work.
+	print "a 1"
+}
 	' > ${MAKEOBJDIRPREFIX}/_.fdisk
 
 	IMG=${MAKEOBJDIRPREFIX}/nanobsd.full.img
