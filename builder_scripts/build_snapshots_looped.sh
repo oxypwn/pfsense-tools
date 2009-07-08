6970c3c13697f3de0d6b36a64f03aeb8c3a68def
Scott Ullrich <sullrich@pfsense.org>
2009-07-08 11:39:01 -0400

    Use PWD

--- builder_scripts/build_snapshots_looped.sh
+++ builder_scripts/build_snapshots_looped.sh
@@ -67,9 +67,9 @@ while [ /bin/true ]; do
 		;;
 	esac
 	echo $NEW_NANO_SIZE > /tmp/nanosize.txt
-	cat pfsense_local.sh | grep -v FLASH_SIZE > /tmp/pfsense_local.sh
+	cat $PWD/pfsense_local.sh | grep -v FLASH_SIZE > /tmp/pfsense_local.sh
 	echo 'export FLASH_SIZE="${NEW_NANO_SIZE}"' >>/tmp/pfsense_local.sh
-	mv /tmp/pfsense_local.sh ./pfsense_local.sh
+	mv /tmp/pfsense_local.sh $PWD/pfsense_local.sh
 	sh ./build_snapshots.sh
 	# Grab a random value and sleep
 	value=`od -A n -d -N1 /dev/random | awk '{ print $1 }'`
