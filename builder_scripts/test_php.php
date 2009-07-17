90f458af2da17289eddd4096f9b8a9bd769271c9
Scott Ullrich <sullrich@pfSense.homeunix.net>
2009-07-17 18:34:52 -0400

    Adding a script that the builder system will use to chroot into the build environment to ensure php is sane.  We will fail the build if PHP is not OK.

---
+++ builder_scripts/test_php.php
@@ -0,0 +1,30 @@
+#!/usr/local/bin/php
+
+<?php
+
+/*
+     This script will chroot via the builder system to test
+     the local php setup.  If we can perform a series of 
+     small tests to ensure the php environment is sane.
+*/
+
+require("functions.inc");
+require("config.inc");
+require("interfaces.inc");
+require("filter.inc");
+
+$config = parse_config(true);
+
+$passed_tests = true;
+
+// Test config.inc
+if(!$config['system']['hostname'] <> "pfSense")	
+	$passed_tests = false;
+
+if($passed_tests) 
+	exit(0);
+
+// Tests failed.
+exit(1);
+
+?>
