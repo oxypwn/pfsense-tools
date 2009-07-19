8e0abc1b37e437f7537608a04d0ec708627fdd82
Scott Ullrich <sullrich@pfSense.homeunix.net>
2009-07-19 13:28:37 -0400

    RELENG_8-supfile already exists, do not copy.

--- builder_scripts/builder_profiles/freebsd_only7/setup_overlay.sh
+++ builder_scripts/builder_profiles/freebsd_only7/setup_overlay.sh
@@ -3,7 +3,6 @@
 TOOLSDIR=/home/pfsense/tools/builder_scripts/
 OVERLAY_PATH="$TOOLSDIR/builder_profiles/freebsd_only7"
 
-cp $OVERLAY_PATH/RELENG_8-supfile	   $TOOLSDIR
 cp $OVERLAY_PATH/pfsense_local.sh    $TOOLSDIR
 cp $OVERLAY_PATH/pfsense-build.conf  $TOOLSDIR
 
--- builder_scripts/builder_profiles/freebsd_only8/setup_overlay.sh
+++ builder_scripts/builder_profiles/freebsd_only8/setup_overlay.sh
@@ -3,7 +3,6 @@
 TOOLSDIR=/home/pfsense/tools/builder_scripts/
 OVERLAY_PATH="$TOOLSDIR/builder_profiles/freebsd_only8/"
 
-co $OVERLAY_PATH/RELENG_8-supfile
 cp $OVERLAY_PATH/pfsense_local.sh    $TOOLSDIR
 cp $OVERLAY_PATH/pfsense-build.conf  $TOOLSDIR
 
