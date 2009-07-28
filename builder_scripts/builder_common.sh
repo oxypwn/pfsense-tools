feeaef3f103c75bf181ebdfb491e58ceed816214
Scott Ullrich <sullrich@pfsense-org.local>
2009-07-27 21:14:40 -0400

    Add missing fi

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -2386,8 +2386,8 @@ install_pkg_install_ports() {
 		if [ ! -d $PORTDIRPFS ]; then 
 			echo "!!!! Could not locate $PORTDIRPFS"
 			print_error_pfS
-		files
-		(cd $PORTDIRPFS && make clean) | egrep -wi '(^>>>|error)'
-		(cd $PORTDIRPFS && make install DESTDIR=$PFSENSEBASEDIR) | egrep -wi '(^>>>|error)'
+			(cd $PORTDIRPFS && make clean) | egrep -wi '(^>>>|error)'
+			(cd $PORTDIRPFS && make install DESTDIR=$PFSENSEBASEDIR) | egrep -wi '(^>>>|error)'
+		fi
 	done
 }
