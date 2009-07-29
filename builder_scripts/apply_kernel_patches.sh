74e0ffceda61a24daa0c5985d7b8e574dd26c65d
Scott Ullrich <sullrich@pfsense-org.local>
2009-07-29 17:50:28 -0400

    Remove MAKEOBJDIRPREFIX/.done_buildworld when applying patches by hand

--- builder_scripts/apply_kernel_patches.sh
+++ builder_scripts/apply_kernel_patches.sh
@@ -10,4 +10,8 @@ CURRENTDIR=`pwd`
 [ -r "${CURRENTDIR}/pfsense_local.sh" ] && . ${CURRENTDIR}/pfsense_local.sh
 [ -r "${CURRENTDIR}/builder_common.sh" ] && . ${CURRENTDIR}/builder_common.sh
 
+if [ -f $MAKEOBJDIRPREFIX/.done_buildworld ]; then
+	echo ">>> Removing $MAKEOBJDIRPREFIX/.done_buildworld"
+	rm $MAKEOBJDIRPREFIX/.done_buildworld
+fi
 update_freebsd_sources_and_apply_patches
