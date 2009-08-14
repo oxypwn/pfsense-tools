2d60b06ea97c4d67b1fe0bb547f70375b2f0860f
Scott Ullrich <sullrich@pfsense.org>
2009-08-14 16:05:10 -0400

    Use patches.RELENG_2_0 for RELENG_2_0 builds which defaults to FreeBSD 7.2.

--- builder_scripts/set_version.sh
+++ builder_scripts/set_version.sh
@@ -151,7 +151,7 @@ RELENG_2_0)
 	export PFSENSE_VERSION=2.0-ALPHA-ALPHA
 	export PFSENSETAG=HEAD
 	export PFSPATCHDIR=${BUILDER_TOOLS}/patches/RELENG_7_2
-	export PFSPATCHFILE=${BUILDER_TOOLS}/builder_scripts/patches.RELENG_7_2
+	export PFSPATCHFILE=${BUILDER_TOOLS}/builder_scripts/patches.RELENG_2_0
 	export CUSTOM_COPY_LIST="${BUILDER_TOOLS}/builder_scripts/copy.list.RELENG_2"	
 	export PFSPORTSFILE=buildports.RELENG_2_0
 	set_items
