a4901a8eefb5bc3bb1efe47a0e7d7aca299c5df8
Scott Ullrich <sullrich@su.local>
2009-09-17 17:16:23 -0400

    Revise comments

--- captiveportal/common.php
+++ captiveportal/common.php
@@ -1,16 +1,37 @@
 <?php
 
+//  Debug mode?
 $debug = false;
+
+// Interface that captive portal tests will be occuring on
 $nic = "em1";
-$ips = "254";
+
+// Number of IP addresses to add.
+$ips = "250";
+
+// IP address of aliases that are going to be added (testing client ips)
 $ip = "192.168.1.";
+
+// IP to start requests from
 $starting = "3";
+
+// URL of captive portal management
 $url = "http://192.168.1.1:8000";
-$iterations = "1";
+
+// Number of iterations to run before exiting test script
+$iterations = "5";
+
+// Sleep time between iterations
 $sleep_between_iterations = "0";
+
+// Number of wget retries if failure
 $tries = "1"; // wget tries
 $timeout = "4"; // wget timeout 
+
+// Sleep between captive portal requests
 //$sleep_between = "30";
+
+// Post data used to auth w/ captive portal
 $post_data = "auth_user=&auth_pass=&redirurl=http%3A%2F%2Fgoogle.com%2F&accept=Continue";
 
 ?>
