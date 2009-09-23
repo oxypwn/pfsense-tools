6e9f6eeee0386167420473509b5ae8e338798d63
Scott Ullrich <sullrich@pfsense.org>
2009-09-23 19:15:06 -0400

    Source the tools/builder_scripts/pfsense_local.sh now

--- builder_scripts/builder_profiles/pfDNS/setup_overlay.sh
+++ builder_scripts/builder_profiles/pfDNS/setup_overlay.sh
@@ -4,7 +4,7 @@ TOOLSDIR=/home/pfsense/tools/builder_scripts/
 
 # Source pfsense_local.sh variables if possible
 if [ -f ./pfsense_local.sh ]; then
-	. ./pfsense_local.sh
+	. ../../pfsense_local.sh
 fi
 
 cp pfsense-build.conf  $TOOLSDIR
