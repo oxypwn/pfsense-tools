bfd7f35827114ec2c51104c0d72dd0611dae9f50
Scott Ullrich <sullrich@pfsense.org>
2009-10-14 02:15:46 -0400

    Author responded, update his info and lic. (phk beer-ware)

--- pfPorts/alix_reset_button/files/alix_reset_button.c
+++ pfPorts/alix_reset_button/files/alix_reset_button.c
@@ -1,8 +1,15 @@
 /*
  * User-space reset-button support for PC Engines ALIX boards.
- * This is a prototype.
- * Written by syso.fml@ext.no-route.org
- *
+ * 
+ * Written by Sylwester Sosnowski fpm@no-route.org
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+ */
+
  * Usage:
  * Use from rc(8).
  */
