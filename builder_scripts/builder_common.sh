f119d7dd4413c7efaa6993949ac02a2f30a6cdb4
Scott Ullrich <sullrich@pfsense.org>
2009-10-28 17:46:00 -0400

    Set NANO_MD_BACKING

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -1989,6 +1989,7 @@ create_mips_diskimage()
 
 	NANO_MAKEFS="makefs -B big \
 		-o bsize=4096,fsize=512,density=8192,optimization=space"
+	NANO_MD_BACKING="swap"
 
 	pprint 2 "build diskimage"
 	pprint 3 "log: ${MAKEOBJDIRPREFIXFINAL}/_.di"
