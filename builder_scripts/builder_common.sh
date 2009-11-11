818b4379e4e7499578fe345747738017e1193a4a
Scott Ullrich <sullrich@pfsense.org>
2009-11-10 22:53:07 -0500

    Include ipmi kernel module

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -1612,7 +1612,7 @@ fi
 	printf "CROSS_COMPILE_PORTS_BINARIES: %s\n" $CROSS_COMPILE_PORTS_BINARIES
 	printf "            SPLIT_ARCH_BUILD: %s\n" $SPLIT_ARCH_BUILD
 if [ -n "$SHOW_ENV" ]; then
-	for LINE in `env | grep -v "PASS" | grep -v "NAME" | grep -v "USER" | grep -v "SSH" | grep -v "GROUP" | grep -v "HOST"`; do
+	for LINE in `env | grep -v "terminal" | grep -v "PASS" | grep -v "NAME" | grep -v "USER" | grep -v "SSH" | grep -v "GROUP" | grep -v "HOST"`; do
 		echo "SHOW_ENV: $LINE"
 	done
 fi
