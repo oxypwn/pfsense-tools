6f38ffd8ef980ffc1fd97b7c4855f22678561b64
Ermal <eri@pfsense.org>
2010-03-23 19:02:36 +0000

    Refactore check_reload_status to the new age. It will check every command for the exact timeout specified for it so system behaviour is predicted. TODO: Adding a config file.

--- pfPorts/check_reload_status/files/check_reload_status.c
+++ pfPorts/check_reload_status/files/check_reload_status.c
@@ -1,7 +1,7 @@
-
 /*
  *   check_reload_status.c
  *   part of the pfSense project
+ *   (C) 2010 Ermal Luçi
  *   (C)2005 Scott Ullrich
  *   All rights reserved
  *
@@ -19,133 +19,148 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <syslog.h>
+#include <unistd.h>
 
+#include <fcntl.h>
+
+#include <pthread.h>
+
+#define filepath 	"/tmp/check_reload_status"
 #define LOGFILE		"/var/log/check_reload_status"
 #define TMPDIR		"/tmp"
 
-/* Default cycle time value 1 minute */
-#define CYCLE		5
+/* Default cycle time value 30 seconds*/
+#define CYCLE		30
 
-static char _sccsid[] = { " $Id$ " };
+/* used for writing to the status file */
+int status = -1;
+#define NONE	0
+#define BEFORE	1
+#define AFTER	2
+
+/* function definitions */
+void	*run_command(void *arg);
 
 /* Check if file exists */
-int fexist(char * filename)
+static int
+fexist(char * filename)
 {
-  struct stat buf;
+	struct stat buf;
 
-  if (( stat (filename, &buf)) < 0)
-    return (0);
+	if (( stat (filename, &buf)) < 0)
+		return (0);
 
-  if (! S_ISREG(buf.st_mode)) {
-    return (0);
-  }
+	if (! S_ISREG(buf.st_mode))
+		return (0);
 
-  return(1);
+	return(1);
+}
+
+static void 
+write_status(char *statusline, int when) {
+
+	ftruncate(status, (off_t)0);
+	if (when == BEFORE)
+		dprintf(status, "Starting %s\n", statusline);
+	else if (when == AFTER)
+		dprintf(status, "After %s\n", statusline);
+	else
+		dprintf(status, "%s\n", statusline);
+}
+
+/* Various commands we support */
+struct commands {
+	char	*file;
+	char	*command;
+	char	*syslog;
+	int	sleep; /* in seconds */
+} known[] = {
+	{ "/tmp/restart_webgui",	"/usr/local/bin/php /etc/rc.restart_webgui",
+		"webConfigurator restart in progress", CYCLE},
+	{ "/tmp/rc.linkup", 		"/etc/rc.linkup.sh", "rc.linkup starting", CYCLE},
+	{ "/tmp/rc.newwanip",		"/usr/local/bin/php /etc/rc.newwanip `cat /tmp/rc.newwanip`",
+		"rc.newwanip starting", 10},
+	{ "/tmp/filter_dirty",		"/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.filter_configure_sync",
+		"reloading filter", 10},
+	{ "/tmp/filter_sync",		"/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.filter_synchronize",
+		"syncing firewall", CYCLE},
+	{ "/tmp/reload_all",		"/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.reload_all",
+		"reloading all", CYCLE},
+	{ "/tmp/reload_interfaces",	"/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.reload_interfaces",
+		"reloading interfaces", CYCLE}, 
+	{ "/tmp/update_dyndns",		"/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.dyndns.update `/bin/cat /tmp/update_dyndns`",
+		"updating dyndns", 20},
+	{ "/tmp/interface_configure",	"/usr/bin/nice -n20 /usr/local/bin/php /etc/interfaces_wan_configure `/bin/cat /tmp/interface_configure`",
+		"configuring interface", 20},
+	{ "/tmp/start_sshd",		"/usr/bin/nice -n20 /etc/sshd",
+		"starting sshd", CYCLE},
+	{ "/tmp/start_ntpd",		"/usr/bin/killall ntpd; /bin/sleep 3; /usr/local/sbin/ntpd -s -f /var/etc/ntpd.conf",
+		"starting ntpd", CYCLE}
+};
+
+void *
+run_command(void *arg) {
+	struct commands *cmd = arg;
+        int howmany, i;
+
+	for (;;) {
+        	if (fexist(cmd->file) == 1) {
+        		syslog(LOG_NOTICE, cmd->syslog);
+                	write_status(cmd->file, BEFORE);
+                	unlink(cmd->file);
+                	system(cmd->command);
+                	write_status(cmd->file, AFTER);
+        	}
+		if (cmd->sleep)
+			__sleep(cmd->sleep);
+		else
+			__sleep(CYCLE);
+	}
+        return NULL;
 }
 
 int main(void) {
-	int cycle_time;
-	cycle_time = CYCLE;
+	pthread_t *threads;
+        int threrr = 0, err = 0, i, howmany;
+
+
 	/* daemonize */
-	syslog(LOG_NOTICE, "check_reload_status is starting");
-	system("echo starting > /tmp/check_reload_status");
-	  /* loop forever until the cows come home */
-	  while(1) {
-	      if(fexist("/tmp/restart_webgui") == 1) {
-	      	syslog(LOG_NOTICE, "webConfigurator restart in progress");
-	        system("echo webConfigurator_Restart_Sleep > /tmp/check_reload_status");
-			sleep(5);
-			system("echo /tmp/restartwebgui > /tmp/check_reload_status");
-			system("/bin/rm /tmp/restart_webgui");
-			system("echo after /tmp/restartwebgui > /tmp/check_reload_status");
-			system("/usr/local/bin/php /etc/rc.restart_webgui");
-	      }
-	      if(fexist("/tmp/rc.linkup") == 1) {
-	      	  syslog(LOG_NOTICE, "rc.linkup starting");
-			  system("echo /tmp/rc.linkup > /tmp/check_reload_status");
-		      system("/etc/rc.linkup.sh");
-		      system("echo after /tmp/rc.linkup > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/rc.linkup");
-	      }
-	      if(fexist("/tmp/rc.newwanip") == 1) {
-		      syslog(LOG_NOTICE, "rc.newwanip starting");
-			  system("echo /tmp/rc.newwanip > /tmp/check_reload_status");
-		      system("/usr/local/bin/php /etc/rc.newwanip `cat /tmp/rc.newwanip`");
-		      system("echo after /tmp/rc.newwanip > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/rc.newwanip");
-	      }
-	      if(fexist("/tmp/filter_dirty") == 1) {
-		      syslog(LOG_NOTICE, "reloading filter");
-		      system("echo reloading_filter > /tmp/check_reload_status");
-		      system("/bin/rm -f /tmp/filter_dirty");
-		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.filter_configure_sync");
-		      system("echo after reloading_filter > /tmp/check_reload_status");
-	      }
-		if(fexist("/tmp/filter_sync") == 1) {
-                      syslog(LOG_NOTICE, "syncing firewall");
-                      system("echo filter_syncing > /tmp/check_reload_status");
-                      system("/bin/rm -f /tmp/filter_sync");
-                      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.filter_synchronize");
-                      system("echo after filter_syncing > /tmp/check_reload_status");
-              }
-	      if(fexist("/tmp/reload_all") == 1) {
-		      syslog(LOG_NOTICE, "reloading all");
-			  system("echo /tmp/reload_all > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/reload_all");
-		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.reload_all");
-		      system("echo after /tmp/reload_all > /tmp/check_reload_status");
-	      }
-	      if(fexist("/tmp/reload_interfaces") == 1) {
-	      	  syslog(LOG_NOTICE, "reloading interfaces");
-			  system("echo /tmp/reload_interfaces > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/reload_interfaces");
-		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.reload_interfaces");
-		       system("echo after /tmp/reload_interfaces > /tmp/check_reload_status");
-	      }
-	      if(fexist("/tmp/update_dyndns") == 1) {
-		      syslog(LOG_NOTICE, "updating dyndns");
-			  system("echo /tmp/update_dyndns > /tmp/check_reload_status");
-		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/rc.dyndns.update `cat /tmp/update_dyndns`");
-		      system("echo after /tmp/update_dyndns > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/update_dyndns");
-	      }
-	      if(fexist("/tmp/interfaces_wan_configure") == 1) {
-	      	  syslog(LOG_NOTICE, "configuring wan");
-			  system("echo /tmp/interfaces_wan_configure > /tmp/check_reload_status");
-		      system("/bin/rm  /tmp/interfaces_wan_configure");
-		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/interfaces_wan_configure");
-		      system("echo after /tmp/interfaces_wan_configure > /tmp/check_reload_status");
-	      }
-	      if(fexist("/tmp/interfaces_opt_configure") == 1) {
-	      	  syslog(LOG_NOTICE, "configuring opt");
-			  system("echo /tmp/interfaces_opt_configure > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/interfaces_opt_configure");
-		      system("/usr/bin/nice -n20 /usr/local/bin/php /etc/interfaces_opt_configure");
-		      system("echo after /tmp/interfaces_opt_configure > /tmp/check_reload_status");
-	      }
-	      if(fexist("/tmp/start_sshd") == 1) {
-		      syslog(LOG_NOTICE, "starting sshd");
-			  system("echo /tmp/start_sshd > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/start_sshd");
-		      system("/usr/bin/nice -n20 /etc/sshd");
-		      system("echo after /tmp/start_sshd > /tmp/check_reload_status");
-	      }
-	      if(fexist("/tmp/start_ntpd") == 1) {
-		      syslog(LOG_NOTICE, "starting ntpd");
-		      system("echo /tmp/start_ntpd > /tmp/check_reload_status");
-		      system("/bin/rm /tmp/start_ntpd");
-		      system("/usr/bin/killall ntpd");
-		      sleep(3);
-		      system("/usr/local/sbin/ntpd -s -f /var/etc/ntpd.conf");
-		      system("echo after /tmp/start_ntpd > /tmp/check_reload_status");
-	      }
-		  system("echo sleeping > /tmp/check_reload_status");
-	      sleep( cycle_time );
-	  }
-	  system("echo exiting > /tmp/check_reload_status");
-	syslog(LOG_NOTICE, "check_reload_status is stopping");
-	exit( 0 );
-	return( 0 );
+	if (daemon(0, 0) < 0) {
+		syslog(LOG_ERR, "check_reload_status could not start.");
+		err = 1;
+		goto error;
+	}
+
+	syslog(LOG_NOTICE, "check_reload_status is starting.");
+
+	status = open(filepath, O_RDWR | O_CREAT | O_FSYNC);
+	if (status < 0) {
+		syslog(LOG_ERR, "check_reload_status could not open file %s", filepath);
+		err = 2;
+		goto error;
+	}
+	write_status("starting", NONE);
+
+	howmany = sizeof(known)/ sizeof(known[0]);
+	dprintf(status, "%d commands present\n", howmany);
+	threads = malloc( howmany * sizeof(pthread_t));
+	if (threads == NULL) {
+		write_status("could not allocate memory for threads", NONE);
+		err = 4;
+		goto error;
+	}
+	for (i = 0; i < howmany; i++) {
+		threrr = pthread_create(&threads[i], NULL, run_command, (void *)&known[i]);
+		if (threrr != 0)
+			dprintf(status, "Could not create thread for command %s.", known[i].command);
+	}
+	for (i = 0; i < howmany; i++)
+		pthread_join(threads[i], NULL);
+
+error:
+	write_status("exiting", NONE);
+	syslog(LOG_NOTICE, "check_reload_status is stopping.");
+	close(status);
+
+	return (err);
 }
-
-
