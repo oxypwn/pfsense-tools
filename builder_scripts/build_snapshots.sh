276f1deeeb9cbfa7207b1d36a755420ca235ec1a
jim-p <jimp@pfsense.org>
2010-07-05 13:57:45 -0400

    Copy the buildtime file exactly instead of using echo.

--- builder_scripts/build_snapshots.sh
+++ builder_scripts/build_snapshots.sh
@@ -365,7 +365,7 @@ copy_to_staging_nanobsd() {
 	if [ -f $STAGINGAREA/nanobsdupdates/$FILENAMEUPGRADE.gz ]; then
 		cp $STAGINGAREA/nanobsdupdates/$FILENAMEUPGRADE.gz $STAGINGAREA/latest-nanobsd-$FILESIZE.img.gz 2>/dev/null
 		sha256 $STAGINGAREA/latest-nanobsd-$FILESIZE.img.gz > $STAGINGAREA/latest-nanobsd-$FILESIZE.img.gz.sha256 2>/dev/null
-		echo $BUILDTIME > $STAGINGAREA/version-nanobsd-$FILESIZE
+		cp $PFSENSEBASEDIR/etc/version.buildtime $STAGINGAREA/version-nanobsd-$FILESIZE
 	fi
 }
 
