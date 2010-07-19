198f8016f455ff540e618c083f5f93d25de09e5f
Scott Ullrich <sullrich@pfsense.org>
2010-07-19 18:01:40 -0400

    Zap the STABLE option, we do not have one currently

--- builder_scripts/set_version.sh
+++ builder_scripts/set_version.sh
@@ -211,22 +211,8 @@ RELENG_7_3)
 RELENG_8_1)
 	echo ">>> Setting builder environment to use RELENG_8_1 ..."
 	export FREEBSD_VERSION="8"
-	export FREEBSD_BRANCH="RELENG_8_0"
-	export SUPFILE="${BUILDER_TOOLS}/builder_scripts/RELENG_8-supfile"
-	export PFSENSE_VERSION=2.0-BETA3
-	export PFSENSETAG=HEAD
-	export PFSPATCHDIR=${BUILDER_TOOLS}/patches/RELENG_8_1
-	export PFSPATCHFILE=${BUILDER_TOOLS}/builder_scripts/patches.RELENG_8_1
-	export CUSTOM_COPY_LIST="${BUILDER_TOOLS}/builder_scripts/copy.list.RELENG_8_0"	
-	export PFSPORTSFILE=buildports.RELENG_2_0
-	set_items
-;;
-
-RELENG_8_STABLE)
-	echo ">>> Setting builder environment to use RELENG_8_1 ..."
-	export FREEBSD_VERSION="8"
 	export FREEBSD_BRANCH="RELENG_8_1"
-	export SUPFILE="${BUILDER_TOOLS}/builder_scripts/RELENG_8_1-supfile"
+	export SUPFILE="${BUILDER_TOOLS}/builder_scripts/RELENG_8-supfile"
 	export PFSENSE_VERSION=2.0-BETA3
 	export PFSENSETAG=HEAD
 	export PFSPATCHDIR=${BUILDER_TOOLS}/patches/RELENG_8_1
