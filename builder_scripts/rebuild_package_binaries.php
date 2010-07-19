b32b75c7c4185337439fb2f4772550b69ec8099b
Scott Ullrich <sullrich@pfsense.org>
2010-07-18 21:56:37 -0400

    FIx minor issue.  This tool now works

--- builder_scripts/rebuild_package_binaries.php
+++ builder_scripts/rebuild_package_binaries.php
@@ -4,12 +4,14 @@
 
 if(file_exists("/etc/inc/")) {
 	include("/etc/inc/functions.inc");
+	include("/etc/inc/util.inc");
 	include("/etc/inc/xmlparse.inc");
 }
 
 if(file_exists("/usr/home/pfsense/pfSenseGITREPO/pfSenseGITREPO")) {
-	include("/usr/home/pfsense/pfSenseGITREPO/pfSenseGITREPO/functions.inc");	
-	include("/etc/inc/xmlparse.inc");
+	include("/usr/home/pfsense/pfSenseGITREPO/pfSenseGITREPO/etc/inc/functions.inc");
+	include("/usr/home/pfsense/pfSenseGITREPO/pfSenseGITREPO/etc/inc/util.inc");
+	include("/usr/home/pfsense/pfSenseGITREPO/pfSenseGITREPO/etc/inc/xmlparse.inc");
 }
 
 function usage() {
@@ -23,24 +25,28 @@ if(!$argv[1])
 // Set the XML filename that we are processing
 $xml_filename = $argv[1];
 
-$pkg = parse_xml_config_pkg($xml_filename, "packagegui");
+$pkg = parse_xml_config_pkg($xml_filename, "pfsensepkgs");
 if(!$pkg) {
 	echo "An error occurred while trying to process {$xml_filename}.  Exiting.";
 	exit;
 }
 
-if(is_dir("/usr/ports")) {
-	echo ">>> /usr/ports not found.  Fetching...";
-	exec("portsnap fetch extract");
+echo ">>> pfSense package binary builder is starting.\n";
+
+if(!is_dir("/usr/ports")) {
+	echo "!!! /usr/ports/ not found.   Please run portsnap fetch extract\n";
+	exit;
 }
 
-if(is_dir("/usr/ports/packages/All")) 
+if(!is_dir("/usr/ports/packages/All")) 
 	mkdir("/usr/ports/packages/All");
 	
 foreach($pkg['packages']['package'] as $pkg) {
 	if($pkg['build_port_path']) {
-		echo ">>> Processing {$pkg['build_port_path']}\n";
-		exec("cd {$pkg['build_port_path']} && make clean package-recursive FORCE_PKG_REGISTER=yes");
+		foreach($pkg['build_port_path'] as $build) {
+			echo ">>> Processing {$build}\n";
+			exec("cd {$build} && make clean package-recursive FORCE_PKG_REGISTER=yes");
+		}
 	}
 }
 
