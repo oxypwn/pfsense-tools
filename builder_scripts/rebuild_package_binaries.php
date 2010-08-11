351ddf61d69113aedd78d35a0a5c8477873a90a3
Scott Ullrich <sullrich@pfsense.org>
2010-08-11 10:36:51 -0400

    Add ;

--- builder_scripts/rebuild_package_binaries.php
+++ builder_scripts/rebuild_package_binaries.php
@@ -92,7 +92,7 @@ foreach($pkg['packages']['package'] as $pkg) {
 			}
 			echo ">>> Processing {$build}";
 			if($build_options) 
-				echo " BUILD_OPTIONS: {$build_options}\n"
+				echo " BUILD_OPTIONS: {$build_options}\n";
 			else 
 				echo "\n";
 			`cd {$build} && make clean depends package-recursive BATCH=yes WITHOUT_X11=yes {$build_options} FORCE_PKG_REGISTER=yes </dev/null 2>&1`;
