27c76b385738f188e24f143fc3e0e9bdccea2952
Ermal <eri@pfsense.org>
2010-09-08 19:17:52 +0000

    Gather statistics from states as number of nat states/srcip/dstip/state rate. Only vm statistics remain to be able to gather all the statistics updaterrd.sh gathers.

--- pfPorts/pfStats/files/pfStats.c
+++ pfPorts/pfStats/files/pfStats.c
@@ -151,21 +151,152 @@ second_thread(void *arg __unused)
 		nanosleep(&ts, NULL);
 	}
 }
+
+void *
+third_thread(void *arg __unused)
+{
+	struct pfioc_states ps;
+	struct pf_status status;
+        struct pf_state *s;
+	struct pf_addr *srclist, *dstlist, *tmplist;
+	struct timespec ts;
+	time_t runtime;
+        char *buf = NULL;
+	int i, j, found;
+	double rate = 0;
+	/* Totally bogus size on initial allocation. */
+	int tsrc = 65535, tdst = 65535, isrc, idst, total, nat;
+
+	while((srclist = malloc(tdst * sizeof(*srclist))) == NULL)
+		;
+	while((dstlist = malloc(tdst * sizeof(*dstlist))) == NULL)
+		;
+
+	ts.tv_sec = 60;
+        ts.tv_nsec = 0;
+	
+	for (;;) {
+		bzero(&ps, sizeof(ps));
+		bzero(srclist, sizeof(srclist));
+		bzero(dstlist, sizeof(dstlist));
+		if (buf != NULL)
+			free(buf);
+		
+		if (ioctl(dev, DIOCGETSTATUS, &status)) {
+			printf("DIOCGETSTATUS");
+			goto sleep;
+		}
+		runtime = time(NULL) - status.since;
+		printf("  inserts %14llu %14.1f/s\n", (unsigned long long)status.fcounters[1],
+			(runtime) ? (double)status.fcounters[1] / (double)runtime : 0);
+		printf("  removals %14llu %14.1f/s\n", (unsigned long long)status.fcounters[2],
+			(runtime) ? (double)status.fcounters[2] / (double)runtime : 0);
+		rate = (runtime) ? (double)status.fcounters[1] / (double)runtime : 0;
+		rate += (runtime) ? (double)status.fcounters[2] / (double)runtime : 0;
+		printf("Rate is : %lli\n", (long long)rate);
+
+		if (ioctl(dev, DIOCGETSTATES, &ps) < 0) {
+			printf("Could not get states size");
+			goto sleep;
+		}
+		if (ps.ps_len) {
+			buf = malloc(ps.ps_len * 2);
+			if (buf == NULL)
+				continue;
+			ps.ps_buf = buf;
+		} else
+			goto sleep;
+		if (ioctl(dev, DIOCGETSTATES, &ps) < 0) {
+			printf("Could not get states");
+			free(buf);
+			goto sleep;
+		}
+
+		s = ps.ps_states;
+		total = ps.ps_len / sizeof(*s);
+		isrc = idst = nat = 0;
+        	for (i = 0; i < total; i++, s++) {
+			printf(".");
+			if (PF_ANEQ(&s->lan.addr, &s->gwy.addr, s->af) ||
+			    (s->lan.port != s->gwy.port))
+				nat++;
+			if (s->direction == PF_OUT) {
+				found = 0;
+				for (j = 0; j < tsrc; j++) {
+					if (PF_AEQ(&srclist[j], &s->lan.addr, s->af)) {
+						found = 1;
+						break;
+					}
+				}
+				if (found)
+					continue;
+				srclist[isrc++] = s->lan.addr;
+				if (isrc >= tsrc) {
+					tsrc *= 2;
+					while ((tmplist = calloc(tsrc, sizeof(*tmplist))) == NULL)
+						;
+					bcopy(srclist, tmplist, sizeof(srclist));
+					free(srclist);
+					srclist = tmplist;
+				}
+			} else {
+				found = 0;
+				for (j = 0; j < tdst; j++) {
+                                	if (PF_AEQ(&dstlist[j], &s->ext.addr, s->af)) {
+						found = 1;
+						break;
+					}
+                        	}
+				if (found)
+					continue;
+                        	srclist[idst++] = s->ext.addr;
+                        	if (idst >= tdst) {
+                                	tdst *= 2;
+                                	while ((tmplist = calloc(tdst, sizeof(*tmplist))) == NULL)
+                                        	;
+                                	bcopy(dstlist, tmplist, sizeof(dstlist));
+                                	free(dstlist);
+                                	dstlist = tmplist;
+                        	}
+			}
+        	}
+		printf("\n");
+		printf("Found total: %d, nat: %d, srcip: %d dstip: %d states\n",
+			total, nat, isrc, idst);
+sleep:
+		nanosleep(&ts, NULL);
+	}
+
+	if (buf != NULL)
+        	free(buf);
+	if (srclist != NULL)
+		free(srclist);
+	if (dstlist != NULL)
+		free(dstlist);
+
+        return (0);
+	
+
+}
+
 int
 main(int argc, char **argv)
 {
 
-	pthread_t first, second;
+	pthread_t first, second, third;
 
 	if ((dev = open(PFDEV, O_RDONLY)) < 0) {
 		printf("Could not open pf(4) device because %s\n", strerror(errno));
 		return (1);
 	}
+
 	pthread_create(&first, NULL, first_thread, NULL);
 	pthread_create(&second, NULL, second_thread, NULL);
+	pthread_create(&third, NULL, third_thread, NULL);
 	
 	pthread_join(first, NULL);
 	pthread_join(second, NULL);
+	pthread_join(third, NULL);
 
 	close(dev);
 
