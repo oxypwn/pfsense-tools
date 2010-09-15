e6f31702fcee4b71d30e3c7c099c55ca8f85da83
Ermal <eri@pfsense.org>
2010-09-15 22:22:31 +0000

    Properly define MODULES_OVERRIDE for the kernel build.

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -3112,7 +3112,7 @@ installworld() {
 	echo ">>> Builder is running the command: env $MAKE_ENV script -aq $LOGFILE make ${makeargs:-} installworld" > /tmp/freesbie_installworld_cmd.txt
 	# make installworld
 	(env $MAKE_ENV script -aq $LOGFILE make ${makeargs:-} installworld || print_error_pfS;) | egrep '^>>>'
-	makeargs="${MAKEOPT:-} SRCCONF=${SRC_CONF} ${MODULES_OVERRIDE:-} TARGET_ARCH=${ARCH} DESTDIR=${BASEDIR}"
+	makeargs="${MAKEOPT:-} SRCCONF=${SRC_CONF} MODULES_OVERRIDE=${MODULES_OVERRIDE:-} TARGET_ARCH=${ARCH} DESTDIR=${BASEDIR}"
 	set +e
 	echo ">>> Builder is running the command: env $MAKE_ENV script -aq $LOGFILE make ${makeargs:-} distribution"  > /tmp/freesbie_installworld_distribution_cmd.txt
 	# make distribution
@@ -3151,7 +3151,7 @@ buildkernel() {
 		echo ">>> DTRACE:      ${DTRACE}"
 	fi
 	unset EXTRA
-	makeargs="${MAKEOPT:-} ${MAKEJ_KERNEL:-} SRCCONF=${SRC_CONF} ${MODULES_OVERRIDE:-} TARGET_ARCH=${ARCH} ${DTRACE}"
+	makeargs="${MAKEOPT:-} ${MAKEJ_KERNEL:-} SRCCONF=${SRC_CONF} MODULES_OVERRIDE=${MODULES_OVERRIDE:-} TARGET_ARCH=${ARCH} ${DTRACE}"
 	echo ">>> Builder is running the command: env $MAKE_ENV script -aq $LOGFILE make $makeargs buildkernel" > /tmp/freesbie_buildkernel_cmd.txt
 	cd $SRCDIR
 	(env $MAKE_ENV script -aq $LOGFILE make $makeargs buildkernel NO_KERNELCLEAN=yo || print_error_pfS;) | egrep '^>>>'
