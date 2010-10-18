e9a67ef431f3a54201aadd552791e64c871be89c
Scott Ullrich <sullrich@pfsense.org>
2010-10-18 17:07:30 -0400

    Adding setup_overlay.sh

--- builder_scripts/builder_profiles/freebsd_only9/setup_overlay.sh
+++ builder_scripts/builder_profiles/freebsd_only9/setup_overlay.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
+# Set manually
 TOOLSDIR=/home/pfsense/tools/builder_scripts/
-OVERLAY_PATH="$TOOLSDIR/builder_profiles/freebsd_only9/"
 
-# Source pfsense_local.sh variables if possible
-if [ -f ./pfsense_local.sh ]; then
-	. ./pfsense_local.sh
-fi
+# Copy pfsense-build.conf into TOOLSDIR
+cp pfsense-build.conf	$TOOLSDIR
+cp remove.list  		$TOOLSDIR
+cp copy.list			$TOOLSDIR
 
-cp $OVERLAY_PATH/pfsense_local.sh    $TOOLSDIR
-cp $OVERLAY_PATH/pfsense-build.conf  $TOOLSDIR
+# cd $TOOLSDIR and read in pfsense_local.sh
+cd $TOOLSDIR
 
+. ./pfsense_local.sh
+
+# Ensure $SRCDIR exists
 mkdir -p $SRCDIR
 
-cd $TOOLSDIR
-./clean_build.sh
-./apply_kernel_patches.sh
+# Start building
+if [ "$1" != "noupdate" ]; then
+	echo ">>> noupdate flag NOT passed. Cleaning and updating GIT repo"
+	./clean_build.sh
+	./update_git_repos.sh
+	./apply_kernel_patches.sh
+fi
+./build_freebsdisoonly.sh
 
