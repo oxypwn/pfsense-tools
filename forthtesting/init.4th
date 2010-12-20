2f9004630d0b0f112bcb37ebed6b66ed6787f44e
jim-p <jimp@pfsense.org>
2010-12-20 16:31:57 -0500

    Add forthetesting/ to tools, cd there and ./setup_test.sh and it will setup a beastie.4th testing environment for you there.

---
+++ forthtesting/init.4th
@@ -0,0 +1,7 @@
+: boot drop exit ;
+: reboot drop exit ;
+
+load screen.4th
+load frames.4th
+load beastie.4th
+beastie-start
---
+++ forthtesting/setup_test.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+WD=`/bin/pwd`
+
+echo Making testmain
+cd /sys/boot/ficl
+/usr/bin/make clean && /usr/bin/make testmain
+cd $WD
+echo Copying testmain to $WD
+/bin/cp -p /sys/boot/ficl/testmain $WD
+
+if [ -f /usr/local/pfsense-fs/boot/beastie.4th ]; then
+	echo Copying 4th files from /usr/local/pfsense-fs/boot/
+	/bin/cp /usr/local/pfsense-fs/boot/beastie.4th $WD
+	/bin/cp /usr/local/pfsense-fs/boot/screen.4th $WD
+	/bin/cp /usr/local/pfsense-fs/boot/frames.4th $WD
+else
+	echo Copying 4th files from /usr/src/sys/boot/forth/
+	/bin/cp /usr/src/sys/boot/forth/beastie.4th $WD
+	/bin/cp /usr/src/sys/boot/forth/screen.4th $WD
+	/bin/cp /usr/src/sys/boot/forth/frames.4th $WD
+fi
+
+echo Deactivating includes in beastie.4th
+
+/usr/bin/sed -i.bak 's/^include/\/ include/' beastie.4th
+
+echo Hack at beastie.4th and then run:
+echo    ./testmain init.4th
