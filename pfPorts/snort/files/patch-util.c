8d1e7ec2d0c073b09e1133648a8e601182bb3f99
robiscool <robrob2626@yahoo.com>
2011-02-09 14:40:22 -0800

    snort, add snort 2.9.0.3 tools

---
+++ pfPorts/snort/files/patch-util.c
@@ -0,0 +1,15 @@
+--- src/util.c	2010-12-09 12:00:46.000000000 -0800
++++ src/util.c	2011-01-31 16:06:06.000000000 -0800
+@@ -199,6 +199,12 @@
+     LogMessage("           Using ZLIB version: %s\n", zlib_ver);
+ #endif
+     LogMessage("\n");
++	LogMessage("     ___   Built Date for Snort on Pfsense 2.0x86 is Feb. 9, 2011.\n");
++	LogMessage(" ___/ f \\  Orion IPS Patches Copyright (C) 2009-2010 Robert Zelaya.\n");
++	LogMessage("/ p \\___/Sense\n");
++	LogMessage("\\___/   \\\n");
++	LogMessage("    \\___/  Using Snort.org dynamic plugins and SnortSam 2.69.\n");
++	LogMessage("\n");
+ 
+     return 0;
+ }
