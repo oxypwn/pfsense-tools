abd798174869c19271793553cbe4e9704e23c992
jim-p <jimp@pfsense.org>
2011-02-21 15:21:14 -0500

    Add back (and relocate) some older versions of pfPorts to make 1.2 builds happier.

---
+++ pfPorts/dnswatch-old/files/dnswatch.c
@@ -0,0 +1,221 @@
+/*
+	$Id$
+	part of pfSense (http://www.pfsense.org)
+	originally part of m0n0wall (http://m0n0.ch/wall)
+	Copyright (C) 2009 Scott Ullrich <sullrich@pfsense.org>
+	Copyright (C) 2009 Ermal Luci <ermal@pfsense.org>
+	Copyright (C) 2007 Manuel Kasper <mk@neon1.net>.
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	1. Redistributions of source code must retain the above copyright notice,
+	   this list of conditions and the following disclaimer.
+	
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+	
+	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <libutil.h>
+#include <err.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <err.h>
+#include <sysexits.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <signal.h>
+
+#include <pthread.h>
+
+/*
+	Resolves each host name in a given list at regular intervals, and runs
+	a command whenever any of them resolves to a different IP address than
+	before.
+	
+	Usage:
+		dnswatch pidfile interval command hostfile
+		
+	interval is in seconds; for best results, set this to be slightly larger
+	than the TTL of the DNS records being watched
+*/
+
+static char *command = NULL;
+pthread_t *threads = NULL;
+static int hosts = 0;
+static int debug = 0;
+static int interval = 10;
+
+void usage(void) {
+	fprintf(stderr, "usage: dnswatch pidfile interval command hostfile\n");
+	exit(4);
+}
+
+void *check_hostname(void *arg)
+{
+	properties local = (properties) arg;
+	struct hostent *he;
+	struct in_addr *addr, ip;
+	struct timespec ts;
+
+	ts.tv_sec = interval;
+        ts.tv_nsec = 0;
+
+	if (!local->name)
+		return 0;
+
+	ip.s_addr = 0;
+	for (;;) {
+		he = gethostbyname2(local->name, AF_INET);
+		if (he == NULL) {
+			syslog(LOG_WARNING, "DNS lookup for host %s failed", local->name);
+		} else {
+			addr = (struct in_addr*)he->h_addr;
+			if (ip.s_addr != 0 && ip.s_addr != addr->s_addr) {
+				ip = *addr;
+				syslog(LOG_WARNING, "hostname %s ip changed to %s, reloading ipsec tunnel.",  local->name, inet_ntoa(*addr));
+				system(command);
+			}
+		}
+		nanosleep(&ts, NULL);
+	}
+}
+
+static void
+handle_signal(int sig)
+{
+        int i;
+
+        switch(sig) {
+        case SIGHUP:
+        case SIGTERM:
+                if (hosts) {
+                        for (i = 0; i < hosts; i++) {
+                                pthread_cancel(threads[i]);
+                        }
+                }
+                break;
+        default:
+                if (debug >= 3)
+                        syslog(LOG_WARNING, "unhandled signal");
+        }
+}
+
+int main(int argc, char *argv[]) {
+	char *file;
+	properties list, props;
+	int fd, i = 0, error = 0;
+	FILE *pidfd = NULL;
+	sig_t sig_error;
+
+	if (argc > 5 || argc < 4)
+		usage();
+
+	interval = atoi(argv[2]);
+	if (interval < 1) {
+		fprintf(stderr, "Invalid interval %d\n", interval);
+		exit(3);
+	}
+	
+	command = argv[3];
+	file = argv[4];
+
+	closefrom(1);
+
+	/* go into background */
+	if (daemon(0, 0) == -1)
+		exit(1);
+	
+	/* write PID to file */
+	pidfd = fopen(argv[1], "w");
+	if (pidfd) {
+		fprintf(pidfd, "%d\n", getpid());
+		fclose(pidfd);
+	}
+	
+	// Attempt to open configuration file which lists hosts
+	fd = open(file, O_RDONLY);
+	if (fd == -1) {
+		syslog(LOG_ERR, "unable to open configuration file '%s'", file);
+		exit(1);
+	}
+
+	// Read hostnames in that are in the configuration file
+	props = properties_read(fd);
+	if (props == NULL) {
+		syslog(LOG_ERR, "error reading configuration file");
+		exit(1);
+	}
+	
+	// Close open file handle for dnswatch configuration
+	close(fd);
+
+	/*
+         * Catch SIGHUP in order to reread configuration file.
+         */
+        sig_error = signal(SIGHUP, handle_signal);
+        if (sig_error == SIG_ERR)
+                err(EX_OSERR, "unable to set signal handler");
+        sig_error = signal(SIGTERM, handle_signal);
+        if (sig_error == SIG_ERR)
+                err(EX_OSERR, "unable to set signal handler");
+
+	list = props;
+	while (list != NULL) {
+		list = list->next;
+		hosts++;
+	}
+
+        threads = malloc(hosts * sizeof(pthread_t));
+        if (threads == NULL) {
+                syslog(LOG_ERR, "error while allocating memory");
+                properties_free(props);
+                exit(5);
+        }
+
+        memset(threads, 0, sizeof(threads) * hosts);
+
+        list = props;
+        i = 0;
+        while (list != NULL) {
+                error = pthread_create(&threads[i], NULL, check_hostname, list);
+                if (error != 0) {
+                        if (debug >= 1)
+                                syslog(LOG_ERR, "Unable to create monitoring thread for host %s", list->name);
+                }
+                i = i + 1;
+                list = list->next;
+        }
+        for (i = 0; i < hosts; i++)
+                pthread_join(threads[i], NULL);
+
+        if (props != NULL)
+                properties_free(props);
+        if (threads != NULL)
+                free(threads);
+
+	return 0;
+}
---
+++ pfPorts/sshlockout_pf-old/files/sshlockout_pf.c
@@ -0,0 +1,450 @@
+/*
+ *  v2.01
+ *  SSHLOCKOUT_PF.C 
+ *  Originally written by Matthew Dillon
+ *  Heavily modified to use PF tables by Scott Ullrich and
+ *  extened to keep a database of last 256 bad attempts 
+ *  (MAXLOCKOUTS) and block user if they go over (MAXATTEMPTS).
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *  
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *  
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ *  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Use: pipe syslog auth output to this program.  e.g. in /etc/syslog.conf:
+ *
+ *   auth.info;authpriv.info				/var/log/auth.log
+ *   auth.info;authpriv.info				|exec /path/to/sshlockout_pf
+ *
+ *  Detects failed ssh login and attempts to map out the originating IP
+ *  using PF's tables.
+ *
+ *  Setup instructions:
+ *   setup a rule in your pf ruleset (near the top) similar to:
+ *   table <sshlockout> persist
+ *   block in log quick from <sshlockout> to any label "sshlockout"
+ *
+ *  Some things to note:
+ *   1. *VERY* simplistic.  IP table entries do not timeout without expiretable
+ *   2. No checks are made for local IPs or nets, or for prior good logins, etc.
+ *   3. Use expiretable binary from cron to help prune the sshlockout table
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <time.h>
+
+// Non changable globals
+#define MAXATTEMPTS 10
+#define MAXLOCKOUTS 256
+#define VERSION	"2.0"
+
+// Wall of shame (invalid login DB)
+static struct sshlog 
+{
+	// IP ADDR Octets
+	int n1;
+	int n2;
+	int n3;
+	int n4;
+	// Invalid login attempts
+	int attempts;
+	// Last invalid timestamp
+	time_t ts;
+} lockouts[MAXLOCKOUTS + 1];
+
+// Function declarations
+static void lockout(char *str, char *lockouttable);
+static void lockout_remove(char *str, char *lockouttable);
+static void check_for_denied_string(char *str, char *lockouttable, char *buf);
+static void check_for_accepted_string(char *str, char *lockouttable, char *buf);
+static void prune_oldest_record(void);
+static void prune_record(int n1, int n2, int n3, int n4);
+static void add_new_record(int n1, int n2, int n3, int n4);
+static void prune_24hour_records(void);
+
+// Start of program - main loop
+int
+main(void) 
+{
+	char buf[1024] = { 0 };
+
+	// Initialize time conversion information
+	tzset();
+
+	// Open syslog file
+	openlog("sshlockout", LOG_PID|LOG_CONS, LOG_AUTH);
+
+	// We are starting up
+	syslog(LOG_ERR, "sshlockout/webConfigurator v%s starting up", VERSION);
+
+	// Open up stderr and stdout to the abyss
+	(void)freopen("/dev/null", "w", stdout);
+	(void)freopen("/dev/null", "w", stderr);
+
+	// Loop through reading in syslog stream looking for
+	// for specific strings that indicate that a user has
+	// attempted login but failed.
+	while (fgets(buf, (int)sizeof(buf), stdin) != NULL) 
+	{
+		/* if this is not sshd or webConfigurator related, continue on without processing */
+		if (strstr(buf, "sshd") == NULL && strstr(buf, "webConfigurator") == NULL)
+			continue;
+		// Check for various bad (or good!) strings in stream
+		check_for_denied_string("Failed password for root from", "sshlockout", buf);
+		check_for_denied_string("Failed password for admin from", "sshlockout",  buf);
+		check_for_denied_string("Failed password for invalid user", "sshlockout", buf);
+		check_for_denied_string("Illegal user", "sshlockout", buf);
+		check_for_denied_string("Invalid user", "sshlockout", buf);
+		check_for_denied_string("authentication error for", "sshlockout", buf);
+		check_for_denied_string("webConfigurator authentication error for", "webConfiguratorlockout", buf);
+		check_for_accepted_string("Successful webConfigurator login for user", "webConfiguratorlockout", buf);
+		check_for_accepted_string("Accepted keyboard-interactive/pam for", "sshlockout", buf);
+		// prune records older than 24 hours
+		prune_24hour_records();
+	}
+
+	// We are exiting
+	syslog(LOG_ERR, "sshlockout/webConfigurator v%s exiting", VERSION);
+
+	// That's all folks.
+	return(0);
+}
+
+// Prune records older than 24 hours
+static void prune_24hour_records(void) 
+{
+
+	// Loop tracking variable
+	int i = 0;
+
+	// Time tracker (unix epoch)
+	time_t now = 0;
+	time_t ts = 0;
+
+	// Init
+    now = time(NULL);
+
+	// Track the oldest record id.
+	// Set the known oldest to now.
+	ts = time(&now);
+
+	while(i < MAXLOCKOUTS) 
+	{
+		// Check to see if item is older than
+		// the oldest entry found thus far.
+		if(lockouts[i].ts - ts > 86400) {
+			lockouts[i].n1 = 0;
+			lockouts[i].n2 = 0;
+			lockouts[i].n3 = 0;
+			lockouts[i].n4 = 0;
+		}
+		i++;
+	}
+}
+
+
+// Check for passed string and lockout the 
+// host if we find the log message in stream.
+static void
+check_for_denied_string(char *str, char *lockouttable, char *buf)
+{
+	char *tmpstr = NULL;
+	if ((str = strstr(buf, str)) != NULL) 
+	{
+		if ((tmpstr = strstr(str, " from")) != NULL) {
+			if (strlen(tmpstr) > 5)
+				lockout(tmpstr + 5, lockouttable);
+		}
+	}
+}
+
+// Check for accepted string and remove the 
+// host from the local database if found.
+static void
+check_for_accepted_string(char *str, char *lockouttable, char *buf)
+{
+	char *tmpstr = NULL;
+	if ((str = strstr(buf, str)) != NULL) 
+	{
+		if ((tmpstr = strstr(str, " from")) != NULL) {
+			if (strlen(tmpstr) > 5)
+				lockout_remove(tmpstr + 5, lockouttable);
+		}
+	}
+}
+
+// Loop through and remove the oldest entry
+static void 
+prune_oldest_record(void) 
+{
+	// Loop counter, status of record in db
+	int i = 0;
+	int oldestrecord = 0;
+
+	// Time tracker (unix epoch)
+	time_t now = 0;
+	time_t ts = 0;
+
+	// Init
+    now = time(NULL);
+
+	// Track the oldest record id.
+	// Set the known oldest to now.
+	ts = time(&now);
+
+	// Loop until we hit MAXLOCKOUTS
+	// looking for an emty slot
+	while(i < MAXLOCKOUTS) 
+	{
+		// Check to see if item is older than
+		// the oldest entry found thus far.
+		if(lockouts[i].ts < ts) 
+			oldestrecord = i;
+		// Increase by one
+		i++;
+	}
+
+	// Clear the oldest record (oldestrecord)
+	lockouts[oldestrecord].n1 = 0;
+	lockouts[oldestrecord].n2 = 0;
+	lockouts[oldestrecord].n3 = 0;
+	lockouts[oldestrecord].n4 = 0;
+	lockouts[oldestrecord].attempts = 0;
+}
+
+// Add new IP address to DB 
+static void 
+add_new_record(int n1, int n2, int n3, int n4) 
+{
+	// Loop counter, status of record in db
+	int foundrecord = 0, i = 0;
+
+	// Time tracker (unix epoch)
+	time_t now;
+
+	// Loop until we hit MAXLOCKOUTS
+	// looking for an empty slot
+	while(i < MAXLOCKOUTS && foundrecord == 0) 
+	{
+		// Look for the IP in the DB
+		if(lockouts[i].n1 == 0 &&
+			lockouts[i].n2 == 0 &&
+			lockouts[i].n3 == 0 &&
+			lockouts[i].n4 == 0) 
+		{
+			foundrecord = 1;
+			break;
+		}
+		// If we did not find an empty slot
+		// go ahead and prune a record and
+		// start the empty slot search again.
+		if(i == MAXLOCKOUTS) {
+			prune_oldest_record();
+			// Set to -1, it will ++ below (+3)
+			i = -1;
+		}
+		// Increase by one
+		i++;
+	}
+
+	// Grab the time
+    now = time(NULL);
+
+	// Add item to DB
+	lockouts[i].n1 = n1;
+	lockouts[i].n2 = n2;
+	lockouts[i].n3 = n3;
+	lockouts[i].n4 = n4;
+
+	// Add last seen epoch
+	lockouts[i].ts = time(&now);
+}
+
+// Record last IP date
+static void 
+record_event(int n1, int n2, int n3, int n4) 
+{
+	// Loop counter, status of record in db
+	int i = 0;
+
+	// Time tracker (unix epoch)
+	time_t now;
+
+	// Loop until we hit MAXLOCKOUTS
+	while(i < MAXLOCKOUTS) 
+	{
+		// Look for the IP in the DB
+		if(lockouts[i].n1 == n1 &&
+			lockouts[i].n2 == n2 &&
+			lockouts[i].n3 == n3 &&
+			lockouts[i].n4 == n4) 
+		{
+			// Update the entries epoch
+    		now = time(NULL);
+			lockouts[i].ts = time(&now);
+			// Note the invalid login attempt
+			lockouts[i].attempts++;
+			break;
+		}
+		// Increase by one
+		i++;
+	}
+}
+
+// Remove specific record from DB (IPADDR)
+static void 
+prune_record(int n1, int n2, int n3, int n4) 
+{
+	// Loop counter, status of record in db
+	int  i = 0;
+
+	// Loop until we hit MAXLOCKOUTS
+	while(i < MAXLOCKOUTS) 
+	{
+		// Look for the IP in the DB
+		if(lockouts[i].n1 == n1 &&
+			lockouts[i].n2 == n2 &&
+			lockouts[i].n3 == n3 &&
+			lockouts[i].n4 == n4) 
+		{
+			// Reset the DB entry
+			lockouts[i].n1 = 0;
+			lockouts[i].n2 = 0;
+			lockouts[i].n3 = 0;
+			lockouts[i].n4 = 0;
+			lockouts[i].attempts = 0;
+			break;
+		}
+		// Increase by one
+		i++;
+	}
+}
+
+// Check string for badness and lockout IPADDR
+// if we find the specified string in stream 
+// and if the attempt account is == MAXATTEMPTS
+static void
+lockout(char *str, char *lockouttable)
+{
+	// IP address octets
+	int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+
+	// Error tracker
+	int ret = 0;
+
+	// Loop counter, status of record in db
+	int i = 0, foundrecord = 0;
+
+	// system() handler variable
+	char buf[256];
+
+	// Variable to track if we are blocking or adding
+	// to database to track invalid logins.
+	int shouldblock = 0;
+
+	// Check passed string and parse out the IP address
+	// If we cannot find a IP address then simply return.
+	if (sscanf(str, "%d.%d.%d.%d", &n1, &n2, &n3, &n4) > 4 || 
+		sscanf(str, "%d.%d.%d.%d", &n1, &n2, &n3, &n4) < 4) 
+			return;
+
+	// Track if we have found the record
+	foundrecord = 0;
+
+	// Check to see if hosts IP is in our lockout table checking
+	// how many attempts.   If the attempts are over 3 then 
+	// purge the host from the table and leave shouldblock = true
+	while(i < MAXLOCKOUTS) 
+	{
+		// Try to find the IP in DB
+		if(lockouts[i].n1 == n1 &&
+			lockouts[i].n2 == n2 &&
+			lockouts[i].n3 == n3 &&
+			lockouts[i].n4 == n4) 
+		{
+			// Found the record, record the attempt
+			record_event(n1, n2, n3, n4);
+			foundrecord = 1;
+			// Check to see if user is above or == MAXATTEMPTS
+			if(lockouts[i].attempts >= MAXATTEMPTS) 
+			{
+				// Block the host
+				shouldblock = 1;
+				break;
+			}
+		}
+		// Increase by one
+		i++;
+	}
+
+	// Entry not found, lets add it to the DB
+	if(foundrecord == 0)
+		add_new_record(n1, n2, n3, n4);
+
+	// If shouldblock is still true go ahead and block the offender
+	if(shouldblock == 1)
+	{
+		// Remove the record we are going to block this host.
+		prune_record(n1, n2, n3, n4);
+
+		// Notify syslog of the host being blocked (IPADDR)
+		syslog(LOG_ERR, "Locking out %d.%d.%d.%d after %i invalid attempts\n", \
+			n1, n2, n3, n4, MAXATTEMPTS);
+
+		// Setup buf with the pfctl command needed to block HOST
+		ret = snprintf(buf, sizeof(buf), "/sbin/pfctl -t %s -T add %d.%d.%d.%d", \
+			lockouttable, n1, n2, n3, n4);
+		// Check for error condition
+		if(ret < 0) 
+		{
+			syslog(LOG_ERR, "Error Locking out %d.%d.%d.%d while allocating snprintf()\n", \
+				n1, n2, n3, n4, MAXATTEMPTS);
+			return;
+		}
+
+		// Execute the command in buf
+		ret = system(buf);
+		// Check for error condition
+		if(ret != 0)
+			syslog(LOG_ERR, "Error Locking out %d.%d.%d.%d while launching pfctl\n", \
+				n1, n2, n3, n4, MAXATTEMPTS);
+	}
+}
+
+// Remove host from logging dabtase
+static void
+lockout_remove(char *str, char *lockouttable)
+{
+	// IP address octets
+	int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+
+	// Check passed string and parse out the IP address
+	// If we cannot find a IP address then simply return.
+	if (sscanf(str, "%d.%d.%d.%d", &n1, &n2, &n3, &n4) > 4 || 
+		sscanf(str, "%d.%d.%d.%d", &n1, &n2, &n3, &n4) < 4) 
+			return;
+
+	// User auth'd.  Remove previous lockout db entries.
+	prune_record(n1, n2, n3, n4);
+}
