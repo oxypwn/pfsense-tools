8b421c93dfc51aa5be35dd91906ea5578687b813
Scott Ullrich <sullrich@pfsense.org>
2011-05-12 16:18:43 -0400

    Add read-only github urls

--- builder_scripts/pfsense_local.sh
+++ builder_scripts/pfsense_local.sh
@@ -223,10 +223,10 @@ export CUSTOM_REMOVE_LIST=${CUSTOM_REMOVE_LIST:-"${BUILDER_SCRIPTS}/remove.list.
 
 # GIT pfSense, BSDInstaller & FreeSBIE GIT repo settings
 export USE_GIT=${USE_GIT:-"yo"}
-export GIT_REPO=${GIT_REPO:-"git@github.com:bsdperimeter/pfsense.git"}
+export GIT_REPO=${GIT_REPO:-"https://github.com/bsdperimeter/pfsense.git"}
 export GIT_REPO_DIR="${BASE_DIR}/pfSenseGITREPO"
 export GIT_REPO_FREESBIE2=${GIT_REPO_FREESBIE2:-"http://gitweb.pfsense.org/freesbie2/mainline.git"}
-export GIT_REPO_TOOLS=${GIT_TOOLS_REPO:-"git@github.com:bsdperimeter/pfsense-tools.git tools"}
+export GIT_REPO_TOOLS=${GIT_TOOLS_REPO:-"https://github.com/bsdperimeter/pfsense-tools.git tools"}
 #export GIT_REPO_BSDINSTALLER=${GIT_REPO_BSDINSTALLER:-"http://gitweb.pfsense.org/bsdinstaller/mainline.git"}
 
 # Custom overlay for people building or extending pfSense images.
