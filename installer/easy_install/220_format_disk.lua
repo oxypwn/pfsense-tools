9f97c60b46024469fdf2f03540f177f6c76ac362
Scott Ullrich <sullrich@gmail.com>
2011-07-01 19:24:44 -0400

    Clear GPART partitions

--- installer/easy_install/220_format_disk.lua
+++ installer/easy_install/220_format_disk.lua
@@ -30,6 +30,14 @@ local format_disk = function(step, dd)
 		return false
 	end
 
+	local cmdsGPT = CmdChain.new()
+	local disk = dd:get_name()
+	cmdsGPT:set_replacements{
+	    disk = disk
+	}
+	cmdsGPT:add("/usr/sbin/cleargpt.sh ${disk}");
+	cmdsGPT:execute()
+
 	dd:cmds_format(cmds)
 
 		if not cmds:execute() then
