bfafb02c1e96efdfb0395d88ba7c8cc10d338d01
Scott Ullrich <sullrich@gmail.com>
2011-07-01 19:30:13 -0400

    Send to /dev/null

--- installer/cleargpt.sh
+++ installer/cleargpt.sh
@@ -2,6 +2,6 @@
 
 DISK=$1
 for PART in `gpart show $DISK | grep -v '=>' | awk '{ print $3 }'`; do
-	/sbin/gpart delete -i $PART $DISK
+	/sbin/gpart delete -i $PART $DISK >/dev/null
 done
-/sbin/gpart destroy $DISK
+/sbin/gpart destroy $DISK >/dev/null
