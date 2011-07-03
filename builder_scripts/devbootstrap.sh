c1ff9bb323852f6f0cb7da54f567620a6031bb0a
Scott Ullrich <sullrich@gmail.com>
2011-07-03 17:21:05 -0400

    Use full path to binaries

--- builder_scripts/devbootstrap.sh
+++ builder_scripts/devbootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-sleep 15
+echo
+sleep 3
+echo
+echo ">>> Starting the pfSense builder setup in 30 seconds..."
+/bin/sleep 30
 echo ""
 echo ""
 echo ">>> Welcome to the pfSense builder environment"
@@ -8,27 +12,30 @@ echo ">>> Please wait while we configure the environment..."
 echo ""
 echo "WITHOUT_X11=yo" > /etc/make.conf
 echo "BATCH=yo" >> /etc/make.conf
-mkdir -p /home/pfsense/pfSenseGITREPO /usr/pfSensesrc
-portsnap fetch extract 
+/bin/mkdir -p /home/pfsense/pfSenseGITREPO /usr/pfSensesrc
+/usr/sbin/portsnap fetch extract 
 cd /usr/ports/textproc/expat2 && make depends install
 cd /usr/ports/devel/git && make depends install
 cd /usr/ports/sysutils/fastest_cvsup/ && make depends install
-rehash   
 cd /home/pfsense && git clone git://github.com/bsdperimeter/pfsense-tools.git tools
 cd /home/pfsense && git clone https://github.com/sullrich/pfSense-freesbie2.git freesbie2
 cd /home/pfsense/tools/builder_scripts && chmod a+rx *.sh
-csup -h `fastest_cvsup -c tld -q` /usr/share/examples/cvsup/standard-supfile
+/usr/bin/csup -h `fastest_cvsup -c tld -q` /usr/share/examples/cvsup/standard-supfile
 cd /home/pfsense/tools/builder_scripts 
-./set_version.sh RELENG_2_0
-./apply_kernel_patches.sh
-./build_pfPorts.sh
-rm -rf /home/pfsense/installer
-./cvsup_bsdinstaller ; ./rebuild_bsdinstaller.sh
+cd /home/pfsense/tools/builder_scripts && ./set_version.sh RELENG_2_0
+cd /home/pfsense/tools/builder_scripts && ./apply_kernel_patches.sh
+cd /home/pfsense/tools/builder_scripts && ./build_pfPorts.sh
+/bin/rm -rf /home/pfsense/installer
+cd /home/pfsense/tools/builder_scripts && ./cvsup_bsdinstaller ; ./rebuild_bsdinstaller.sh
 echo ">>> Environment is complete. Building ISO..."
-./build_iso.sh
-if [ -f /tmp/pfSense.iso ]; then
+cd /home/pfsense/tools/builder_scripts && ./build_iso.sh
+if [ -f /tmp/builder/pfSense.iso ]; then
 	echo ">>> ISO build completed."
 	echo ">>> Moving devbootstrap.sh to /root/"
-	mv /etc/rc.local /root/devbootstrap.sh
+	/bin/mv /etc/rc.local /root/devbootstrap.sh
+	ls -lah /tmp/builder/
 fi
+
+echo
 echo ">>> Thanks for using the pfSense OVA build environment.  Goodbye."
+echo
