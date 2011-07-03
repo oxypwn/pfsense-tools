56aeacdfe99c14b26bbcecd5f2017ea166a38209
Scott Ullrich <sullrich@gmail.com>
2011-07-02 22:20:59 -0400

    Adding devbootstrap script

---
+++ builder_scripts/devbootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "WITHOUT_X11=yo" >> /etc/make.conf
+echo "BATCH=yo" >> /etc/make.conf
+mkdir -p /home/pfsense/pfSenseGITREPO /usr/pfSensesrc
+portsnap fetch extract 
+cd /usr/ports/textproc/expat2 && make depends install
+cd /usr/ports/devel/git && make depends install
+cd /usr/ports/sysutils/fastest_cvsup/ && make depends install
+# tcsh requires reloading available binaries - if you run this command and it fails -- don't worry.
+rehash   
+cd /home/pfsense && git clone git://github.com/bsdperimeter/pfsense-tools.git tools
+cd /home/pfsense && git clone https://github.com/sullrich/pfSense-freesbie2.git freesbie2
+cd /home/pfsense/tools/builder_scripts && chmod a+rx *.sh
+csup -h `fastest_cvsup -c tld -q` /usr/share/examples/cvsup/standard-supfile
+cd /home/pfsense/tools/builder_scripts 
+./set_version.sh RELENG_2_0
+./apply_kernel_patches.sh
+rm -rf /home/pfsense/installer
+/cvsup_bsdinstaller ; ./rebuild_bsdinstaller.sh
+./build_iso.sh
