f7c1937e298982e5dfea7e8788ba751be68e71ef
Scott Ullrich <sullrich@gmail.com>
2011-07-03 18:38:43 -0400

    Only pkg_add once

--- builder_scripts/devbootstrap.sh
+++ builder_scripts/devbootstrap.sh
@@ -18,15 +18,19 @@ echo ">>> Please wait while we configure the environment..."
 echo "WITHOUT_X11=yo" > /etc/make.conf
 echo "BATCH=yo" >> /etc/make.conf
 /bin/mkdir -p /home/pfsense/pfSenseGITREPO /usr/pfSensesrc
-pkg_add -r fastest_cvsup
-/usr/bin/csup -h `fastest_cvsup -c tld -q` /usr/share/examples/cvsup/ports-supfile
+if [ ! -f /usr/local/bin/fastest_cvsup ]; then
+	echo ">>> Installing fastest_cvsup binary package..."
+	( pkg_add -r fastest_cvsup ) >/dev/null
+	rm -rf /var/db/pkg/*
+fi
+/usr/bin/csup -h `/usr/local/bin/fastest_cvsup -c tld -q` /usr/share/examples/cvsup/ports-supfile
 cd /usr/ports/textproc/expat2 && make depends install
 cd /usr/ports/devel/git && make depends install
 cd /usr/ports/sysutils/fastest_cvsup/ && make depends install
 cd /home/pfsense && git clone git://github.com/bsdperimeter/pfsense-tools.git tools
 cd /home/pfsense && git clone https://github.com/sullrich/pfSense-freesbie2.git freesbie2
 cd /home/pfsense/tools/builder_scripts && chmod a+rx *.sh
-/usr/bin/csup -h `fastest_cvsup -c tld -q` /usr/share/examples/cvsup/standard-supfile
+/usr/bin/csup -h `/usr/local/bin/fastest_cvsup -c tld -q` /usr/share/examples/cvsup/standard-supfile
 cd /home/pfsense/tools/builder_scripts 
 cd /home/pfsense/tools/builder_scripts && ./set_version.sh RELENG_2_0
 cd /home/pfsense/tools/builder_scripts && ./apply_kernel_patches.sh
