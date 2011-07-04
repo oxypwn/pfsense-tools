128615ace26e8e77c703e5f37e93b688f96a1d98
Scott Ullrich <sullrich@gmail.com>
2011-07-04 15:13:40 -0400

    Revert, that did not work

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -586,9 +586,9 @@ recompile_pfPorts() {
 		echo ">>> Executing $PFPORTSBASENAME"
 
 		if [ "$1" != "" ]; then
-			eval( su - root -c "${USE_PORTS_FILE} -P ${1} -J '${MAKEJ_PORTS}' ${CHECK_PORTS_INSTALLED}" ) 2>&1
+			( su - root -c "${USE_PORTS_FILE} -P ${1} -J '${MAKEJ_PORTS}' ${CHECK_PORTS_INSTALLED}" ) 2>&1
 		else
-			eval( su - root -c "${USE_PORTS_FILE} -J '${MAKEJ_PORTS}' ${CHECK_PORTS_INSTALLED}" ) 2>&1 \
+			( su - root -c "${USE_PORTS_FILE} -J '${MAKEJ_PORTS}' ${CHECK_PORTS_INSTALLED}" ) 2>&1 \
 				| egrep -v '(\-Werror|ignored|error\.[a-z])' | egrep -wi "(^>>>|error)"
 		fi
 
