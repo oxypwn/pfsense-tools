71f0b676bd058f9c24b83ad626a3de0408142a3b
Scott Ullrich <sullrich@gmail.com>
2011-09-27 19:39:31 -0400

    Add script to checkout pfsense sources

---
+++ builder_scripts/checkout_pfsense_sources.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+#
+# Common functions to be used by build scripts
+#
+#  build_iso.sh
+#  Copyright (C) 2004-2009 Scott Ullrich
+#  All rights reserved.
+#  
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#  
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#  
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  
+#  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+#  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+#  AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+#  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+# Crank up error reporting, debugging.
+#  set -e 
+#  set -x
+
+# Suck in local vars
+. ./pfsense_local.sh
+
+# Suck in script helper functions
+. ./builder_common.sh
+
+# If a embedded build has been performed we need to nuke
+# /usr/obj.$dir/ since full uses a different
+# src.conf
+if [ -f ${MAKEOBJDIRPREFIX}/pfSense_wrap.$FREEBSD_VERSION.world.done ]; then
+	echo -n "Removing $MAKEOBJDIRPREFIX/* since embedded build performed prior..."
+	rm -rf ${MAKEOBJDIRPREFIX}/*
+	echo "done."
+fi
+
+# Define src.conf
+if [ -z "${SRC_CONF:-}" ]; then
+	export SRC_CONF="${BUILDER_SCRIPTS}/conf/src.conf.$FREEBSD_VERSION"
+	export SRC_CONF_INSTALL="${BUILDER_SCRIPTS}/conf/src.conf.$FREEBSD_VERSION.install"
+fi
+
+# Add etcmfs and rootmfs to the EXTRAPLUGINS plugins used by freesbie2
+export EXTRAPLUGINS="${EXTRAPLUGINS:-} rootmfs varmfs etcmfs"
+
+if [ ! -z "${CUSTOM_REMOVE_LIST:-}" ]; then
+	echo ">>> Using ${CUSTOM_REMOVE_LIST:-} ..."
+	export PRUNE_LIST="${CUSTOM_REMOVE_LIST:-}"
+else
+	echo ">>> Using ${BUILDER_SCRIPTS}/remove.list.iso.$FREEBSD_VERSION ..."
+	export PRUNE_LIST="${BUILDER_SCRIPTS}/remove.list.iso.$FREEBSD_VERSION"
+fi
+
+# This should be run first
+launch
+
+# Make sure source directories are present.
+ensure_source_directories_present
+
+# Ensure binaries are present that builder system requires
+install_required_builder_system_ports
+
+# Check if we need to force a ports rebuild
+check_for_forced_pfPorts_build
+
+# Clean up items that should be cleaned each run
+freesbie_clean_each_run
+
+# Allow old CVS_CO_DIR to be deleted later
+if [ -d $CVS_CO_DIR ]; then 
+	chflags -R noschg $CVS_CO_DIR
+fi 
+
+# Output build flags
+print_flags
+
+update_cvs_depot
