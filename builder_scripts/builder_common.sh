dccc50307119f426f08ec9b17b9f074b7a2a6add
sullrich <sullrich@sullrichs-iMac.local>
2011-10-20 16:44:19 -0400

    Fix typo

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -2652,7 +2652,7 @@ ova_prereq_check() {
 		if [ "$BUILDPLATFORM" = "amd64" ]; then
 			if [ ! -d /usr/lib32 ]; then
 				echo ">>> Building 32bit library compat support"
-				cd /usr/src ; make build32 install32 ; /etc/rc.d/ldconfig restart
+				cd /usr/src && make build32 install32 && /etc/rc.d/ldconfig restart
 			fi
 		fi
 		mkdir -p /var/db/ports/virtualbox-ose
