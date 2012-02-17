f2e2b5ddf65f3875b310dcaa3a8794c19ce95d76
Ermal <eri@pfsense.org>
2012-02-17 10:33:30 +0000

    There is presently a supported way for this
    Revert "Move this code block up, remove patch rejects, original files"
    
    This reverts commit 36ec60f6c0dffaec66e6c98b02677e0451f232ab.

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -3327,12 +3327,6 @@ update_freebsd_sources_and_apply_patches() {
 		fi
 	done
 
-	echo ">>> Removing old patch rejects..."
-	find $SRCDIR -name "*.rej" -exec rm {} \;
-	echo ">>> Removing original files ..."
-	find $SRCDIR -name "*.orig" | sed 's/.orig//g' | xargs rm -f
-	find $SRCDIR -name "*.orig" | xargs rm -f
-
 	# CVSUp freebsd version -- this MUST be after Loop through and remove files
 	BASENAMESUPFILE=`basename $SUPFILE`
 	echo -n ">>> Obtaining FreeBSD sources ${BASENAMESUPFILE}..."
@@ -3341,6 +3335,9 @@ update_freebsd_sources_and_apply_patches() {
 		| grep -v "error\." | grep -v "opensolaris" | \
 		grep -v "httpd-error"
 	echo "Done!"
+
+	echo ">>> Removing old patch rejects..."
+	find $SRCDIR -name "*.rej" -exec rm {} \;
 
 	echo -n ">>> Applying patches from $PFSPATCHFILE please wait..."
 	# Loop through and patch files
