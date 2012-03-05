99c7637c9865fee504ab79833f43c80d9f1ab779
Ermal <eri@pfsense.org>
2012-03-05 23:04:24 +0000

    Force package addition. Its better to get something rather than fight wihtout much information the ports system

--- builder_scripts/install_tmp_pkgs_in_chroot.sh
+++ builder_scripts/install_tmp_pkgs_in_chroot.sh
@@ -3,5 +3,5 @@
 /usr/sbin/pwd_mkdb -d /etc/ /etc/master.passwd
 FILELIST=`cd /tmp/pkg && ls -Utr`
 for FILE in $FILELIST; do
-	cd /tmp/pkg && pkg_add $FILE || true
+	cd /tmp/pkg && pkg_add -f $FILE || true
 done
