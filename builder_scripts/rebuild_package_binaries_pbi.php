8f78088930a3df5707c889e34068659a593cb06c
jim-p <jimp@pfsense.org>
2012-06-23 17:33:46 -0400

    Remove this echo, was just there for debugging.

--- builder_scripts/rebuild_package_binaries_pbi.php
+++ builder_scripts/rebuild_package_binaries_pbi.php
@@ -182,7 +182,6 @@ function overlay_pfPort($port_path) {
 	if (file_exists("{$pfports}/{$port_name}") && is_dir("{$pfports}/{$port_name}")) {
 		echo ">>> [" . date("H:i:s") . "] Overelaying pfPort {$port_name} onto {$port_path} ... ";
 		if (file_exists($port_path) && is_dir($port_path)) {
-			echo "Preserving old port in {$port_path}.orig ...";
 			system("/bin/rm -rf {$port_path}.orig");
 			system("/bin/mv {$port_path} {$port_path}.orig");
 		}
