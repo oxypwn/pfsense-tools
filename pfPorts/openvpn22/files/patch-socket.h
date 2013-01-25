9f8d113db8280f79c426246b0b78347ce0908fea
jim-p <jimp@pfsense.org>
2013-01-24 22:53:48 -0500

    Try again to fix openvpn22 build.

--- pfPorts/openvpn22/files/patch-socket.h
+++ pfPorts/openvpn22/files/patch-socket.h
@@ -1,11 +1,11 @@
---- socket.h.orig	2011-05-18 14:50:28.000000000 -0400
-+++ socket.h	2011-05-18 14:50:39.000000000 -0400
-@@ -242,7 +242,7 @@
+--- ./socket.h.orig	2011-12-13 17:58:56.000000000 +0100
++++ ./socket.h	2013-01-11 21:50:11.000000000 +0100
+@@ -225,7 +225,7 @@
  
  #if PASSTOS_CAPABILITY
    /* used to get/set TOS. */
 -  uint8_t ptos;
-+  uint32_t ptos;
++  int ptos;
    bool ptos_defined;
  #endif
  
