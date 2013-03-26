c7f2244b99a294150ca61cc3ef231a4c5575629b
Ermal <eri@pfsense.org>
2013-03-26 10:11:39 +0000

    Do not account for not local ips with rate_abusers. Fixes #2909 and Ticket #2822

--- pfPorts/rate/files/patch-rate_abusers.c
+++ pfPorts/rate/files/patch-rate_abusers.c
@@ -1,5 +1,5 @@
---- rate_abusers.c.orig	2013-03-05 18:36:19.000000000 +0000
-+++ rate_abusers.c	2013-03-05 18:48:47.000000000 +0000
+--- rate_abusers.c.orig	2013-03-26 09:50:36.000000000 +0000
++++ rate_abusers.c	2013-03-26 10:10:02.000000000 +0000
 @@ -195,8 +195,6 @@
  	struct ip *iph;
  	u_int32_t sip;
@@ -9,7 +9,7 @@
  
  	if(caplen < sizeof(struct ip)) return;
  
-@@ -206,14 +204,12 @@
+@@ -206,14 +204,19 @@
  	sip = iph->ip_src.s_addr;
  	dip = iph->ip_dst.s_addr;
  
@@ -17,15 +17,21 @@
 -	if(is_ours(ipci, dip)) in = len;
 +	if (!len)
 +		return;
-+	if ((is_ours(ipci, sip) && is_ours(ipci, dip)) && (!opt_local)) return;
  
 -	if(!(in || out)) return;
 -	if(in && out && (!opt_local)) return;
--
++	if(is_ours(ipci, sip) && is_ours(ipci, dip) && (!opt_local)) return;
+ 
 -	if(in)  add_entry(ntohl(dip), in, 0);
 -	if(out) add_entry(ntohl(sip), 0, out);
-+	add_entry(ntohl(sip), (long long)len, 0);
-+	add_entry(ntohl(dip), 0, (long long)len);
++	if(is_ours(ipci, sip) && !is_ours(ipci, dip))
++		add_entry(ntohl(sip), 0, (long long)len);
++	if(!is_ours(ipci, sip) && is_ours(ipci, dip))
++		add_entry(ntohl(dip), (long long)len, 0);
++	if(is_ours(ipci, sip) && is_ours(ipci, dip)) {
++		add_entry(ntohl(dip), (long long)len, 0);
++		add_entry(ntohl(sip), 0, (long long)len);
++	}
  }
  
  void r_abusers_setup(int argc, char ** argv,
