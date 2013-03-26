13fefb5ec572a82261bd77c8a183bc079246628b
Ermal <eri@pfsense.org>
2013-03-26 10:17:06 +0000

    Optimize a bit. Ticket #2909 and Ticket #2822

--- pfPorts/rate/files/patch-rate_abusers.c
+++ pfPorts/rate/files/patch-rate_abusers.c
@@ -1,15 +1,16 @@
---- rate_abusers.c.orig	2013-03-26 09:50:36.000000000 +0000
-+++ rate_abusers.c	2013-03-26 10:10:02.000000000 +0000
-@@ -195,8 +195,6 @@
+--- rate_abusers.c.orig	2013-03-26 10:14:00.000000000 +0000
++++ rate_abusers.c	2013-03-26 10:15:53.000000000 +0000
+@@ -195,8 +195,7 @@
  	struct ip *iph;
  	u_int32_t sip;
  	u_int32_t dip;
 -	u_int32_t host;
 -	long long in = 0, out = 0;
++	int slocal = 0, dlocal = 0;
  
  	if(caplen < sizeof(struct ip)) return;
  
-@@ -206,14 +204,19 @@
+@@ -206,14 +205,21 @@
  	sip = iph->ip_src.s_addr;
  	dip = iph->ip_dst.s_addr;
  
@@ -20,15 +21,18 @@
  
 -	if(!(in || out)) return;
 -	if(in && out && (!opt_local)) return;
-+	if(is_ours(ipci, sip) && is_ours(ipci, dip) && (!opt_local)) return;
- 
+-
 -	if(in)  add_entry(ntohl(dip), in, 0);
 -	if(out) add_entry(ntohl(sip), 0, out);
-+	if(is_ours(ipci, sip) && !is_ours(ipci, dip))
++	slocal = is_ours(ipci, sip);
++	dlocal = is_ours(ipci, dip);
++	if(slocal && dlocal && (!opt_local)) return;
++
++	if(slocal && !dlocal)
 +		add_entry(ntohl(sip), 0, (long long)len);
-+	if(!is_ours(ipci, sip) && is_ours(ipci, dip))
++	else if(!slocal && dlocal)
 +		add_entry(ntohl(dip), (long long)len, 0);
-+	if(is_ours(ipci, sip) && is_ours(ipci, dip)) {
++	else if(slocal && dlocal) {
 +		add_entry(ntohl(dip), (long long)len, 0);
 +		add_entry(ntohl(sip), 0, (long long)len);
 +	}
