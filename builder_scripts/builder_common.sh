82ada5c57afd32d599509499dbc76fa469d909f4
jim-p <jimp@pfsense.org>
2013-04-24 10:33:39 -0400

    Add a way to skip setting the prompt.

--- builder_scripts/builder_common.sh
+++ builder_scripts/builder_common.sh
@@ -1297,7 +1297,9 @@ cust_fixup_wrap() {
 }
 
 setup_tcshrc_prompt() {
-	echo 'set prompt="%{\033[0;1;33m%}[%{\033[0;1;37m%}`cat /etc/version`%{\033[0;1;33m%}]%{\033[0;1;33m%}%B[%{\033[0;1;37m%}%n%{\033[0;1;31m%}@%{\033[0;1;37m%}%M%{\033[0;1;33m%}]%{\033[0;1;32m%}%b%/%{\033[0;1;33m%}(%{\033[0;1;37m%}%h%{\033[0;1;33m%})%{\033[0;1;36m%}%{\033[0;1;31m%}:%{\033[0;40;37m%} "' > ${PFSENSEBASEDIR}/root/.tcshrc
+	if [ ! -n ${SKIP_TCSH_PROMPT} ]; then
+		echo 'set prompt="%{\033[0;1;33m%}[%{\033[0;1;37m%}`cat /etc/version`%{\033[0;1;33m%}]%{\033[0;1;33m%}%B[%{\033[0;1;37m%}%n%{\033[0;1;31m%}@%{\033[0;1;37m%}%M%{\033[0;1;33m%}]%{\033[0;1;32m%}%b%/%{\033[0;1;33m%}(%{\033[0;1;37m%}%h%{\033[0;1;33m%})%{\033[0;1;36m%}%{\033[0;1;31m%}:%{\033[0;40;37m%} "' > ${PFSENSEBASEDIR}/root/.tcshrc
+	fi
 	echo 'set autologout="0"' >> ${PFSENSEBASEDIR}/root/.tcshrc
 	echo 'set autolist set color set colorcat' >> ${PFSENSEBASEDIR}/root/.tcshrc
 	echo 'setenv CLICOLOR "true"' >> ${PFSENSEBASEDIR}/root/.tcshrc
