8f04a107f249b1fa83ab49e79e6150506d92efdc
Ermal <eri@pfsense.org>
2013-06-20 11:05:16 +0000

    Remove options from here since htey will bes etp properly later

--- builder_scripts/devbootstrap.sh
+++ builder_scripts/devbootstrap.sh
@@ -45,10 +45,6 @@ echo ">>> Starting the pfSense builder setup in 15 seconds..."
 DCPUS=`sysctl kern.smp.cpus | cut -d' ' -f2`
 CPUS=`expr $DCPUS '*' 2`
 echo ">>> Detected CPUs * 2: $CPUS"
-
-echo WITHOUT_X11="yo" > /etc/make.conf
-echo BATCH="yo" >> /etc/make.conf
-echo SUBTHREADS="${CPUS}" >> /etc/make.conf
 echo clear_tmp_enable="YES" >> /etc/rc.conf
 
 echo ""
