1caff68a401593c6b690ed4a9b9e009cdf32c7c5
Ermal <eri@pfsense.org>
2013-06-20 11:07:47 +0000

    Include builder config when showing flags

--- builder_scripts/print_builder_variables.sh
+++ builder_scripts/print_builder_variables.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 CURRENTDIR=`pwd`
+[ -r "${CURRENTDIR}/pfsense-build.conf" ] && . ${CURRENTDIR}/pfsense-build.conf
 [ -r "${CURRENTDIR}/pfsense_local.sh" ] && . ${CURRENTDIR}/pfsense_local.sh
 [ -r "${CURRENTDIR}/builder_common.sh" ] && . ${CURRENTDIR}/builder_common.sh
 
