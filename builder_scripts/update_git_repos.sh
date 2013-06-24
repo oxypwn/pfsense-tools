620dde37e1a29657c39bfba8b27d453e7c5a00f3
Ermal <eri@pfsense.org>
2013-06-24 12:49:27 +0000

    Restore normal includes

--- builder_scripts/update_git_repos.sh
+++ builder_scripts/update_git_repos.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
 
 # Suck in local vars
-if [ -f ./pfsense_local.sh ]; then
-	. ./pfsense_local.sh
-elif [ -f ../pfsense_local.sh]; then
-	. ../pfsense_local.sh
-else
-	echo "You are calling this script from wrong location"
-	exit 1
-fi
+. ./pfsense_local.sh
 
 # Suck in script helper functions
-if [ -f ./builder_common.sh ]; then
-        . ./builder_common.sh
-elif [ -f ../builder_common.sh]; then
-        . ../builder_common.sh
-else
-        echo "You are calling this script from wrong location"
-        exit 1
-fi
+. ./builder_common.sh
 
 set +e
 
