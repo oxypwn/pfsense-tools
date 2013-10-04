1c8e4ebc11fac1b7df9cbd83175ef57ff1c4357a
Renato Botelho <garga@FreeBSD.org>
2013-10-04 16:14:08 -0300

    take FREEBSD_VERSION in consideration to set DEFAULT_KERNEL for build_iso.sh as it's one on build_updates.sh

--- builder_scripts/build_iso.sh
+++ builder_scripts/build_iso.sh
@@ -63,7 +63,7 @@ else
 	export PRUNE_LIST="${BUILDER_SCRIPTS}/conf/rmlist/remove.list.iso.$FREEBSD_VERSION"
 fi
 
-export DEFAULT_KERNEL=${DEFAULT_KERNEL:-pfSense_SMP.8}
+export DEFAULT_KERNEL=${DEFAULT_KERNEL:-pfSense_SMP.${FREEBSD_VERSION}}
 
 # This should be run first
 launch
