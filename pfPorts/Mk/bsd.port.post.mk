63231dae574f2b7a489dd63e066eb1d2fc5ca23c
Renato Botelho <garga@FreeBSD.org>
2014-01-08 11:26:45 -0200

    Remove outdated and useless Mk/ copy from pfPorts

--- pfPorts/Mk/bsd.port.post.mk
+++
@@ -1,7 +0,0 @@
-# $FreeBSD: ports/Mk/bsd.port.post.mk,v 1.3 1999/08/25 04:40:21 obrien Exp $
-
-AFTERPORTMK=	yes
-
-.include "bsd.port.mk"
-
-.undef AFTERPORTMK
--- pfPorts/Mk/bsd.port.pre.mk
+++
@@ -1,7 +0,0 @@
-# $FreeBSD: ports/Mk/bsd.port.pre.mk,v 1.3 1999/08/25 04:40:21 obrien Exp $
-
-BEFOREPORTMK=	yes
-
-.include "bsd.port.mk"
-
-.undef BEFOREPORTMK
