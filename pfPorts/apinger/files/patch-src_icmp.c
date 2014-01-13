ed066bf72ed264191def27e99a8a73cea3e3ce18
Renato Botelho <garga@FreeBSD.org>
2014-01-13 15:54:15 -0200

    Reduce diffs from ports apinger and set IPV6 on make.conf

---
+++ pfPorts/apinger/files/patch-src_icmp.c
@@ -0,0 +1,14 @@
+
+$FreeBSD: head/net/apinger/files/patch-src_icmp.c 300897 2012-07-14 14:29:18Z beat $
+
+--- src/icmp.c.orig
++++ src/icmp.c
+@@ -197,7 +197,7 @@
+ 	debug("checking CMSG...");
+ 	for (c = CMSG_FIRSTHDR(&msg); c; c = CMSG_NXTHDR(&msg, c)) {
+ 		debug("CMSG level: %i type: %i",c->cmsg_level,c->cmsg_type);
+-		if (c->cmsg_level != SOL_SOCKET || c->cmsg_type != SO_TIMESTAMP)
++		if (c->cmsg_level != SOL_SOCKET || c->cmsg_type != SCM_TIMESTAMP)
+ 			continue;
+ 		if (c->cmsg_len < CMSG_LEN(sizeof(struct timeval)))
+ 			continue;
